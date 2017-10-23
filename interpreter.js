b43fdffd01da503076eb7b7e75a897f701c6f4b3
shine1594 <panamera1594@gmail.com>
2017-10-23 11:18:11 +0900

    [REF] remove global variables

--- interpreter.js
+++ interpreter.js
@@ -1,66 +1,68 @@
 var model = require('./model');
-var statusList = ['todo', 'doing', 'done'];
 
-var commandList = {
-    show: {
-        validate: function(tokens) {
-            //show 명령어 parameter 개수는 1개
-            //parameter는 status 값 ('todo' OR 'doing' OR 'done')
+var interpreter = {
+    statusList: ['todo', 'doing', 'done'];
+    commandList: {
+        show: {
+            validate: function(tokens) {
+                //show 명령어 parameter 개수는 1개
+                //parameter는 status 값 ('todo' OR 'doing' OR 'done')
 
-            var status = tokens[0];
+                var status = tokens[0];
 
-            if (tokens.length !== 1) {
-                return false;
+                if (tokens.length !== 1) {
+                    return false;
+                }
+
+                return statusList.indexOf(status) > -1;
             }
+        },
+        add: {
+            validate: function(tokens) {
+                return tokens.length === 1;
+            }
+        },
+        update: {
+            validate: function(tokens) {
+                //첫 번째 parameter는 정수 (id)
+                //두 번째 parameter는 status 값 ('todo' OR 'doing' OR 'done')
 
-            return statusList.indexOf(status) > -1;
+                if (tokens.length !== 2) {
+                    return false;
+                }
+
+                var id = tokens[0];
+                var status = tokens[1];
+
+                return /^\+?(0|[1-9]\d*)$/.test(id) && statusList.indexOf(status) > -1;
+            }
         }
     },
-    add: {
-        validate: function(tokens) {
-            return tokens.length === 1;
-        }
+    validate: function(tokens) {
+        var command = tokens[0];
+        var isValidCommand = commandList.hasOwnProperty(command);
+
+        tokens.shift(); //remove first element
+
+        return isValidCommand && commandList[command].validate(tokens);
     },
-    update: {
-        validate: function(tokens) {
-            //첫 번째 parameter는 정수 (id)
-            //두 번째 parameter는 status 값 ('todo' OR 'doing' OR 'done')
+    execute: function(input) {
+        var tokens = input.split('$');
+        var command = tokens[0];
 
-            if (tokens.length !== 2) {
-                return false;
-            }
+        var isValidInput = validate(tokens);
+        if (!isValidInput) {
+            console.log('명령어 형식이 올바르지 않습니다.');
+            return false;
+        }
 
-            var id = tokens[0];
-            var status = tokens[1];
-
-            return /^\+?(0|[1-9]\d*)$/.test(id) && statusList.indexOf(status) > -1;
+        try {
+            // 첫 번째 토큰인 command가 validate method에서 삭제 된 상태
+            model[command].apply(null, tokens);
+        } catch (exception) {
+            console.log(exception);
         }
     }
-};
-
-var validate = function(tokens) {
-    var command = tokens[0];
-    var isValidCommand = commandList.hasOwnProperty(command);
-
-    tokens.shift(); //remove first element
-
-    return isValidCommand && commandList[command].validate(tokens);
 }
 
-exports.execute = function(input) {
-    var tokens = input.split('$');
-    var command = tokens[0];
-
-    var isValidInput = validate(tokens);
-    if (!isValidInput) {
-        console.log('명령어 형식이 올바르지 않습니다.');
-        return false;
-    }
-
-    try {
-        // 첫 번째 토큰인 command가 validate method에서 삭제 된 상태
-        model[command].apply(null, tokens);
-    } catch (exception) {
-        console.log(exception);
-    }
-};
+module.exports = interpreter;
--- model.js
+++ model.js
@@ -1,85 +1,86 @@
-nextId = 1;
-taskList = [];
-
-var showCurrentStatus = function() {
-    var counts = {
-        todo: 0,
-        doing: 0,
-        done: 0
-    }
-
-    taskList.forEach(function(task) {
-        var status = task.status;
-        counts[status]++;
-    });
-
-    console.log(`현재상태 : todo:${counts.todo}개, doing:${counts.doing}개, done:${counts.done}개`);
-}
-
-exports.show = function (status) {
-    var tasks = taskList.filter(function(task) {
-        return task.status === status
-    });
-
-    if (tasks.length === 0) {
-        throw `${status}상태의 task가 존재하지 않습니다.`;
-    } else if (status === 'done'){
-        //완료 시간 계산
-        tasks.forEach(function(task) {
-            var timeDiff = null;
-
-            if (task.startAt === undefined) {
-                timeDiff = task.doneAt - task.createdAt;
-            } else {
-                timeDiff = task.doneAt - task.startAt;
-            }
-
-            var taskClearTime = Math.floor(timeDiff / 1000 / 60 / 60);
-
-            console.log(`${task.id}, ${task.content}, ${taskClearTime}시간`);
-        });
-    } else {
-        tasks.forEach(function(task) {
-            console.log(`${task.id}, ${task.content}`);
-        });
-    }
-};
-exports.add = function (content) {
-    var newTask = {
-        id: nextId++,
-        status: 'todo',
-        content: content,
-        createdAt: new Date()
-    }
-
-    taskList.push(newTask);
-
-    console.log(`id: ${newTask.id},  "${newTask.content}" 항목이 새로 추가됐습니다.`);
-    showCurrentStatus();
-};
-
-exports.update = function (id, status) {
-    id = parseInt(id);
-    var targetTask = taskList.find(function(task) {
-        return task.id === id;
-    });
-
-    if (targetTask === undefined) {
-        throw `id가 ${id}인 task가 존재하지 않습니다.`;
-    } else if (targetTask.status === status) {
-        throw `해당하는 task가 이미 ${status}상태입니다.`;
-    } else {
-        //상태 변경
-        targetTask.status = status;
-
-        //작업 시작/완료시각 저장
-        if (status === 'doing') {
-            targetTask.startAt = new Date();
-        } else if (status === 'done') {
-            targetTask.doneAt = new Date();
+var model {
+    nextId: 1,
+    taskLis: [],
+    showCurrentStatus: function () {
+        var counts = {
+            todo: 0,
+            doing: 0,
+            done: 0
         }
 
-        //3초 후에 결과 출력
-        setTimeout(showCurrentStatus, 3000);
+        taskList.forEach(function(task) {
+            var status = task.status;
+            counts[status]++;
+        });
+
+        console.log(`현재상태 : todo:${counts.todo}개, doing:${counts.doing}개, done:${counts.done}개`);
+    },
+    show: function(status) {
+        var tasks = taskList.filter(function(task) {
+            return task.status === status
+        });
+
+        if (tasks.length === 0) {
+            throw `${status}상태의 task가 존재하지 않습니다.`;
+        } else if (status === 'done'){
+            //완료 시간 계산
+            tasks.forEach(function(task) {
+                var timeDiff = null;
+
+                if (task.startAt === undefined) {
+                    timeDiff = task.doneAt - task.createdAt;
+                } else {
+                    timeDiff = task.doneAt - task.startAt;
+                }
+
+                var taskClearTime = Math.floor(timeDiff / 1000 / 60 / 60);
+
+                console.log(`${task.id}, ${task.content}, ${taskClearTime}시간`);
+            });
+        } else {
+            tasks.forEach(function(task) {
+                console.log(`${task.id}, ${task.content}`);
+            });
+        }
+    },
+    add: function(content) {
+        var newTask = {
+            id: nextId++,
+            status: 'todo',
+            content: content,
+            createdAt: new Date()
+        }
+
+        taskList.push(newTask);
+
+        console.log(`id: ${newTask.id},  "${newTask.content}" 항목이 새로 추가됐습니다.`);
+        showCurrentStatus();
+    },
+    update: function(id, status) {
+        id = parseInt(id);
+        var targetTask = taskList.find(function(task) {
+            return task.id === id;
+        });
+
+        if (targetTask === undefined) {
+            throw `id가 ${id}인 task가 존재하지 않습니다.`;
+        } else if (targetTask.status === status) {
+            throw `해당하는 task가 이미 ${status}상태입니다.`;
+        } else {
+            //상태 변경
+            targetTask.status = status;
+
+            //작업 시작/완료시각 저장
+            if (status === 'doing') {
+                targetTask.startAt = new Date();
+            } else if (status === 'done') {
+                targetTask.doneAt = new Date();
+            }
+
+            //3초 후에 결과 출력
+            setTimeout(showCurrentStatus, 3000);
+        }
     }
-};
+}
+
+module.exports = model;
