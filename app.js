ee54fadae29ffcaa8a2589e34d9733cc3ac927b7
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-10-24 13:43:12 +0900

    change array to splitInput

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ var common = require('./common.js');
     var user = new User();
 
     rl.on('line', function (answer) {
-        var array = answer.split("$");
-        var cmd = array[0];
+        var splitInput = answer.split("$");
+        var cmd = splitInput[0];
         if (cmd === "add") {
-            var title = array[1];
+            var title = splitInput[1];
             user.addTask(title).showAllTask();
         } else if (cmd === "show") {
-            var status = common.STATUS_CONSTANT[array[1]];
+            var status = common.STATUS_CONSTANT[splitInput[1]];
             user.showTasks(status);
         } else if (cmd === "update") {
-            var id = array[1];
-            var status = common.STATUS_CONSTANT[array[2]];
+            var id = splitInput[1];
+            var status = common.STATUS_CONSTANT[splitInput[2]];
             user.updateTask(id, status).showAllTask();
         } else if (cmd === "short") {
             user.showShortTask();
