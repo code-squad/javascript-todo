b3a4aff963edf1f97f8a99704a1c3dee03bd537d
wfe-luigi <kdh3979@naver.com>
2017-10-24 22:00:15 +0900

    소요시간 계산 추가

--- todo.js
+++ todo.js
@@ -8,13 +8,10 @@
 > "add$자바스크립트 공부하기";
 > id: 5,  "자바스크립트 공부하기" 항목이 새로 추가됐습니다.  //추가된 결과 메시지를 이렇게 출력
 > 현재상태 :  todo:1개, doing:2개, done:2개
-
 > "show$doing;
 > "1, 그래픽스공부", "4, 블로그쓰기"  //id값과 함께 task제목이 출력된다.
-
 > "show$done;
 > //완료한 목록노출.(doing와 동일한 구조로 출력)
-
 > "update$3$done;
 > 현재상태 :  todo:1개, doing:1개, done:3개  //변경된 모든 상태가 노출.
 */
@@ -36,33 +33,26 @@ function printMsg() {
     }
     console.log(result);
 }
-const obj = [
-    {
-        id: 1,
-        task: "자바스크립트 공부",
-        state: "doing"
-    },
-    {
-        id: 2,
-        task: "그래픽스 공부",
-        state: "todo"
-    },
-    {
-        id: 3,
-        task: "낮잠자기",
-        state: "done"
-    },
-    {
-        id: 4,
-        task: "간식먹기",
-        state: "doing"
-    },
-    {
-        id: 5,
-        task: "만화보기",
-        state: "done"
+const obj = [];
+/*
+//key:value 를 검색해서 value->value2로 수정 
+//ex)setState("state","doing","done") -> state가 doing인 객체의 state를 done으로 수정
+function setValue(key, value, value2) {
+    for (property in obj) {
+        if (obj[property][key] === value) {
+            obj[property][key] = value2;
+        }
     }
-]
+}
+
+//객체를 순회하며 key:value 가 있는 객체를 출력
+function getObj(key, value) { //key로 서치. ex)getObj("task","간식먹기") getState 랑 살짝겹침
+for (property in obj) {
+    if (obj[property][key] === value) printMsg(objTostr(obj[property]));
+}
+start();
+}
+*/
 
 //배열을 순회하며 state count 를 출력
 function getState() {
@@ -78,7 +68,7 @@ function getState() {
 function objTostr(target) {
     var result = "";
     for (property in target) {
-        if (target[property].constructor == 'object') objTostr(target[property]);
+        if (target[property].constructor === 'object') objTostr(target[property]);
         else result += property + ":" + target[property] + " ";
     }
     return result;
@@ -88,47 +78,51 @@ function objTostr(target) {
 function add(task) {
     var id = 1;
     id += obj[obj.length - 1]['id'];
-    obj[obj.length] = { "id": id, "task": task, "state": "todo" };
-    printMsg("id : ", id, "  \"", task, "\" 항목이 새로 추가됬습니다.")
+    obj[obj.length] = { "id": id, "task": task, "state": "todo", "start": Date.now() };
+    printMsg("id : ", id, "  \"", task, "\" 항목이 새로 추가됬습니다. ");
     getState();
-}
-
-//객체를 순회하며 key:value 가 있는 객체를 출력
-function getObj(key, value) { //key로 서치. ex)getObj("task","간식먹기") getState 랑 살짝겹침
-    for (property in obj) {
-        if (obj[property][key] === value) printMsg(objTostr(obj[property]));
-    }
-    start();
 }
 
 //showing 전용함수 . 출력형태를 객체전체에서 id,task로 수정 -> "1, 그래픽스 공부" , "4, 블로그쓰기" .. 
 function showing(key, value) {
     var result = [];
-    for (property in obj) {
-        if (obj[property][key] === value)
-            result += "\"" + obj[property]['id'] + ", " + obj[property]['task'] + "\"  ";
+    if (value === "done") {
+        for (property in obj) {
+            var task = obj[property]['task'];
+            var time = parseInt((Date.now() - obj[property]['start']) / 1000);
+            if (obj[property][key] === value)
+                result += "\'" + task + ", " + time + "초\' ";
+        }
+    }
+    else {
+        for (property in obj) {
+            var id = obj[property]['id'];
+            var task = obj[property]['task'];
+            if (obj[property][key] === value)
+                result += "\'" + id + ", " + task + "\' ";
+        }
     }
     printMsg(result);
     start();
-}
-
-//key:value 를 검색해서 value->value2로 수정 
-//ex)setState("state","doing","done") -> state가 doing인 객체의 state를 done으로 수정
-function setValue(key, value, value2) {
-    for (property in obj) {
-        if (obj[property][key] === value) {
-            obj[property][key] = value2;
-        }
-    }
 }
 
 //obj[property][id] == index 로 값을 찾아서 state 수정
 //매개변수가 '부족'할때만 에러메세지 update$index$state -> 3개
 //id:index 검색 -> state:value로 수정 ex)setState(1,"done") -> id가 1인 객체의 state를 "done"으로 수정
 function setState(index, value) {
-    for (property in obj) {
-        if (obj[property]['id'] === index) {
-            obj[property]['state'] = value;
+    if (value === "done") {
+        for (property in obj) {
+            if (obj[property]['id'] === index) {
+                obj[property]['state'] = value;
+                obj[property]['end'] = Date.now();
+            }
+        }
+    }
+    else {
+        for (property in obj) {
+            if (obj[property]['id'] === index) {
+                obj[property]['state'] = value;
+            }
         }
     }
     getState();
@@ -141,7 +135,7 @@ function start() {
         var cmd = answer.split("$")[0]; //answer = add$test -> answer.split("$")[0] = add
         var check = Number(allcmd.indexOf(cmd)); //allcmd.indexOf(cmd)); //-1일경우 없는 명령어
 
-        if (check == -1) {
+        if (check === -1) {
             printMsg("wrong input!");
             start();
         }
@@ -167,5 +161,3 @@ function start() {
     });
 }
 start();
-//getState();
-//rl.close();
