3e51ed41e6e32c5bcdda569ddcc220bafa698dde
wfe-luigi <kdh3979@naver.com>
2017-10-25 22:42:53 +0900

    전체코드 리터럴객체화

--- todo.js
+++ todo.js
@@ -1,148 +1,119 @@
-/*
-할일을 추가할 수 있다.
-할일이 추가되면 id 값을 생성하고 결과를 알려준다.
-상태는 3가지로 관리된다. todo, doing, done.
-각 일(task)는 상태값을 가지고 있고, 그 상태값을 변경할 수 있다.
-각 상태에 있는 task는 show함수를 통해서 볼 수 있다.
-명령어를 입력시이 '$'를 구분자로 사용해서 넣는다.
-> "add$자바스크립트 공부하기";
-> id: 5,  "자바스크립트 공부하기" 항목이 새로 추가됐습니다.  //추가된 결과 메시지를 이렇게 출력
-> 현재상태 :  todo:1개, doing:2개, done:2개
-> "show$doing;
-> "1, 그래픽스공부", "4, 블로그쓰기"  //id값과 함께 task제목이 출력된다.
-> "show$done;
-> //완료한 목록노출.(doing와 동일한 구조로 출력)
-> "update$3$done;
-> 현재상태 :  todo:1개, doing:1개, done:3개  //변경된 모든 상태가 노출.
-*/
-
-//nodjs에서 제공하는 모듈(라이브러리)를 불러온다. 
 const readline = require('readline');
-
-//불러온 readline을 사용할 수 있게 초기화를 한다. 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const obj = [{ id: 1, task: "테스트1", state: "doing" }];
 
-//이어붙여서 출력
-function printMsg() {
-    var result = [];
-    for (var i = 0; i < arguments.length; i++) {
-        result += arguments[i];
-    }
-    console.log(result);
+function Task(task) {
+    this.id = '';
+    this.task = task;
+    this.state = 'todo';
+    this.start = new Date();
+    this.end = null;
 }
-const obj = [{ id: "ttt", state: "doing" }, { id: "test1", state: "done", start: 10, end: 60 }, { id: "test2", state: "done", start: 10, end: 15 }, { id: "test1", state: "done", start: 10, end: 30 }];
-/*
-//key:value 를 검색해서 value->value2로 수정 
-//ex)setState("state","doing","done") -> state가 doing인 객체의 state를 done으로 수정
-function setValue(key, value, value2) {
-    for (property in obj) {
-        if (obj[property][key] === value) {
-            obj[property][key] = value2;
+
+var util = {
+    printMsg: function () {
+        var result = []
+        for (var i = 0; i < arguments.length; i++) {
+            result += arguments[i];
         }
+        console.log(result);
+    },
+    //object를 string 으로 변환하여 출력
+    //obj = [ { id: 1, task: "자바스크립트 공부", state: "doing" } -> id:1 task:자바스크립트 공부 state:doing
+    objTostr: function (target) {
+        var result = "";
+        for (property in target) {
+            if (target[property].constructor === 'object') util.objTostr(target[property]);
+            else result += property + ":" + target[property] + " ";
+        }
+        return result;
     }
 }
 
-//객체를 순회하며 key:value 가 있는 객체를 출력
-function getObj(key, value) { //key로 서치. ex)getObj("task","간식먹기") getState 랑 살짝겹침
-for (property in obj) {
-    if (obj[property][key] === value) printMsg(objTostr(obj[property]));
+var todo = {
+
+    //add 명령어 함수. 배열의 마지막 객체의 id값을 1증가하여 id생성. 매개변수를 task명으로 넣고 현재상태출력
+    add: function (task) {
+        var id = 1;
+        id += obj[obj.length - 1]['id'];
+        obj[obj.length] = { "id": id, "task": task, "state": "todo", "start": null };
+        util.printMsg("id : ", id, "  \"", task, "\" 항목이 새로 추가됬습니다. ");
+        this.getState();
+    },
+
+    //배열을 순회하며 state count 를 출력
+    getState: function () {
+        var flag = { todo: 0, doing: 0, done: 0 };
+        for (property in obj) {
+            flag[obj[property]['state']]++;
+        }
+        util.printMsg("현재상태 : ", util.objTostr(flag));
+    },
+
+    //showing 전용함수 . 출력형태를 객체전체에서 id,task로 수정 -> "1, 그래픽스 공부" , "4, 블로그쓰기" .. 
+    showing: function (key, value) {
+        var result = [];
+        if (value === "done") {
+            for (property in obj) {
+                var task = obj[property]['task'];
+                var time = parseInt((Date.now() - obj[property]['start']) / 1000);
+                if (obj[property][key] === value)
+                    result += "\'" + task + ", " + time + "초\' ";
+            }
+        }
+        else {
+            for (property in obj) {
+                var id = obj[property]['id'];
+                var task = obj[property]['task'];
+                if (obj[property][key] === value)
+                    result += "\'" + id + ", " + task + "\' ";
+            }
+        }
+        util.printMsg(result);
+        start();
+    },
+
+    //obj[property][id] == index 로 값을 찾아서 state 수정
+    //매개변수가 '부족'할때만 에러메세지 update$index$state -> 3개
+    //id:index 검색 -> state:value로 수정 ex)setState(1,"done") -> id가 1인 객체의 state를 "done"으로 수정
+    setState: function (index, value) {
+        if (value === "done") {
+            for (property in obj) {
+                if (obj[property]['id'] === index) {
+                    obj[property]['state'] = value;
+                    obj[property]['end'] = Date.now();
+                }
+            }
+        }
+        else {
+            for (property in obj) {
+                if (obj[property]['id'] === index) {
+                    obj[property]['state'] = value;
+                }
+            }
+        }
+        this.getState();
+        start();
+    },
+    //state가 done인 상태의 객체를 찾아서 소요시간이 더 짧은 객체만 result에 남겨서 출력
+    getMinTask: function () {
+        var runtime = Date.now();
+        var result;
+        for (property in obj) {
+            if (obj[property]['state'] === "done" && obj[property]['end'] - obj[property]['start'] < runtime) {
+                runtime = obj[property]['end'] - obj[property]['start'];
+                result = obj[property];
+            }
+        }
+        util.printMsg(result);
+    }
+
 }
 start();
-}
-*/
 
-//배열을 순회하며 state count 를 출력
-function getState() {
-    var flag = { todo: 0, doing: 0, done: 0 };
-    for (property in obj) {
-        flag[obj[property]['state']]++;
-    }
-    printMsg("현재상태 : ", objTostr(flag));
-}
-
-//object를 string 으로 변환하여 출력
-//obj = [ { id: 1, task: "자바스크립트 공부", state: "doing" } -> id:1 task:자바스크립트 공부 state:doing
-function objTostr(target) {
-    var result = "";
-    for (property in target) {
-        if (target[property].constructor === 'object') objTostr(target[property]);
-        else result += property + ":" + target[property] + " ";
-    }
-    return result;
-}
-
-//add 명령어 함수. 배열의 마지막 객체의 id값을 1증가하여 id생성. 매개변수를 task명으로 넣고 현재상태출력
-function add(task) {
-    var id = 1;
-    id += obj[obj.length - 1]['id'];
-    obj[obj.length] = { "id": id, "task": task, "state": "todo", "start": Date.now() };
-    printMsg("id : ", id, "  \"", task, "\" 항목이 새로 추가됬습니다. ");
-    getState();
-}
-
-//showing 전용함수 . 출력형태를 객체전체에서 id,task로 수정 -> "1, 그래픽스 공부" , "4, 블로그쓰기" .. 
-function showing(key, value) {
-    var result = [];
-    if (value === "done") {
-        for (property in obj) {
-            var task = obj[property]['task'];
-            var time = parseInt((Date.now() - obj[property]['start']) / 1000);
-            if (obj[property][key] === value)
-                result += "\'" + task + ", " + time + "초\' ";
-        }
-    }
-    else {
-        for (property in obj) {
-            var id = obj[property]['id'];
-            var task = obj[property]['task'];
-            if (obj[property][key] === value)
-                result += "\'" + id + ", " + task + "\' ";
-        }
-    }
-    printMsg(result);
-    start();
-}
-
-//obj[property][id] == index 로 값을 찾아서 state 수정
-//매개변수가 '부족'할때만 에러메세지 update$index$state -> 3개
-//id:index 검색 -> state:value로 수정 ex)setState(1,"done") -> id가 1인 객체의 state를 "done"으로 수정
-function setState(index, value) {
-    if (value === "done") {
-        for (property in obj) {
-            if (obj[property]['id'] === index) {
-                obj[property]['state'] = value;
-                obj[property]['end'] = Date.now();
-            }
-        }
-    }
-    else {
-        for (property in obj) {
-            if (obj[property]['id'] === index) {
-                obj[property]['state'] = value;
-            }
-        }
-    }
-    getState();
-    start();
-}
-
-//state가 done인 상태의 객체를 찾아서 소요시간이 더 짧은 객체만 result에 남겨서 출력
-function getMinTask() {
-    var runtime = Date.now();
-    var result;
-    for (property in obj) {
-        if (obj[property]['state'] === "done" && obj[property]['end'] - obj[property]['start'] < runtime) {
-            runtime = obj[property]['end'] - obj[property]['start'];
-            result = obj[property];
-        }
-    }
-    console.log(result);
-}
-getMinTask();
-rl.close();
 //시작지점 종료입력이 나오기전까지 작동
 function start() {
     rl.question("\ncmd: add showing update exit\n>", function (answer) {
@@ -151,24 +122,24 @@ function start() {
         var check = Number(allcmd.indexOf(cmd)); //allcmd.indexOf(cmd)); //-1일경우 없는 명령어
 
         if (check === -1) {
-            printMsg("wrong input!");
+            util.printMsg("wrong input!");
             start();
         }
         else {
             if (check === 0) { // check = 0
-                add(answer.split("$")[1]);
+                todo.add(answer.split("$")[1]);
                 start();
             }
             else if (cmd === "showing") { // check == 1
-                showing("state", answer.split("$")[1]);
+                todo.showing("state", answer.split("$")[1]);
                 start();
             }
             else if (cmd === "update") { // check == 2
-                setState(Number(answer.split("$")[1]), answer.split("$")[2]);
+                todo.setState(Number(answer.split("$")[1]), answer.split("$")[2]);
                 start();
             }
             else if (cmd === "exit") {
-                printMsg("Do your Best!");
+                util.printMsg("Do your Best!");
                 rl.close();
             }
             else rl.close();
