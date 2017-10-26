cba35500f2b778da28c657288bb73db888d90898
wfe-luigi <kdh3979@naver.com>
2017-10-26 19:38:52 +0900

    소요시간측정 방법 변화
    기존에 객체를 순회하면서 카운트 하던 방식을
    state 변화할때마다 count 를 하는 방식으로 변환

--- todo.js
+++ todo.js
@@ -3,14 +3,15 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-const obj = [{ id: 1, task: "테스트1", state: "doing" }];
+const obj = [];
 
-function Task(task) {
-    this.id = '';
+function Task(id, task) {
+    this.id = id;
     this.task = task;
     this.state = 'todo';
-    this.start = new Date();
+    this.start = null;
     this.end = null;
+    this.runtime = null;
 }
 
 var util = {
@@ -35,22 +36,59 @@ var util = {
 
 var todo = {
 
-    //add 명령어 함수. 배열의 마지막 객체의 id값을 1증가하여 id생성. 매개변수를 task명으로 넣고 현재상태출력
+    //시작지점 종료입력이 나오기전까지 작동
+    run: function () {
+        rl.question("\ncmd: add showing update exit\n>", function (answer) {
+            var allcmd = ["add", "showing", "update", "mintask", "exit"];
+            var cmd = answer.split("$")[0]; //answer = add$test -> answer.split("$")[0] = add
+            var check = Number(allcmd.indexOf(cmd)); //allcmd.indexOf(cmd)); //-1일경우 없는 명령어
+
+            if (check === -1) {
+                util.printMsg("wrong input!");
+                todo.run();
+            }
+            else {
+                if (check === 0) { // check = 0
+                    todo.add(answer.split("$")[1]);
+                    todo.run();
+                }
+                else if (cmd === "showing") { // check == 1
+                    todo.showing("state", answer.split("$")[1]);
+                    todo.run();
+                }
+                else if (cmd === "update") { // check == 2
+                    todo.setState(Number(answer.split("$")[1]), answer.split("$")[2]);
+                    todo.run();
+                }
+                else if (cmd === "mintask") {
+                    todo.getMinTask();
+                    todo.run();
+                }
+                else if (cmd === "exit") {
+                    util.printMsg("Do your Best!");
+                    rl.close();
+                }
+                else rl.close();
+            }
+        });
+    },
+    state: {
+        MinTime: Infinity,
+        MinTask: null,
+        stateCount: { todo: 0, doing: 0, done: 0 }
+    },
+    //add 명령어 함수. 배열의 length를 id로 기록.  매개변수를 task명으로 넣고 현재상태출력
     add: function (task) {
-        var id = 1;
-        id += obj[obj.length - 1]['id'];
-        obj[obj.length] = { "id": id, "task": task, "state": "todo", "start": null };
+        var id = obj.length + 1;
+        this.state.stateCount.todo++;
+        obj[obj.length] = new Task(id, task);
         util.printMsg("id : ", id, "  \"", task, "\" 항목이 새로 추가됬습니다. ");
         this.getState();
     },
 
-    //배열을 순회하며 state count 를 출력
+    //state객체의 stateCount를 가져옴
     getState: function () {
-        var flag = { todo: 0, doing: 0, done: 0 };
-        for (property in obj) {
-            flag[obj[property]['state']]++;
-        }
-        util.printMsg("현재상태 : ", util.objTostr(flag));
+        util.printMsg("현재상태 : ", util.objTostr(this.state.stateCount));
     },
 
     //showing 전용함수 . 출력형태를 객체전체에서 id,task로 수정 -> "1, 그래픽스 공부" , "4, 블로그쓰기" .. 
@@ -59,7 +97,7 @@ var todo = {
         if (value === "done") {
             for (property in obj) {
                 var task = obj[property]['task'];
-                var time = parseInt((Date.now() - obj[property]['start']) / 1000);
+                var time = obj[property]['runtime'];
                 if (obj[property][key] === value)
                     result += "\'" + task + ", " + time + "초\' ";
             }
@@ -73,77 +111,56 @@ var todo = {
             }
         }
         util.printMsg(result);
-        start();
+        this.run();
     },
 
     //obj[property][id] == index 로 값을 찾아서 state 수정
-    //매개변수가 '부족'할때만 에러메세지 update$index$state -> 3개
     //id:index 검색 -> state:value로 수정 ex)setState(1,"done") -> id가 1인 객체의 state를 "done"으로 수정
+    //doing으로 update 일때 start 기록, done일때 end 기록 -> 이전시간보다 작다면 state.mintask에 기록
+    //현재상태를 출력할때마다 객체순회를 하지않기위해 state에 카운트 변화를 기록
     setState: function (index, value) {
+        (this.state.stateCount[value])++;
         if (value === "done") {
             for (property in obj) {
                 if (obj[property]['id'] === index) {
+                    (this.state.stateCount[obj[property]['state']])--;
                     obj[property]['state'] = value;
                     obj[property]['end'] = Date.now();
+                    obj[property]['runtime'] = (obj[property]['end'] - obj[property]['start']) / 1000;
+                    if (obj[property]['runtime'] <= this.state.MinTime) {
+                        this.state.MinTask = obj[property];
+                        this.state.MinTime = obj[property]['runtime'];
+                    }
+                }
+            }
+        }
+        else if (value === "doing") {
+            for (property in obj) {
+                if (obj[property]['id'] === index) {
+                    (this.state.stateCount[obj[property]['state']])--;
+                    obj[property]['state'] = value;
+                    obj[property]['start'] = Date.now();
                 }
             }
         }
         else {
             for (property in obj) {
                 if (obj[property]['id'] === index) {
-                    obj[property]['state'] = value;
+                    (this.state.stateCount[obj[property]['state']])--
                 }
             }
         }
         this.getState();
-        start();
+        this.run();
     },
-    //state가 done인 상태의 객체를 찾아서 소요시간이 더 짧은 객체만 result에 남겨서 출력
+    //update$id$done 명령어 실행때마다 짧은 task를 state객체에 기록했다가 호출
     getMinTask: function () {
-        var runtime = Date.now();
-        var result;
-        for (property in obj) {
-            if (obj[property]['state'] === "done" && obj[property]['end'] - obj[property]['start'] < runtime) {
-                runtime = obj[property]['end'] - obj[property]['start'];
-                result = obj[property];
-            }
-        }
+        var get = this.state.MinTask;
+        var task = get.task;
+        var time = get.runtime;
+        var result = "\'" + task + ", " + time + "초\' ";
         util.printMsg(result);
     }
 
 }
-start();
-
-//시작지점 종료입력이 나오기전까지 작동
-function start() {
-    rl.question("\ncmd: add showing update exit\n>", function (answer) {
-        var allcmd = ["add", "showing", "update", "exit"];
-        var cmd = answer.split("$")[0]; //answer = add$test -> answer.split("$")[0] = add
-        var check = Number(allcmd.indexOf(cmd)); //allcmd.indexOf(cmd)); //-1일경우 없는 명령어
-
-        if (check === -1) {
-            util.printMsg("wrong input!");
-            start();
-        }
-        else {
-            if (check === 0) { // check = 0
-                todo.add(answer.split("$")[1]);
-                start();
-            }
-            else if (cmd === "showing") { // check == 1
-                todo.showing("state", answer.split("$")[1]);
-                start();
-            }
-            else if (cmd === "update") { // check == 2
-                todo.setState(Number(answer.split("$")[1]), answer.split("$")[2]);
-                start();
-            }
-            else if (cmd === "exit") {
-                util.printMsg("Do your Best!");
-                rl.close();
-            }
-            else rl.close();
-        }
-    });
-}
-//start();
+todo.run();
