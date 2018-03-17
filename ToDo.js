fb9d8286bd5bb955f4d952370ec3534be40c976b
HYEON(win) <antaehyeon@live.co.kr>
2018-03-17 16:51:26 +0900

    [Add] 할일관리 애플리케이션의 기본기능이 구현된 코드를 추가한다

--- ToDo.js
+++ ToDo.js
@@ -21,57 +21,132 @@
     > 현재상태 :  todo:1개, doing:1개, done:3개  //변경된 모든 상태가 노출.
 */
 
-function command(data) {
-    // 정규표현식으로 $ 문자 구분
-    
+function command(works, data) {
+    // $ 문자 구분
+    let result = data.split("$");
+    let mode = result[0];
+    let content = result[1];
+    let replaceTargetIndex = "";
     // switch-case
+    switch(mode) {
+        case "add":
+         addWorkToDo(works, content);
+         break;
+        case "show":
+         showWorkList(works, content);
+         break;
+        case "update":
+         replaceTargetIndex = result[1];
+         content = result[2];
+         updateWorkState(works, replaceTargetIndex, content);
+         break;
+    }
 }
 
-function addWorkToDo() {
-    
+function addWorkToDo(works, content) {
     // Obj를 새로 생성해서
-    // 배열에 추가
 
     // ID 값 계산
     // 배열의 Index 를 Id 로 체크하면 될 듯 함
 
+    // 배열에 추가
+
+    const forIndexMatchingData = 1;
+
+    let idCount = works.length + forIndexMatchingData;
+
+    var workObj = {
+        id: idCount,
+        content: content,
+        state: "todo"
+    };
+
+    works.push(workObj);
+
     // 출력
-    printCurrentState();
+    log("id: " + idCount + ", " + "\"" + content + "\" 항목이 새로 추가됐습니다.");
+    printCurrentState(works);
 }
 
-function showWorkList() {
+function showWorkList(works, state) {
     // todo인지, doing인지, done인지
     // 배열을 순차적으로 탐색하면서
     // state 값을 판별해서 맞는 state 만 출력
 
     // ID, 내용 순으로 출력
+
+    let content = "";
+    let result = "";
+    let mode = (state === "todo") ? "TO DO" : (state === "doing") ? "DOING" : "DONE";
+
+    log(" << " + mode + " 상태인 목록을 출력합니다 >> ");
+
+    for(let i=0; i<works.length; i++) {
+        if (works[i].state == state) {
+            content = "\"" + works[i].id + ". " + works[i].content + "\"";
+            result += content;
+        }
+    }
+
+    result = (result === "") ? "해당 상태인 목록이 없습니다" : result;
+
+    log(result);
 }
 
-function updateWorkState() {
+function updateWorkState(works, id, content) {
     // 해당 ID 값을 상태로 바꿔주는 기능으로 구현
     // ID에 맞게 해당 state를 수정 후
+
+    let forIndexMatchingData = 1;
+    let index = id - forIndexMatchingData;
+
+    works[index].state = content;
     
-    printCurrentState();
+    printCurrentState(works);
 }
 
-function printCurrentState() {
+function printCurrentState(works) {
     // 현재상태 출력
 
     // 배열을 순차적으로 탐색하면서
     // state의 값을 카운트 하며 
     // todo, doing, done 갯수 출력
+
+    let todoCount = 0;
+    let doingCount = 0;
+    let doneCount = 0;
+
+    let state = "";
+
+    for (let i=0; i < works.length; i++) {
+        state = works[0].state;
+        if (state === "todo") {
+            todoCount++;
+        } else if (state === "doing") {
+            doingCount++;
+        } else {
+            doneCount++;
+        }
+    }
+
+    log("현재상태 : " + "todo " + todoCount + "개, doing : " + doingCount + "개, done : " + doneCount + "개");
 }
 
-(function () {
-    var workObj = {
-        id: null,
-        content: "",
-        state: ""
-    }
+function log(data) {
+    console.log(data);
+}
+
+var run = function () {
     var works = [];
 
-    command("add$자바스크립트 공부하기");
-    command("show$doing");
-    command("show$done");
-    command("update$3$done");    
-})
+    command(works, "add$자바스크립트 공부하기");
+    command(works, "show$todo");
+    command(works, "show$doing");
+    command(works, "show$done");
+    command(works, "update$1$done");
+    
+    command(works, "show$todo");
+    command(works, "show$done");
+};
+
+run();
