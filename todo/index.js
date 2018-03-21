1bde44e782527099866c74277a8349fcfeb876f3
chany <lyhy0310@gmail.com>
2018-03-21 12:10:19 +0900

    add fn check 여러개 확인

--- todo/index.js
+++ todo/index.js
@@ -14,39 +14,37 @@ const command = (function(){
         Doing: 'doing',
         Done: 'done',
     }
-
-
+    const todos = {};
+    let id = 0;
+    const stateCounter = {
+        todo: 0,
+        doing: 0,
+        done: 0,
+    }
     const controller = function(order){
-        const todos = {};
-        let id = 0;
 
-        const compileOrder = function(target){
-            
-            const [first,second,last] = target.split('$');
-            
+        const compileOrder = function(order){
+
+            const [first,second,last] = order.split('$');
+
             const isActions = actions => !!(actions === Actions.Add| Actions.Show| Actions.Update )
-            
-            if(!isActions(first)) throw new Error('명령어는 add show update 중 하나입니다. ex) add$공부')
-            
-            const actions = first;
 
-            const addTask = task => {
-                id +=1
+            if(!isActions(first)) throw new Error('명령어는 add show update 중 하나입니다. ex) add$공부')
+            const actions = first;
+            id+=1;
+            const addTask = (task) => {
                 todos[id] = {
                     task,
                     state: State.Todo  
                 }
+               
                 let idList = Object.keys(todos);
                 let states = stateCheck()
                 console.log(`id: ${idList[idList.length-1]} ${todos[id].task} 항목이 추가 되었습니다 `)
                 console.log(`현재상태 : todo: ${states.todo}개 doing: ${states.doing}개 todo: ${states.done}개`)
+                return 
             }
             const stateCheck = () => {
-                const stateCounter = {
-                    todo: 0,
-                    doing: 0,
-                    done: 0,
-                }
                 for(key in todos){
                     switch(todos[key].state){
                         case State.Todo: {
@@ -70,11 +68,16 @@ const command = (function(){
                 case Actions.Add: return addTask(second)
             }
         }
-    
         return compileOrder(order);
 
     }
     return controller
 })();
 
+
 command('add$자바스크립트공부');
+command('add$ES6공부');
+command('add$TIL 블로그 글 쓰기');
+command('add$이전에 짠 것들 Refactoring하기');
+command('add$Express 공부');
+command('add$React 공부');
