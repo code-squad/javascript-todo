cc7e50b749fdce8a753c4044ec561b9d8841fbfb
chany <lyhy0310@gmail.com>
2018-03-23 22:23:04 +0900

    기본 기능 구현 함수-객체 형태로 다시 수정+ 객체 key값으로 탐색하는 것을 활용하는 방안으로 수정

--- todo/index.js
+++ todo/index.js
@@ -1,206 +1,130 @@
-// reg = new RegExp("^[\\w\\-]+(\\.[\\w\\-_]+)*@[\\w\\-]+(\\.[\\w\\-]+)*(\\.[a-zA-Z]{2,3})$", "gi");
-// var re = new RegExp("ab+c");
-// 정규표현식 없이 .split() 구현 or 정규표현식! 
-// let result = target.match(r)
-// JS에서 enum바로 true False로 체크해주는 거 있으면 좋겠다. 못 찾은 걸 수도 
 const enumCheck = (checkData, enumData) => {
-    return !!Object.values(checkData).filter(item => item === enumData).length;
+    return !!enumData[checkData]
 }
 const notNumber = number => isNaN(Number(number))
 
 const command = (function () {
-    const Actions = {
-        Add: 'add',
-        Show: 'show',
-        Update: 'update',
-    }
-    const State = {
-        Todo: 'todo',
-        Doing: 'doing',
-        Done: 'done',
-    }
-    const todos = {};
-    let id = 0;
-
-    const printType = {
-        Filter: "filter",
-        Add: "add",
-    }
     const errMsg = {
-        addWrongState: 'to do 볼 수 있는 상태는  todo, dpoing, done 중 하나입니다. ex) add$공부',
-        updateWrongState: 'update할 수 있는 상태는  todo, dpoing, update 중 하나입니다. ex) update$1$done',
+        notActions: `명령어는 add show done 중 하나입니다. ex) show$todo`,
+        wrongState: 'state는  todo, dpoing, done 중 하나입니다',
         notNumber: 'id 값은 숫자여야 합니다.  ex) update$1$done',
         notHaveThisId: '해당 id 값이 없습니다'
     }
-    const stateCounter = {
+    const enums = {}
+    enums.actions = {
+        add: 'add',
+        show: 'show',
+        update: 'update',
+    }
+    enums.state = {
+        todo: 'todo',
+        doing: 'doing',
+        done: 'done',
+    }
+    const todos = {};
+    todos.id = 0;
+    todos.stateCounter = {
         todo: 0,
         doing: 0,
         done: 0,
-    }
-    const controller = function (order) {
-
-        const compileOrder = function (order) {
-
-            const [first, second, last] = order.split('$');
-            const actions = first;
-
-            if (!enumCheck(Actions, actions)) throw new Error('명령어는 add show done 중 하나입니다. ex) show$todo')
-
-            const stateUpdate = (before, update) => {
-                stateCounter[before] -= 1
-                stateCounter[update] += 1
-            }
-            const stateAddtoDo = () => {
-                stateCounter.todo += 1
-            }
-            const stateInit = () => {
+    };
+    const todoController = {
+        compileOrder(order) {
+            const [actions, target, update] = order.split('$');
+            if (!enumCheck(actions, enums.actions)) throw new Error(errMsg.notActions)
+            this.todoActions[actions](target, update);
+        },
+        todoActions: {
+            add(todo) {
+                todos.id += 1
+                todos[todos.id] = {
+                    id: todos.id,
+                    todo,
+                    state: enums.state.todo,
+                }
+                todos.stateCounter.todo += 1;
+                this.printAdded();
+                this.printState();
+            },
+            show(state) {
+                if (!enumCheck(state, enums.state)) throw new Error(errMsg.wrongState)
+                let tasks = this.getOnlyTaskData(todos);
+                const filtered = Object.values(tasks).filter(task => task.state === state);
+                this.printSameState(filtered, state)
+            },
+            update(id, state) {
+                //ErrorCheck
+                if (!enumCheck(state, enums.state)) throw new Error(errMsg.wrongState)
+                if (notNumber(id)) throw Error(errMsg.notNumber)
+                if (!this.validIdCheck(id)) throw Error(errMsg.notHaveThisId)
+                // update
+                let beforeState = todos[id].state;
+                todos[id].state = state;
+                todos.stateCounter[beforeState] -= 1;
+                todos.stateCounter[state] += 1;
+                this.stateCount(todos);
+            },
+            validIdCheck(id) {
+                let tasks = this.getOnlyTaskData(todos);
+                return !!tasks[id];
+            },
+            printState() {
+                console.log(`현재상태 : todo: ${todos.stateCounter.todo}개 doing: ${todos.stateCounter.doing}개 done: ${todos.stateCounter.done}개`)
+            },
+            printAdded() {
+                console.log(`id: ${todos.id} ${todos[todos.id].todo} 항목이 추가 되었습니다`)
+            },
+            printSameState(filterList, state) {
+                console.log(`원하는 상태목록은 :D ${state}`)
+                filterList.forEach(todo => {
+                    console.log(`${todo.id}, ${todo.todo}`)
+                })
+            },
+            initState() {
+                const stateCounter = todos.stateCounter;
                 stateCounter.todo = 0;
                 stateCounter.doing = 0;
                 stateCounter.done = 0;
-            }
+            },
+            getOnlyTaskData(todos) {
+                let copy = Object.assign({}, todos)
+                delete copy.id;
+                delete copy.stateCounter;
+                return copy;
+            },
+            stateCount(todos) {
+                this.initState();
+                let tasks = this.getOnlyTaskData(todos);
 
-            const stateCheck = () => {
-                stateInit();
-                for (key in todos) {
-                    let state = todos[key].state
-                    switch (state) {
-                        case State.Todo:
-                            stateCounter[state] += 1;
-                            break;
-                        case State.Doing:
-                            stateCounter[state] += 1;
-                            break;
-                        case State.Done:
-                            stateCounter[state] += 1;
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                return stateCounter;
-            }
-
-            const addTodo = (todo) => {
-                id += 1;
-                todos[id] = {
-                    id,
-                    todo,
-                    state: State.Todo
-                }
-                const printData = {
-                    id: todos[id].id,
-                    todo: todos[id].todo,
-                }
-                stateAddtoDo();
-                return printsByType(printData, printType.Add);
-            }
-
-            const statePrint = () => {
-                console.log(`현재상태 : todo: ${stateCounter.todo}개 doing: ${stateCounter.doing}개 done: ${stateCounter.done}개`)
-            }
-            const printsByType = (data, type) => {
-                const filterPrint = data => data.forEach(todo => {
-                    console.log(`${todo.id}, ${todo.todo}`)
-                })
-                const addPrint = data => {
-                    console.log(`id: ${data.id} ${data.todo} 항목이 추가 되었습니다 `)
-
-                    statePrint();
-                }
-                switch (type) {
-                    case printType.Add:
-                        return addPrint(data);
-                    case printType.Filter:
-                        return filterPrint(data);
-                }
-
-            }
-            const enumCheckTodoState = (data, errMsg) => {
-                if (!enumCheck(State, data)) throw new Error(errMsg)
-                return;
-            }
-            const showTodo = filter => {
-                enumCheckTodoState(filter, errMsg.addWrongState);
-                const filteredTodos = Object.values(todos).filter(el => el.state === filter);
-                printsByType(filteredTodos, printType.Filter);
-            }
-            const validIdCheck = id => {
-                const idList = Object.keys(todos);
-                return idList.filter(el => el === id).length !== 0
-            }
-
-
-            const checkSpendTime = (id) => {
-                let startTime = todos[id].startTime.getTime();
-                let doneTime = todos[id].doneTime.getTime();
-                let spendTime = doneTime - startTime;
-                console.log('spenTime', spendTime);
-            }
-
-            const updateTimer = (id, state) => {
-                console.log('id', id, 'state', state);
-                switch (state) {
-                    case State.Doing:
-                        return todos[id].startTime = new Date();
-
-                    case State.Done:
-                        {
-                            todos[id].doneTime = new Date();
-                            return todos[id].spendTimes = checkSpendTime(id);;
-                        }
-
-                }
-
-            }
-            const printTime = (id, state) => {
-                switch (state) {
-                    case State.Doing:
-                        return todos[id].startTime
-                    case State.Done:
-                        return todos[id].spendTimes
-                    default:
-                        break;
-                }
-            }
-            const updateTodo = (id, state) => {
-                let beforeState = todos[id].state;
-                enumCheckTodoState(state, errMsg.updateWrongState);
-                if (notNumber(id)) throw Error(errMsg.notNumber)
-                if (!validIdCheck(id)) throw Error(errMsg.notHaveThisId)
-                todos[id].state = state;
-                stateUpdate(beforeState, state);
-                // updateTimer(id, state);
-                // console.log('printTime', printTime(id, state));
-                return statePrint();
-            }
-            switch (actions) {
-                case Actions.Add:
-                    return addTodo(second)
-                case Actions.Show:
-                    return showTodo(second)
-                case Actions.Update:
-                    return updateTodo(second, last);
-            }
+                const stateCounter = todos.stateCounter
+                Object.values(tasks).forEach(
+                    todo => stateCounter[todo.state] += 1
+                );
+            },
         }
-
-        return compileOrder(order);
     }
-    return controller
+    return todoController.compileOrder.bind(todoController);
 })();
+
 
 
 command('add$자바스크립트공부');
 command('add$ES6공부');
-
 command('add$TIL 블로그 글 쓰기');
 command('add$이전에 짠 것들 Refactoring하기');
 command('add$Express 공부');
-command('add$React 공부');
+
 command('show$todo');
 
-command('update$1$todo');
-command('update$1$doing');
-command('update$1$done');
+command('update$10$doing');
 
-// command('update$$done');
-// command('show$done');
+
+// command('update$1$doing');
+// command('update$1$done');
+
+
+// ErroCheck
+
+// command('update$10$doing');
+// command('shows$todos');
+// command('show$todos');
