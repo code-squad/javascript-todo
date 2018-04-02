5026020770cbb8d6175dcf843c93b56261e6ad70
youngdo212 <nsa06031@gmail.com>
2018-04-02 18:02:35 +0900

    Implement todoList using Object literal
    todolist를 객체리터럴로 구현

--- 180329_todolist.js
+++ 180329_todolist.js
@@ -1,104 +1,107 @@
-let thingsList = [];
-let category = {
-  "todo" : [],
-  "doing": [],
-  "done": []
+const todoList = {
+  thingsList : [],
+
+  category : {
+    "todo" : [],
+    "doing": [],
+    "done": []
+  },
+
+  id : 0,
+
+  Thing: function(name){
+    this.id = ++todoList.id;
+    this.name = name;
+    this.status = "todo";
+    this.startTime = 0;
+    this.endTime = 0;
+    this.setStatus = function(status){
+      if(status === "doing") this.startTime = (new Date()).getMilliseconds();
+      if(status === "done") this.endTime = (new Date()).getMilliseconds();
+      this.status = status;
+    }
+  },
+
+  command: function(order){
+    let subString = order.split(/\$/);
+    switch(subString[0]){
+      case "add":
+        this.addThing(subString[1]);
+        break;
+      case "show":
+        this.showThing(subString[1]);
+        break;
+      case "update":
+        this.updateThing(subString[1], subString[2]);
+        break;
+      default : throw "올바른 값을 입력하세요";
+    }
+  },
+
+  addThing: function(name){
+    let thing = new this.Thing(name);
+    this.pushId(thing["id"], "todo");  
+    this.thingsList.push(thing);
+    console.log(`id: ${thing["id"]}, "${thing["name"]}" 항목이 새로 추가되었습니다`);
+    this.printStatus();
+  },
+
+  pushId: function(id, status){
+    this.category[status].push(id);
+  },
+
+  showThing: function(status){
+    let result = [];
+    this.category[status].forEach(id=>{
+      let thing = this.searchThing(id);
+      let subStr = '';
+      if(status === "done") subStr = `(소요시간: ${this.measureTime(thing)})`;
+      result.push(`"${id}, ${thing["name"]}${subStr}"`);
+    });
+    console.log(result.join());
+  },
+
+  searchThing: function(id){
+    for(let i = 0; i < this.thingsList.length; i++){
+      if(this.thingsList[i]["id"] === id) return this.thingsList[i];
+    }
+    throw "값을 찾지 못했습니다";
+  },
+
+  measureTime: function(thing){
+    let result;
+    result = thing["endTime"] - thing["startTime"];
+    return result;
+  },
+
+  updateThing: function(id, status){
+    id = +id;
+    let thing = this.searchThing(id);
+    this.deleteId(id, thing["status"]);
+    thing.setStatus(status);
+    this.pushId(id, thing["status"]);
+    this.printStatus();
+  },
+
+  deleteId: function(id, status){
+    let index = this.category[status].indexOf(id);
+    this.category[status].splice(index, 1);
+  },
+
+  printStatus: function(){
+    let numTodo = this.category["todo"].length;
+    let numDoing = this.category["doing"].length;
+    let numDone = this.category["done"].length;
+    console.log(`현재상태 :   todo: ${numTodo}개, doing: ${numDoing}개, done: ${numDone}개`);
+  },
 }
 
-Thing.prototype.id = 0;
-
-function Thing(name){
-  this.id = ++Thing.prototype.id;
-  this.name = name;
-  this.status = "todo";
-  this.startTime = 0;
-  this.endTime = 0;
-  this.setStatus = function(status){
-    if(status === "doing") this.startTime = (new Date()).getMilliseconds();
-    if(status === "done") this.endTime = (new Date()).getMilliseconds();
-    this.status = status;
-  }
-}
-
-function command(order){
-  let subString = order.split(/\$/);
-  switch(subString[0]){
-    case "add":
-      addThing(subString[1]);
-      break;
-    case "show":
-      showThing(subString[1]);
-      break;
-    case "update":
-      updateThing(subString[1], subString[2]);
-      break;
-    default : throw "올바른 값을 입력하세요";
-  }
-}
-
-function addThing(name){
-  let thing = new Thing(name);
-  pushId(thing["id"], "todo");  
-  thingsList.push(thing);
-  console.log(`id: ${thing["id"]}, "${thing["name"]}" 항목이 새로 추가되었습니다`);
-  printStatus();
-}
-
-function pushId(id, status){
-  category[status].push(id);
-}
-
-function showThing(status){
-  let result = [];
-  category[status].forEach(id=>{
-    let thing = searchThing(id);
-    let subStr = '';
-    if(status === "done") subStr = `(소요시간: ${measureTime(thing)})`;
-    result.push(`"${id}, ${thing["name"]}${subStr}"`);
-  });
-  console.log(result.join());
-}
-
-function searchThing(id){
-  for(let i = 0; i < thingsList.length; i++){
-    if(thingsList[i]["id"] === id) return thingsList[i];
-  }
-  throw "값을 찾지 못했습니다";
-}
-
-function measureTime(thing){
-  let result;
-  result = thing["endTime"] - thing["startTime"];
-  return result;
-}
-
-function updateThing(id, status){
-  id = +id;
-  let thing = searchThing(id);
-  deleteId(id, thing["status"]);
-  thing.setStatus(status);
-  pushId(id, thing["status"]);
-  printStatus();
-}
-
-function deleteId(id, status){
-  let index = category[status].indexOf(id);
-  category[status].splice(index, 1);
-}
-
-function printStatus(){
-  let numTodo = category["todo"].length;
-  let numDoing = category["doing"].length;
-  let numDone = category["done"].length;
-  console.log(`현재상태 :   todo: ${numTodo}개, doing: ${numDoing}개, done: ${numDone}개`);
-}
-
-command("add$자전거 타기");
-command("update$1$doing");
-command("show$todo");
-command("add$독서하기");
-command("update$2$doing");
-command("update$2$done");
-command("show$todo");
-command("update$1$done");
-command("show$done");
+todoList.command("add$자전거 타기");
+todoList.command("update$1$doing");
+todoList.command("show$todo");
+todoList.command("add$독서하기");
+todoList.command("update$2$doing");
+todoList.command("update$2$done");
+todoList.command("show$done");
+todoList.command("update$1$done");
+todoList.command("show$done");
