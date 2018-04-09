59d562e3fe5b76b1327d0e0e9dff86b39f78a343
youngdo212 <nsa06031@gmail.com>
2018-04-09 20:21:45 +0900

    refactor: TodoList를 class로 변경

--- 180329_todolist.js
+++ 180329_todolist.js
@@ -14,78 +14,80 @@ class Thing{
   }
 }
 
-const TodoList = {
-  Data : {
-    lastId : 0,
-    ThingsDict : {}
-  },
-
-  command: function(order){
+class TodoList{
+  constructor(){
+    this.Data = {
+      lastId : 0,
+      ThingsDict : {}
+    };
+  }
+  
+  command(order){
     const fns = {
       add: this.addThing,
       show: this.showThing,
       update: this.updateThing
     }
     const [flag, ...details] = order.split(/\$/);
-    // ES6 문법인 distructuring과 rest parameter를 이용해 split된 값을 받습니다.    
+    // ES6 문법인 destructuring과 rest parameter를 이용해 split된 값을 받습니다.    
     fns[flag].call(this, ...details);
     // fns객체에서 flag 키에 맞는 함수를 호출합니다. 함수 안의 함수가 호출되면서 실행컨텍스트가 바뀌고 thisBinding은 디폴트값인 global이 됩니다.
     // 호출되는 함수의 컨택스트가 참조하는 객체가 todoList객체이어야 하므로 this를 인자로 넘겨줘야 합니다.
-  },
+  }
 
-  addThing: function(name){
+  addThing(name){
     const thing = new Thing(name);
     const id = this.getId();
     this.setId(id, thing);
     this.printAddedThing(id, thing);
     this.printStatus();
-  },
+  }
 
   printAddedThing(id,thing){
     console.log(`id: ${id}, "${thing["name"]}" 항목이 새로 추가되었습니다`);
-  },
+  }
 
-  getId: function(){
+  getId(){
     return ++this.Data.lastId;
-  },
+  }
 
-  setId: function(id, thing){
+  setId(id, thing){
     const newThing = {};
     newThing[id] = thing;
     this.Data.ThingsDict = getModifiedObject({source: this.Data.ThingsDict, targetProp: id, initVal: newThing});
-  },
+  }
 
-  showThing: function(status){
+  showThing(status){
     let sameStatusGroup = Object.keys(this.Data.ThingsDict);
     sameStatusGroup = sameStatusGroup.filter(id => this.searchThing(id).status === status);
     sameStatusGroup = sameStatusGroup.reduce((acc, id) => [...acc, this.makeSentence(id)], []);
     console.log(sameStatusGroup.join());
-  },
+  }
 
-  searchThing: function(id){
+  searchThing(id){
     return this.Data.ThingsDict[id];
-  },
+  }
 
-  makeSentence: function(id){
+  makeSentence(id){
     const thing = this.searchThing(id);
     const subStr = thing.status === "done" ? `(소요시간: ${this.measureTime(thing)})` : '';
     return `"${id}, ${thing["name"]}${subStr}"`
-  },
+  }
 
-  measureTime: function({endTime, startTime}){
+  measureTime({endTime, startTime}){
     let elapsedTime;
     elapsedTime = endTime - startTime;
     return elapsedTime;
-  },
+  }
 
-  updateThing: function(id, status){
+  updateThing(id, status){
     let thing = getModifiedObject({source: this.searchThing(id)});
     Thing.prototype.setStatus.call(thing, status);
     this.setId(id, thing);
     this.printStatus();
-  },
+  }
 
-  printStatus: function(){
+  printStatus(){
     const NumStatus = {
       todo: 0,
       doing: 0,
@@ -98,12 +100,14 @@ const TodoList = {
   }
 }
 
-TodoList.command("add$자전거 타기");
-TodoList.command("update$1$doing");
-TodoList.command("show$doing");
-TodoList.command("add$독서하기");
-TodoList.command("update$2$doing");
-TodoList.command("update$2$done");
-TodoList.command("show$done");
-TodoList.command("update$1$done");
-TodoList.command("show$done");
+const newTodoList = new TodoList();
+
+newTodoList.command("add$자전거 타기");
+newTodoList.command("update$1$doing");
+newTodoList.command("show$doing");
+newTodoList.command("add$독서하기");
+newTodoList.command("update$2$doing");
+newTodoList.command("update$2$done");
+newTodoList.command("show$done");
+newTodoList.command("update$1$done");
+newTodoList.command("show$done");
