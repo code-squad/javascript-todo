9350104c8f9f28fd41c828b4a0d8c797a347d36e
feanar729 <feanar729@gmail.com>
2018-05-25 16:25:27 +0900

    update & refactoring: update시 중복된 데이터를 배열에 넣는 버그(수정), 데이터 구조 별도의 객체 파일로 수정, 각 기능 데이터 구조에 맞게 refactoring 진행

--- todo.js
+++ todo.js
@@ -1,93 +1,101 @@
+const worklist = require("./workList.js");
+
 const noticeMSG = {
   notSimbol: "해당 글자는 입력 할수 없는 기호입니다.",
   addTodo: " 항목이 추가 되었습니다.",
 };
 
-const work = {
-  todo: [],
-  doing: ["3: 그래픽스 공부", "4: 알고리즘", "5: 수학공부", "6: 블로그쓰기", "7: 컴퓨터공학", "8: 데이터베이스"],
-  done: [],
-  timeList: []
-};
 
-// 시간 계산/데이터를 저장하게 하는 함수
-/*
-  구상:
-  - 시간을 어떻게 계산하게 할것인가?
-  - 계산된 시간을 어떻게 저장하게 하고 넘겨줄 것인가?
-  - 넘겨받은 시간을 어떻게 계산하고 출력할 것인가?
-*/
-
-function checkTime() {
+// 시간 함수
+function checkTime(commandTodo) {
+  const splitTodo = commandTodo.split("$");
   const time = new Date();
   const getMSTime = time.getMilliseconds();
+  let calTime = 0;
 
-  return getMSTime;
+  if (splitTodo[1] === "doing") {
+    return getMSTime;
+  } else if (splitTodo[1] === "done") {
+    worklist.doing.map(doingData => {
+      if (Number(splitTodo[0]) === doingData.id) {
+        calTime = Math.abs(getMSTime - doingData.time);
+      }
+    });
+    return calTime;
+  }
 }
 
 // todo 추가
 function addTodo(commandTodo) {
-  const inputTodo = work.todo.length + 1 + ": " + commandTodo;
-  work.todo.push(inputTodo);
-
-  const todoData = work.todo.reduce((allAddData, addTodo) => {
-    return addTodo;
+  // [] id값이 private 한 값으로 하도록 설계(추후)
+  const inputTodo = { id: worklist.todo.length, name: commandTodo, time: 0 };
+  worklist.todo.push(inputTodo);
+  
+  const todoData = worklist.todo.reduce((allAddData, addTodo) => {
+    return addTodo.name;
   });
-  const addTodoMSG = "todo 목록에 " + todoData + '"' + noticeMSG.addTodo;
+  
+  const addTodoMSG = "todo 목록에 " + todoData + noticeMSG.addTodo;
   console.log(addTodoMSG);
-
+  
   showStatus();
 }
 
 // todo 리스트 출력
 function showTask(commandTodo) {
-  work[commandTodo].forEach(doingData => {
-    const list = commandTodo + "목록에 id " + doingData;
+  worklist[commandTodo].forEach(workData => {
+    const message = commandTodo + "목록에 id " + workData.id + ": " + workData.name;
+    const doneMSG = message + " [ 소요시간: " + workData.time + "/ms ]";
+
     switch (commandTodo) {
       case "todo":
-        console.log(list);
+        console.log(message);
         return;
       case "doing":
-        console.log(list);
+        console.log(message);
         return;
       case "done":
-        let timeData = work.timeList.reduce((acc, word) => { return word.split(":"); });
-        let preMSTime = checkTime();
-        console.log(list + "", "[ 완료시간:", preMSTime - timeData[0] + "/ms ]");
+        console.log(doneMSG);
         return;
     }
   });
 }
 
-// todo 데이터 update 입/출력 함수
-function updateTask(idxTodo, modifyTodo) {
-  for (let key in work) {
-    work[key].forEach(todoData => {
-      const split = todoData.split(/\:/);
-      if (split[0] === idxTodo) {
-        let find = work[key].indexOf(split[1])
-        work[modifyTodo].push(split[0] + ":" + split[1]);
-        work[key].splice(find - 1, 1);
-      }
-    });
-  }
 
+// todo 데이터 update 입/출력 함수
+function updateTask(commandTodo) {
+  const splitCommand = commandTodo.split("$");
+  const idxTodo = Number(splitCommand[0]);
+  const modifyTodo = splitCommand[1];
+  const checkingTime = checkTime(commandTodo);
+
+  Object.values(worklist).map(workData => {
+    for (let key in workData) {
+      if (workData[key].id === idxTodo) {
+        worklist[modifyTodo].push({id: workData[key].id, name: workData[key].name, time: checkingTime });
+        workData.splice(key, 1);
+      }
+    }
+  });
   showStatus();
 }
 
+
 // 현재 todo list 출력 함수
 function showStatus() {
-  const todoList = work["todo"].length;
-  const doingList = work["doing"].length;
-  const doneList = work["done"].length;
+  const todoList = worklist["todo"].length;
+  const doingList = worklist["doing"].length;
+  const doneList = worklist["done"].length;
   console.log(`현재상태 : todo: ${todoList}개, doing: ${doingList}개, done: ${doneList}개`);
 }
 
 // 명령이 입력 / 구분 함수
 function inputCommand(inputWord) {
-  if (inputWord.indexOf("$") === -1) {
-    console.log(noticeMSG.notSimbol);
-    return;
+  for (let i = 0; i < inputWord.length; i++) {
+    if (inputWord.indexOf("$") === -1) {
+      console.log(noticeMSG.notSimbol);
+      return;
+    }
   }
   const splitCommand = inputWord.split(/\$/);
 
@@ -97,20 +105,26 @@ function inputCommand(inputWord) {
     case "show":
       return showTask(splitCommand[1]);
     case "update":
-      let time = checkTime();
-      work.timeList.push(time + ":ms");
-      return updateTask(splitCommand[1], splitCommand[2]);
+      return updateTask(splitCommand[1] + "$" + splitCommand[2]);
   }
 }
 
 inputCommand("add$자바스크립트 공부하기");
-// // inputCommand("show!가짜!"); // 해당 글자는 입력 할수 없는 기호입니다.
+// inputCommand("show!가짜!"); // 해당 글자는 입력 할수 없는 기호입니다.
 inputCommand("add$영어단어외우기");
 
-inputCommand("show$doing");
-inputCommand("show$todo");
+// inputCommand("show$todo");
+// inputCommand("show$doing");
+// inputCommand("show$done");
 
+// inputCommand("update$6$done");
+inputCommand("update$1$doing");
 inputCommand("update$1$done");
-inputCommand("update$5$done");
-inputCommand("update$4$done");
+
+inputCommand("show$doing");
 inputCommand("show$done");
+inputCommand("update$done");
+
+// console.log(worklist.todo);
+// console.log(worklist.doing);
+// console.log(worklist.done);
---
+++ workList.js
@@ -0,0 +1,45 @@
+const worklist = {
+  todo: [
+    {
+      id: 0,
+      name: "testData",
+      time: 2 
+    }
+  ],
+  doing: [
+    {
+      id: 3,
+      name: "그래픽스 공부",
+      time: 321
+    }, 
+    {
+      id: 4,
+      name: "알고리즘",
+      time: 12
+    },
+    {
+      id: 5,
+      name: "수학공부",
+      time: 512
+    },    
+    {
+      id: 6,
+      name: "블로그쓰기",
+      time: 961
+    },
+  ],
+  done: [
+    {
+      id: 7,
+      name: "컴퓨터공학",
+      time: 256
+    },
+    {
+      id: 8,
+      name: "데이터베이스",
+      time: 11
+    } 
+  ],
+};
+
+module.exports = worklist;
