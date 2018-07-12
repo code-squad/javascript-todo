90bd551789c03b290914b85de87882d86fd8d2b5
juchan96 <ghkdwncks96@gmail.com>
2018-07-12 16:51:22 +0900

    step 1 완료

--- todo.js
+++ todo.js
@@ -1,51 +1,56 @@
-const currentStateList = {
-  todo: {
-    idList: [],
-    taskName: [],
-    currentState: 0
-  },
-  doing: {
-    idList: [],
-    taskName: [],
-    currentState: 0
-  },
-  done: {
-    idList: [],
-    taskName: [],
-    currentState: 0
+const currentStateData = {
+  item: [],
+  currentState: {
+    todo: 0,
+    doing: 0,
+    done: 0
   },
 
   command(message) {
-    const [accessfn, accessmessage] = message.split("$");
-    if (accessfn === "add") return this.add(accessmessage)
-    else if (accessfn === "show") return this.show(this[accessmessage])
-    else if (accessfn === "update") return this.update(accessmessage)
+    const [accessfn, accessMsg, accessMsg2] = message.split("$");
+    if (accessfn === "add") return this.addData(accessMsg);
+    else if (accessfn === "show") return this.showData(accessMsg);
+    else if (accessfn === "update") return this.updateData(accessMsg2, parseInt(accessMsg));
   },
 
-  add(taskName) {
-    this.todo.taskName.push(taskName);
-    let taskNameLength = this.todo.taskName.length;
-    this.todo.idList.push(taskNameLength);
-    this.todo.currentState = taskNameLength;
-    let showIdList = this.todo.idList.slice(-1);
-    let showTaskName = this.todo.taskName.slice(-1);
-    return `id: ${showIdList}, "${showTaskName}" 항목이 새로 추가됐습니다.`;
+  addData(taskName) {
+    const startingIDVal = 1;
+    const taskNameLength = this.item.length + startingIDVal;
+    const dataSample = {
+      id: taskNameLength,
+      taskName: taskName,
+      state: "todo",
+    };
+    const showId = dataSample.id;
+    const showTaskName = dataSample.taskName;
+    this.item.push(dataSample);
+    this.currentState.todo++
+    console.log("현재 상태:", this.currentState);
+    return `id: ${showId}, "${showTaskName}" 항목이 새로 추가됐습니다.`;
   },
 
-  show(state) {
+  showData(state) {
     const showMessage = [];
-    state.taskName.forEach((element, index) => {
-      showMessage.push(`ID: ${state.idList[index]}, taskName: ${element}`)
+    this.item.forEach((element, index) => {
+      if (element.state === state) showMessage.push(element);
     });
     return showMessage;
   },
+  //item 리스트(배열)에서 특정 id값을 찾아 해당하는 데이터의 state를 바꿔주는 함수.
+  updateData(state, id) {
+    this.item.forEach((element) => {
+      if (element.id === id) {
+        this.currentState[element.state]--;
+        this.currentState[state]++;
+        element.state = state;
+      };
+    });
+    console.log("현재 상태:", this.currentState);
+    return this.item;
+  },
+};
 
-  update() {
-    return `todo: ${this.todo.currentState}, doing: ${this.doing.currentState}, done: ${this.done.currentState}`
-  }
-}
-
-// console.log(currentStateList.command("add$자바스크립트 공부하기"));
-// console.log(currentStateList.command("add$자바 공부하기"));
-console.log(currentStateList.command("show$doing"));
-// console.log(currentStateList.command("update$done"));
+console.log(currentStateData.command("add$자바스크립트 공부하기"));
+console.log(currentStateData.command("add$자바 공부하기"));
+console.log(currentStateData.command("show$todo"));
+console.log(currentStateData.command("update$1$done"));
