70e469b0565ea6101ee552c10d0b25a1a60dab77
juchan96 <ghkdwncks96@gmail.com>
2018-07-17 17:08:12 +0900

    refactor: 소요시간을 계산해주는 getLeadTime 함수, updateData에 state인자 값이 올바른 값이 할당 됐는지 확인해주는 checkState함수 추가.

--- step2.js
+++ step2.js
@@ -20,34 +20,65 @@ const currentStateData = {
       id: taskNameLength,
       taskName: taskName,
       state: "todo",
+      leadTime: 0
     };
     const showId = dataSample.id;
     const showTaskName = dataSample.taskName;
     this.item.push(dataSample);
-    this.currentState.todo++
+    this.currentState.todo++;
     console.log("현재 상태:", this.currentState);
     return `id: ${showId}, "${showTaskName}" 항목이 새로 추가됐습니다.`;
   },
   //원하는 item데이터(todo, doing, done) 보여주는 함수.
   showData(state) {
-    const showMessage = this.item.filter(element => element.state === state);
-    return showMessage;
+    let returnMsg = "";
+    this.item.forEach((element) => {
+      if (state === element.state && (state === "todo" || state === "doing")) {
+        returnMsg += `ID: ${element.id}, taskName: ${element.taskName}, state: ${element.state}\n`;
+      }
+      else if (state === element.state && state === "done") {
+        returnMsg += `ID: ${element.id}, taskName: ${element.taskName}, state: ${element.state}, leadTime: ${element.leadTime}\n`;
+      }
+    });
+    return returnMsg;
   },
   //item 리스트(배열)에서 특정 id값을 찾아 해당하는 데이터의 state를 바꿔주는 함수.
   updateData(state, id) {
-    this.item.forEach((element) => {
-      if (element.id === id) {
-        this.currentState[element.state]--;
-        this.currentState[state]++;
-        element.state = state;
-      };
-    });
-    console.log("현재 상태:", this.currentState);
-    return this.item;
+    if (this.checkState(state)) {
+      this.item.forEach((element) => {
+        if (element.id === id) {
+          this.currentState[element.state]--;
+          this.currentState[state]++;
+          element.state = state;
+          const leadTime = this.getLeadTime(element.state, element.leadTime);
+          element.leadTime = leadTime;
+        }
+      });
+      return this.item;
+    } else return `"${state}"는 올바르지 않은 state입니다. 올바른 state를 입력해주세요.`;
+  },
+  //updateData함수에서 받은 'state'의 인자값이 올바른지 아닌지 판별해주는 함수.
+  checkState(state) {
+    if (state === "doing" || state === "done") return true;
+    else return false
+  },
+  //소요 시간 계산해주는 함수.
+  getLeadTime(state, initLeadTime) {
+    if (state === "doing") {
+      const startingTime = Date.now();
+      return startingTime;
+    }
+    else if (state === "done") {
+      const endTime = Date.now()
+      return endTime - initLeadTime;
+    }
   },
 };
 
 console.log(currentStateData.command("add$자바스크립트 공부하기"));
 console.log(currentStateData.command("add$자바 공부하기"));
-console.log(currentStateData.command("show$todo"));
+console.log(currentStateData.command("update$1$doing"));
 console.log(currentStateData.command("update$1$done"));
+console.log(currentStateData.command("show$done"));
+// console.log(currentStateData.command("show$done"));
+
