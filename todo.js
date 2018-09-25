047019f11d30b498277fb9342bec4b7da234cb48
Boyoung Kim <boyoung@vanary.com>
2018-09-25 23:19:42 +0900

    Improve namespace of each methods

--- todo.js
+++ todo.js
@@ -10,73 +10,57 @@ const todo = {
         this.todoList.push(taskToAdd);
         this.countOfStatus.todo++;
 
-        this.logUpdateResult('add', {taskId: taskId, taskName: newTaskName});
+        this.printUpdateResult('add', {taskId: taskId, taskName: newTaskName});
     },
-    updateTask({id: taskId, nextStatus}) {
+    updateTask({id, nextStatus}) {
         const newStatus = nextStatus.toLowerCase();
-        const targetTask = this.todoList[taskId-1];
-        const {name: taskName, status: prevStatus} = targetTask;
+        const targetTask = this.todoList[id-1];
+        const {name: targetTaskName, status: currentStatus} = targetTask;
         
         targetTask.status = newStatus;
-        this.countOfStatus[prevStatus]--;
+        this.countOfStatus[currentStatus]--;
         this.countOfStatus[newStatus]++;
 
-        this.logUpdateResult('update', {taskId: taskId, taskName: taskName, prevStatus: prevStatus, nextStatus: newStatus});
+        this.printUpdateResult('update', {taskId: id, taskName: targetTaskName, prevStatus: currentStatus, nextStatus: newStatus});
     },
-    removeTask({id: taskId}) {
-        const {name: taskName, status} = this.todoList[taskId-1];
-        delete this.todoList[taskId-1];
+    removeTask({id}) {
+        const {name, status} = this.todoList[id-1];
+        delete this.todoList[id-1];
         this.countOfStatus[status]--;
 
-        this.logUpdateResult('remove', {taskId: taskId, taskName: taskName});
+        this.printUpdateResult('remove', {taskId: id, taskName: name});
     },
-    logUpdateResult(actionType, {taskId, taskName, prevStatus, nextStatus}) {
-        let actionResult = ''
+    printUpdateResult(actionType, {taskId, taskName, prevStatus, nextStatus}) {
+        const countOfTasksPerStatus = `현재상태 : todo: ${this.countOfStatus.todo}개, doing: ${this.countOfStatus.doing}개, done: ${this.countOfStatus.done}개`;
 
         if (actionType === 'add') {
-            actionResult = `id: ${taskId} "${taskName}" 항목이 새로 추가됐습니다.\n현재상태 : todo: ${this.countOfStatus.todo}개, doing: ${this.countOfStatus.doing}개, done: ${this.countOfStatus.done}개`;    
-        } else if (actionType === 'update') {
-            actionResult = `id: ${taskId} "${taskName}" 항목이 ${prevStatus} => ${nextStatus} 상태로 업데이트 됐습니다.\n현재상태 : todo: ${this.countOfStatus.todo}개, doing: ${this.countOfStatus.doing}개, done: ${this.countOfStatus.done}개`;
-        } else if (actionType === 'remove') {
-            actionResult = `id: ${taskId}, "${taskName}" 항목 삭제 완료`;
+            console.log(`id: ${taskId} "${taskName}" 항목이 새로 추가됐습니다.\n${countOfTasksPerStatus}`);    
+            return
+        } 
+        if (actionType === 'update') {
+            console.log(`id: ${taskId} "${taskName}" 항목이 ${prevStatus} => ${nextStatus} 상태로 업데이트 됐습니다.\n${countOfTasksPerStatus}`);
+            return
+        } 
+        if (actionType === 'remove') {
+            console.log(`id: ${taskId}, "${taskName}" 항목 삭제 완료`);
+            return
         }
-        
-        console.log(actionResult);
     }
 };
 
 //Test cases
-
+/*
 todo.addTask({name: "자바스크립트 공부하기", tag:"programming"});
+//> id: 1 "자바스크립트 공부하기" 항목이 새로 추가됐습니다.
+//> 현재상태 : todo: 1개, doing: 0개, done: 0개
 
 todo.updateTask({id:1,  nextStatus:"doNe"});
+//> id: 1 "자바스크립트 공부하기" 항목이 todo => done 상태로 업데이트 됐습니다.
+//> 현재상태 : todo: 0개, doing: 0개, done: 1개
 
 todo.removeTask({id:1});
+//> id: 1, "자바스크립트 공부하기" 항목 삭제 완료
 
 console.log(todo.todoList[0], todo.countOfStatus);
-
-/*
-[요구사항]
-새로운 task를 추가할 수 있음
-태그(tag)를 옵션으로 추가할 수 있음
-모든 task는 id를 가짐
-todo/doing/done 상태를 가짐(add시에는 todo)
-상태변경을 할 수 있음
-code 구현형태
-
-코드의 형태는 객체리터럴 형태로 구현한다.
-
-[동작형태]
-
-> todo.add({name: "자바스크립트 공부하기", tag:"programming"});  // 태그를 입력받는다.
-id: 5,  "자바스크립트 공부하기" 항목이 새로 추가됐습니다. 
-현재상태 :  todo:1개, doing:2개, done:2개
-
-> todo.update({id:4,  nextstatus:"doNe"});  //대소문자 모두 잘 된다.
-id: 4,  "자바스크립트 공부하기" 항목이 todo => done 상태로 업데이트 됐습니다.
-현재상태 :  todo:1개, doing:1개, done:4개  
-
-> todo.remove({id:3});
-id:3, iOS공부하기 삭제완료. '
 
 */
