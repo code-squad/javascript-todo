04b384acbef76755751d52b504bf70f7869cbdda
cohily12 <k96079123@gmail.com>
2018-09-28 00:14:54 +0900

    add remove method

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-/*
-다음처럼 동작하는 할일(task)관리 프로그램을 만듭니다.
-
-새로운 task를 추가할 수 있음
-태그(tag)를 옵션으로 추가할 수 있음
-모든 task는 id를 가짐
-todo/doing/done 상태를 가짐(add시에는 todo)
-상태변경을 할 수 있음
-
-코드의 형태는 객체리터럴 형태로 구현한다.
-
-> todo.add({name: "자바스크립트 공부하기", tag:"programming"});  // 태그를 입력받는다.
-id: 5,  "자바스크립트 공부하기" 항목이 새로 추가됐습니다. 
-현재상태 :  todo:1개, doing:2개, done:2개
-
-> todo.update({id:4,  nextstatus:"doNe"});  //대소문자 모두 잘 된다.
-id: 4,  "자바스크립트 공부하기" 항목이 todo => done 상태로 업데이트 됐습니다.
-현재상태 :  todo:1개, doing:1개, done:4개  
-
-> todo.remove({id:3});
-id:3, iOS공부하기 삭제완료. '
-*/
-
 const todo = {
     list: [],
 
@@ -28,26 +5,34 @@ const todo = {
         task.id = this.list.length + 1;
         task.status = 'todo';
         this.list.push(task);
-        this.printTodoList('add', task);    
+        this.printResult('add', task);    
     },
 
     update({id, nextstatus}){
-        const mappedTask = this.list[id-1];
-        const prevStatus = mappedTask.status;
-        mappedTask.status = nextstatus.toLowerCase();
-        this.printTodoList('update', mappedTask, prevStatus); 
+        const targetTask = this.list[id-1];
+        const prevStatus = targetTask.status;
+        targetTask.status = nextstatus.toLowerCase();
+        this.printResult('update', targetTask, prevStatus); 
     },
 
-    printTodoList(methodName, task, prevStatus){
+    remove({id}){
+        const targetTask = this.list[id-1];
+        this.list[id-1] = undefined;
+        this.printResult('remove', targetTask);
+    },
+
+    printResult(methodName, task, prevStatus){
         if(methodName === 'add'){
             console.log(`id: ${task.id},  \"${task.name}\" 항목이 새로 추가됐습니다. 현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
         } else if(methodName === 'update'){
             console.log(`id: ${task.id},  \"${task.name}\" 항목이 ${prevStatus} => ${task.status} 상태로 업데이트 됐습니다. 현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
+        } else {
+            console.log(`id: ${task.id},  ${task.name} 삭제완료`);
         }
     },
 
     countTodoStatus(status){
-        const listOfStatus = this.list.filter(task => task.status === status);
-        return listOfStatus.length;
+        const countStatus = this.list.filter(task => !!task && task.status === status).length;
+        return countStatus;
     }
 };
