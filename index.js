d5186b9da10b5c9e3841f331730ab7d2d6a9c3a0
cohily12 <k96079123@gmail.com>
2018-09-28 00:59:06 +0900

    improve readability

--- index.js
+++ index.js
@@ -11,22 +11,26 @@ const todo = {
     update({id, nextstatus}){
         const targetTask = this.list[id-1];
         const prevStatus = targetTask.status;
+
         targetTask.status = nextstatus.toLowerCase();
         this.printResult('update', targetTask, prevStatus); 
     },
 
     remove({id}){
         const targetTask = this.list[id-1];
+
         this.list[id-1] = undefined;
         this.printResult('remove', targetTask);
     },
 
     printResult(methodName, task, prevStatus){
         if(methodName === 'add'){
-            console.log(`id: ${task.id},  \"${task.name}\" 항목이 새로 추가됐습니다. 현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
-        } else if(methodName === 'update'){
-            console.log(`id: ${task.id},  \"${task.name}\" 항목이 ${prevStatus} => ${task.status} 상태로 업데이트 됐습니다. 현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
-        } else {
+            console.log(`id: ${task.id},  \"${task.name}\" 항목이 새로 추가됐습니다. \n현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
+        }
+        if(methodName === 'update'){
+            console.log(`id: ${task.id},  \"${task.name}\" 항목이 ${prevStatus} => ${task.status} 상태로 업데이트 됐습니다. \n현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
+        }
+        if(methodName === 'remove'){
             console.log(`id: ${task.id},  ${task.name} 삭제완료`);
         }
     },
