e952eef45d0eeca59a46d506cf7cd227832d365c
Kim <a01088570194@gmail.com>
2018-09-30 23:53:17 +0900

    Add findIdex&findDataById method for find to data by unique id

--- todo.js
+++ todo.js
@@ -8,26 +8,33 @@ const todosList = {
             id: this.id,
             status : 'todo'
         });
-        this.showActiveMessage('add');
+        this.showActiveMessage('add', this.id);
         this.getCurrentStatus('todo','done','doing');
         this.id++;
     },
     remove({id}){
-        let prevStatus = this.todos[id-1].name;
-        this.todos.splice(id-1, 1);
+        let prevStatus = this.findDataById(id)[0].name;
+        const idx = this.findIdex(id);
+        this.todos.splice(idx, 1);
         this.showActiveMessage('remove', id, prevStatus);
         this.getCurrentStatus('todo','done','doing');
     },
     update({id,  nextstatus}){
-        let prevStatus = this.todos[id-1].status;
-        this.todos[id-1].status = nextstatus.toLowerCase();
+        let prevStatus = this.findDataById(id)[0].status;
+        this.findDataById(id)[0].status = nextstatus.toLowerCase();
         this.showActiveMessage('update', id, prevStatus);
         this.getCurrentStatus('todo','done','doing');
     },
+    findIdex(id){
+        return this.todos.findIndex((v) => v.id === this.findDataById(id)[0].id);
+    },
+    findDataById(id){
+        return this.todos.filter((v) => v.id === id);
+    },
     showActiveMessage(activeType, id, prevStatus){
-        (activeType === 'add') ? console.log(`id: ${this.id} ${this.todos[this.id-1].name} 항목이 새로 추가되었습니다.`) :
+        (activeType === 'add') ? console.log(`id: ${this.id} ${this.findDataById(id)[0].name} 항목이 새로 추가되었습니다.`) :
         (activeType === 'remove') ? console.log(`id:${id}, ${prevStatus} 삭제완료.`) :
-        (activeType === 'update') ? console.log(`id:${this.id},  "${this.todos[id-1].name}" 항목이 ${prevStatus} => ${this.todos[id-1].status} 상태로 업데이트 됐습니다.`) :
+        (activeType === 'update') ? console.log(`id:${id},  "${this.findDataById(id)[0].name}" 항목이 ${prevStatus} => ${this.findDataById(id)[0].status} 상태로 업데이트 됐습니다.`) :
         console.log('');
     },
     getCurrentStatus(todo, done, doing){
@@ -57,4 +64,9 @@ todosList.add({name: "알고리즘 공부하기", tag:"programming"});
 todosList.add({name: "자료구조 공부하기", tag:"programming"});
 todosList.add({name: "OS 공부하기", tag:"programming"});
 todosList.remove({id:2});
+todosList.remove({id:3});
+todosList.add({name: "OS 공부하기", tag:"programming"});
+todosList.remove({id:4});
+todosList.remove({id:5});
+todosList.add({name: "OS 공부하기", tag:"programming"});
 todosList.update({id:1,  nextstatus:"doNe"});
