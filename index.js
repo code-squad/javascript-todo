30f4e0c282e931461820522acf948abf9d851b26
cohily12 <k96079123@gmail.com>
2018-10-01 01:57:38 +0900

    add getTime method

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const todo = {
-    listObj: {},
+    id: {},
     taskId: 0,
 
     add(task){
         task.id = ++this.taskId;
         task.status = 'todo';
-        this.listObj[task.id] = task;
+        this.id[task.id] = task;
         this.printResult('add', task);    
     },
 
     update({id, nextstatus}){
         const caseInsensitiveStatus = nextstatus.toLowerCase();
-        const targetTask = this.listObj[id];
+        const targetTask = this.id[id];
         const prevStatus = targetTask.status;
 
         if(caseInsensitiveStatus === 'doing') targetTask.timeInfo = Date.now();
@@ -22,42 +22,53 @@ const todo = {
     },
 
     remove({id}){
-        const targetTask = this.listObj[id];
-
-        delete this.listObj[id];
+        const targetTask = this.id[id];
+        delete this.id[id];
         this.printResult('remove', targetTask);
     },
 
     printResult(methodName, task, prevStatus){
         if(methodName === 'add'){
-            console.log(`id: ${task.id},  \"${task.name}\" 항목이 새로 추가됐습니다. \n현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
+            console.log(`id: ${task.id}, \"${task.name}\" 항목이 새로 추가됐습니다. \n현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
         }
         if(methodName === 'update'){
-            console.log(`id: ${task.id},  \"${task.name}\" 항목이 ${prevStatus} => ${task.status} 상태로 업데이트 됐습니다. \n현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
+            console.log(`id: ${task.id}, \"${task.name}\" 항목이 ${prevStatus} => ${task.status} 상태로 업데이트 됐습니다. \n현재상태 : todo: ${this.countTodoStatus('todo')}, doing: ${this.countTodoStatus('doing')}, done: ${this.countTodoStatus('done')}`);
         }
         if(methodName === 'remove'){
-            console.log(`id: ${task.id},  ${task.name} 삭제완료`);
+            console.log(`id: ${task.id}, ${task.name} 삭제완료`);
         }
     },
 
     countTodoStatus(status){
-        const countStatus = Object.values(this.listObj).filter(task => task.status === status).length;
+        const countStatus = Object.values(this.id).filter(task => task.status === status).length;
         return countStatus;
     }
 }
 
 const todoPrint = {
-    showTag(tag){
-        let resultStr = ``;
-        const tagObj = Object.values(todo.listObj).filter(task => task.tag === tag).reduce((statusObj, task) => {
+    getTodoByStatus(list, status){
+        const todoByStatus = Object.values(list).reduce((statusObj, task) => {
             statusObj[task.status].push(task);
             return statusObj;
-        }, {todo:[], doing: [], done: []});
-    
-        for(let status in tagObj){
-            if(tagObj.hasOwnProperty(status) && tagObj[status].length){
-                let tagStr = `[ ${status} , 총${tagObj[status].length}개 ]`;
-                tagObj[status].forEach(task => tagStr += `\n- ${task.id}번, ${task.name}`);
+        }, {todo: [], doing: [], done: []});
+        return !status ? todoByStatus : todoByStatus[status];
+    },
+
+    getTodoByTag(tag){
+        const todoByTag = Object.values(todo.id).reduce((tagObj, task) => {
+            if(task.tag) !tagObj[task.tag] ? tagObj[task.tag] = [task] : tagObj[task.tag].push(task);
+            return tagObj;
+        }, {});
+        return !tag ? todoByTag : todoByTag[tag];
+    },
+
+    showTag(tag){
+        const todoTag = this.getTodoByStatus(this.getTodoByTag(tag));
+        let resultStr = ``;
+        
+        for(let status in todoTag){
+            if(todoTag.hasOwnProperty(status) && todoTag[status].length){
+                const tagStr = todoTag[status].reduce((str, task) => str += `\n- ${task.id}번, ${task.name}`, `[ ${status} , 총${todoTag[status].length}개 ]`);
                 resultStr += tagStr + '\n\n';
             }
         }
@@ -65,16 +76,12 @@ const todoPrint = {
     },
 
     showTags(){
+        const todoTags = this.getTodoByTag();
         let resultStr = ``;
-        const tagObj = Object.values(todo.listObj).filter(task => task.tag).reduce((tagObj, task) => {
-            !tagObj[task.tag] ? tagObj[task.tag] = [task] : tagObj[task.tag].push(task);
-            return tagObj;
-        }, {})
 
-        for(let tag in tagObj){
-            if(tagObj.hasOwnProperty(tag)){
-                let tagStr = `[ ${tag} , 총${tagObj[tag].length}개 ]`;
-                tagObj[tag].forEach(task => tagStr += `\n- ${task.id}번, ${task.name}, [${task.status}]`);
+        for(let tag in todoTags){
+            if(todoTags.hasOwnProperty(tag)){
+                const tagStr = todoTags[tag].reduce((str, task) => str += `\n- ${task.id}번, ${task.name}, ${task.status}`, `[ ${tag} , 총${todoTags[tag].length}개 ]`);
                 resultStr += tagStr + '\n\n';
             }
         }
@@ -82,47 +89,63 @@ const todoPrint = {
     },
 
     show(status){
-        const resultStr = Object.values(todo.listObj).filter(task => task.status === status).reduce((statusStr, task) => {
-            task.tag ? statusStr += `- ${task.id}번, ${task.name}, [${task.tag}]\n` : statusStr += `- ${task.id}번, ${task.name}\n`;
+        const resultStr = this.getTodoByStatus(todo.id, status).reduce((statusStr, task) => {
+            if(status === 'done' && task.timeInfo) { 
+                task.tag ? statusStr += `- ${task.id}번, ${task.name}, [${task.tag}], ${todoPrint.getTime(task.timeInfo)}\n` : statusStr += `- ${task.id}번, ${task.name}\n`;
+            } else {
+                task.tag ? statusStr += `- ${task.id}번, ${task.name}, [${task.tag}]\n` : statusStr += `- ${task.id}번, ${task.name}\n`;
+            }
             return statusStr;
         }, '');
         console.log(resultStr);
     },
 
     showAll(){
-        const todoObj = Object.values(todo.listObj).reduce((statusObj, task) => {
-            statusObj[task.status].push(task);
-            return statusObj;
-        }, {todo:[], doing: [], done: []});
-
-        function makeStatusStr(status){
-            let tagStr = `[ ${status} , 총${todoObj[status].length}개 ]`;
-            todoObj[status].forEach(task => tagStr += `\n- ${task.id}번, ${task.name}, [${task.tag}]`);
-            return tagStr;
-        }
-
-        var asyncPrint = new Promise(function(resolve, reject){
-            resolve(makeStatusStr);
+        const self = this;
+        const asyncPrint = new Promise(function(resolve, reject){
+            resolve(self.show);
         });
 
-        asyncPrint.then(function(value){
-            console.log(`\"총 ${Object.values(todo.listObj).length}개의 리스트를 가져왔습니다. 2초뒤에 todo내역을 출력합니다.....\"\n`);
+        asyncPrint.then(function(show){
+            console.log(`\"총 ${Object.values(todo.id).length}개의 리스트를 가져왔습니다. 2초뒤에 todo내역을 출력합니다.....\"\n`);
             setTimeout(function(){
-                console.log(value('todo') + '\n\n\"지금부터 3초뒤에 doing내역을 출력합니다.....\"\n');
+                console.log(self.show('todo') + '\n\n\"지금부터 3초뒤에 doing내역을 출력합니다.....\"\n');
             }, 2000);
-            return makeStatusStr;
-        }).then(function(value){
+            return show;
+        }).then(function(show){
             setTimeout(function(){
-                console.log(value('doing') + '\n\n\"지금부터 2초뒤에 done내역을 출력합니다.....\"\n');
+                console.log(self.show('doing') + '\n\n\"지금부터 2초뒤에 done내역을 출력합니다.....\"\n');
             }, 5000);
-            return makeStatusStr;
-        }).then(function(value){
+            return show;
+        }).then(function(show){
             setTimeout(function(){
-                console.log(value('done'));
+                console.log(self.show('done'));
             }, 7000);
         })
+    },
+    
+    getTime(timeInfo){
+        const min = 60 * 1000;
+        const hour = 60 * min;
+        const day = 24 * hour;
+
+        const days = parseInt(timeInfo / day);
+        timeInfo -= (days * day);
+        const hours = parseInt(timeInfo / hour);
+        timeInfo -= (hours * hour);
+        const mins = parseInt(Math.floor(timeInfo / min));
+        timeInfo -= (mins * min);
+
+        let timeStr = ``
+
+        if(days) timeStr += `${days}일\n`;
+        if(hours) timeStr += `${hours}시간\n`;
+        if (mins) timeStr += `${mins}분`;
+
+        return timeStr;
     }
 }
+
 
 todo.add({name: "js 공부하기", tag:"programming"});
 todo.add({name: "algorithm 공부하기", tag:"programming"});
@@ -142,4 +165,9 @@ todo.update({id: 5, nextstatus: 'doing'});
 
 todo.remove({id: 4, nextstatus: 'doing'});
 
+todoPrint.showTag('programming');
+todoPrint.showTags();
+todoPrint.show('todo');
+todoPrint.show('doing');
+todoPrint.show('done');
 todoPrint.showAll();
