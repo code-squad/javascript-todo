32acb5dfe8904ca2470bcc2031f27155f2cac6f8
Head <mrdoo309@gmail.com>
2018-10-01 11:42:35 +0900

    스켈레톤 코드 작성

---
+++ todo.js
@@ -0,0 +1,27 @@
+const todo = {
+    task: [{
+        id : 1,
+        name:'자바스크립트 공부하기',
+        status:'todo',
+        tag:'',
+    }], //데이터 축적
+    add: function(Obj) {
+
+    },//해야할일 추가 함수
+    update: function(Obj) {
+
+    },//상태 업데이트 함수
+    remove: function(Obj) {
+
+    },//할일 제거 함수
+    printTask: function() {
+
+    },//현재 해야할 일들과 상태를 출력해주는 함수
+    showTags: function() {
+
+    },//태그들의 이름을 출력해주는 함수
+    printTagRelate: function(tag) {
+
+    }//tag가 같은 할 일들 출력
+     
+}//해야 할일 객체
