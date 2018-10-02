2816a08ed5203dfeb34cd78d46a29f6cbf41f509
Head <mrdoo309@gmail.com>
2018-10-02 14:56:23 +0900

    새로운 객체 안 배열에 저장하도록 만들어 문제 해결

--- todo.js
+++ todo.js
@@ -1,17 +1,18 @@
-let task = []//데이터를 축적시키는 배열
+const saveData = {
+    task: [],//데이터를 축적시키는 배열
+    idArrays: []
+}
+console.log(`할일의 상태가 done이라면 빨리 지워주세요.
+데이터의 개수가 100개가 넘어가면 에러를 반환합니다.`);
+
 const todo = {
     getRanNum: function () {
-        ranNum = Math.floor(Math.random() * 10);
-        return this.checkOverlap(ranNum)
-    },
-
-    checkOverlap: function (num) {
-        task.forEach(obj => {
-            if (obj['id'] === num) {
-                return this.getRanNum();
-            }
-        })
-        return num;
+        const ranNum = Math.floor(Math.random() * 100)
+        if(saveData.idArrays.indexOf(ranNum) !== -1) {
+            return this.getRanNum() 
+        }
+        saveData.idArrays.push(ranNum)
+        return ranNum
     },
 
     getStatusNum: function (accumulatedTask) {
@@ -22,9 +23,9 @@ const todo = {
         }
         accumulatedTask.forEach(obj => {
             obj['status'] === 'todo' ? statusNum.todo++ :
-            obj['status'] === 'doing' ? statusNum.doing++ :
-            obj['status'] === 'done' ? statusNum.done++ :
-            console.log('에러메세지 : 현재상태가 존재하지 않습니다.')
+                obj['status'] === 'doing' ? statusNum.doing++ :
+                    obj['status'] === 'done' ? statusNum.done++ :
+                        console.log('에러메세지 : 현재상태가 존재하지 않습니다.')
         })
         return statusNum
     },
@@ -40,47 +41,47 @@ const todo = {
             status: 'todo',
             tag: objToAdd.tag
         }
-        task.push(newTodo)
-        let statusNum = this.getStatusNum(task)
+        saveData.task.push(newTodo)
+        let statusNum = this.getStatusNum(saveData.task)
         console.log(`ID : ${newTodo.id}, ${newTodo.name} 항목이 추가되었습니다.`);
         todo.printStatusNum(statusNum)
     },//해야할일 추가 함수
 
     update: function (objToUpdate) {
-        task = task.map(taskObj => {
+        saveData.task = saveData.task.map(taskObj => {
             if (objToUpdate.id === taskObj.id) {
                 taskObj.status = objToUpdate.nextstatus.toLowerCase();
                 return taskObj
             }
             return taskObj
         })
-        const changedTask = task.filter(taskObj => {
+        const changedTask = saveData.task.filter(taskObj => {
             if (objToUpdate.id === taskObj.id) {
                 return taskObj
             }
         })
-        let statusNum = this.getStatusNum(task)
+        let statusNum = this.getStatusNum(saveData.task)
         console.log(`ID: ${changedTask[0].id}, ${changedTask[0].name} 항목이 todo => ${changedTask[0].status} 상태로 업데이트 되었습니다.`)
         todo.printStatusNum(statusNum)
     },//상태 업데이트 함수
 
     remove: function (objToRemove) {
-        let extractedTask = task.filter(taskObj => taskObj.id === objToRemove.id)
-        let removedTask = task.filter(taskObj => taskObj.id !== objToRemove.id)
-        task = removedTask
+        let extractedTask = saveData.task.filter(taskObj => taskObj.id === objToRemove.id)
+        let removedTask = saveData.task.filter(taskObj => taskObj.id !== objToRemove.id)
+        saveData.task = removedTask
         console.log(`ID : ${extractedTask[0].id}, ${extractedTask[0].name} 삭제 완료`)
     },//할일 제거 함수
 
     printTask: function () {
         console.log(`입력된 할 일들`)
-        task.forEach(obj => {
+        saveData.task.forEach(obj => {
             console.log(`ID : ${obj.id}, 이름 : ${obj.name}, 상태 : ${obj.status}, 태그 : ${obj.tag}`)
         })
     },//현재 해야할 일들과 상태를 출력해주는 함수
 
     printTagRelate: function (tag) {
         console.log(`현재 ${tag} 태그를 가진 할 일들은 다음과 같습니다.`);
-        const tagSeparatedTask = task.filter(obj => {
+        const tagSeparatedTask = saveData.task.filter(obj => {
             return obj.tag === tag
         })
         tagSeparatedTask.forEach(obj => {
@@ -91,5 +92,6 @@ const todo = {
 
 todo.add({ name: '자바스크립트', tag: 'programming' });
 todo.add({ name: '이름', tag: 'getName' });
-
+todo.add({ name: '이름1', tag: 'gmm' })
+todo.add({ name: '이름2', tag: 'awef' })
 
