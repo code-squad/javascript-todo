154a4caf8b407c59921d476d306591dd55267741
Head <mrdoo309@gmail.com>
2018-10-02 19:28:59 +0900

    saveData객체를 없애고 그 내용을 todo객체 안으로 집어 넣음, todo.idArrays의 값은 todo.add를 호출할때마다 추가되도록 수정.

--- todo.js
+++ todo.js
@@ -1,18 +1,12 @@
-const saveData = {
-    task: [],//데이터를 축적시키는 배열
-    idArrays: []
-}
-console.log(`할일의 상태가 done이라면 빨리 지워주세요.
-데이터의 개수가 100개가 넘어가면 에러를 반환합니다.`);
-
 const todo = {
+    task: [],
+    idArrays: [],
     getRanNum: function () {
-        const ranNum = Math.floor(Math.random() * 100)
-        if(saveData.idArrays.indexOf(ranNum) !== -1) {
-            return this.getRanNum() 
+        const ranNum = Math.floor(Math.random() * 5)
+        if(this.idArrays.includes(ranNum)) {
+            return this.getRanNum();
         }
-        saveData.idArrays.push(ranNum)
-        return ranNum
+        return ranNum;
     },
 
     getStatusNum: function (accumulatedTask) {
@@ -22,10 +16,10 @@ const todo = {
             done: 0
         }
         accumulatedTask.forEach(obj => {
-            obj['status'] === 'todo' ? statusNum.todo++ :
-            obj['status'] === 'doing' ? statusNum.doing++ :
-            obj['status'] === 'done' ? statusNum.done++ :
-            console.log('에러메세지 : 현재상태가 존재하지 않습니다.')
+        obj['status'] === 'todo' ? statusNum.todo++ :
+        obj['status'] === 'doing' ? statusNum.doing++ :
+        obj['status'] === 'done' ? statusNum.done++ :
+        console.log('에러메세지 : 현재상태가 존재하지 않습니다.')
         })
         return statusNum
     },
@@ -41,49 +35,50 @@ const todo = {
             status: 'todo',
             tag: objToAdd.tag
         }
-        saveData.task.push(newTodo)
-        let statusNum = this.getStatusNum(saveData.task)
+        this.idArrays.push(newTodo.id)
+        this.task.push(newTodo)
+        let statusNum = this.getStatusNum(this.task)
         console.log(`ID : ${newTodo.id}, ${newTodo.name} 항목이 추가되었습니다.`);
-        todo.printStatusNum(statusNum)
+        this.printStatusNum(statusNum)
     },//해야할일 추가 함수
 
     update: function (objToUpdate) {
-        saveData.task = saveData.task.map(taskObj => {
+        this.task = this.task.map(taskObj => {
             if (objToUpdate.id === taskObj.id) {
                 taskObj.status = objToUpdate.nextstatus.toLowerCase();
                 return taskObj
             }
             return taskObj
         })
-        const changedTask = saveData.task.filter(taskObj => {
+        const changedTask = this.task.filter(taskObj => {
             if (objToUpdate.id === taskObj.id) {
                 return taskObj
             }
         })
-        let statusNum = this.getStatusNum(saveData.task)
+        let statusNum = this.getStatusNum(this.task)
         console.log(`ID: ${changedTask[0].id}, ${changedTask[0].name} 항목이 todo => ${changedTask[0].status} 상태로 업데이트 되었습니다.`)
-        todo.printStatusNum(statusNum)
+        this.printStatusNum(statusNum)
     },//상태 업데이트 함수
 
     remove: function (objToRemove) {
-        let extractedTask = saveData.task.filter(taskObj => taskObj.id === objToRemove.id)
-        let deletedArrays = saveData.idArrays.filter(value => objToRemove.id !== value)
-        saveData.idArrays = deletedArrays
-        let removedTask = saveData.task.filter(taskObj => taskObj.id !== objToRemove.id)
-        saveData.task = removedTask
+        let extractedTask = this.task.filter(taskObj => taskObj.id === objToRemove.id)
+        let deletedArrays = this.idArrays.filter(value => objToRemove.id !== value)
+        this.idArrays = deletedArrays
+        let removedTask = this.task.filter(taskObj => taskObj.id !== objToRemove.id)
+        this.task = removedTask
         console.log(`ID : ${extractedTask[0].id}, ${extractedTask[0].name} 삭제 완료`)
     },//할일 제거 함수
 
     printTask: function () {
         console.log(`입력된 할 일들`)
-        saveData.task.forEach(obj => {
+        this.task.forEach(obj => {
             console.log(`ID : ${obj.id}, 이름 : ${obj.name}, 상태 : ${obj.status}, 태그 : ${obj.tag}`)
         })
     },//입력된 데이터들을 출력해주는 함수
 
     printSameTag: function (tag) {
         console.log(`현재 ${tag} 태그를 가진 할 일들은 다음과 같습니다.`);
-        const tagSeparatedTask = saveData.task.filter(obj => {
+        const tagSeparatedTask = this.task.filter(obj => {
             return obj.tag === tag
         })
         tagSeparatedTask.forEach(obj => {
@@ -93,10 +88,11 @@ const todo = {
 }//해야 할일 객체
 
 // 테스트
-// todo.add({ name: '자바스크립트', tag: 'programming' });
-// todo.add({ name: 'C++', tag: 'programming'});
-// todo.add({ name: '회식', tag: '회사'});
-// todo.add({ name: '노래연습', tag: '자기개발'});
-// todo.add({ name: '과장님업무',tag:'회사'})
-// todo.add({ name: '요리연습', tag: '자기개발'})
+todo.add({ name: '자바스크립트', tag: 'programming' });
+console.log(todo.idArrays)
+todo.add({ name: 'C++', tag: 'programming' });
+console.log(todo.idArrays)
+todo.add({ name: '회식', tag: '회사' });
+todo.add({ name: '노래연습', tag: '자기개발' });
+todo.add({ name: '과장님업무', tag: '회사' })
 
