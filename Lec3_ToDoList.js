c560a0c6eb189e6e3c5faf7b74fd80d4bd52e513
developersoom <developer.soom@gmail.com>
2018-10-08 16:56:18 +0900

    Move 'taskList' inside of todo{}

--- Lec3_ToDoList.js
+++ Lec3_ToDoList.js
@@ -1,14 +1,15 @@
-let taskList = [];
+// let taskList = [];
 let idList = [];
 let statusList = [];
 
 const todo = {
+    taskList: [],
     add(task) {
         this.id = idList.length + 1;
         task.id = this.id;
         task.status = 'todo';
         idList.push(this.id);
-        taskList.push(task);
+        this.taskList.push(task);
         statusList.push(task.status);
         const [todo, doing, done] = this.findStatus(statusList);
         console.log(`id : ${this.id}, "${task.name}" 항목이 새로 추가되었습니다.
@@ -17,7 +18,7 @@ const todo = {
 
     update(idAndStatus) {
         idAndStatus.nextstatus = idAndStatus.nextstatus.toLowerCase();
-        const taskToUpdate = taskList.find(idAndStatus => idAndStatus.id === this.id);
+        const taskToUpdate = this.taskList.find(idAndStatus => idAndStatus.id === this.id);
         const before = taskToUpdate.status;
         const after = idAndStatus.nextstatus;
         taskToUpdate.status = after;
@@ -29,9 +30,9 @@ const todo = {
     },
 
     remove(id) {
-        const taskToRemove = taskList.find(id => id.id === id.id);
+        const taskToRemove = this.taskList.find(id => id.id === id.id);
         console.log(`id : ${id.id}, "${taskToRemove.name}" 삭제 완료.`);
-        taskList.splice(taskList.indexOf(taskToRemove), 1);
+        this.taskList.splice(this.taskList.indexOf(taskToRemove), 1);
         statusList.splice(statusList.indexOf(taskToRemove), 1);
         idList.splice(idList.indexOf(taskToRemove), 1);
     },
