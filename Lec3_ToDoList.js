051f0a46857458d942b2a0cc2bb4cc89dda719dd
developersoom <developer.soom@gmail.com>
2018-10-08 18:05:06 +0900

    Edit findStatus() without global variable

--- Lec3_ToDoList.js
+++ Lec3_ToDoList.js
@@ -1,18 +1,11 @@
-// let taskList = [];
-let idList = [];
-let statusList = [];
-
 const todo = {
     taskList: [],
     add(task) {
-        this.id = idList.length + 1;
-        task.id = this.id;
         task.status = 'todo';
-        idList.push(this.id);
+        task.id = new Date().getUTCMilliseconds();
         this.taskList.push(task);
-        statusList.push(task.status);
-        const [todo, doing, done] = this.findStatus(statusList);
-        console.log(`id : ${this.id}, "${task.name}" 항목이 새로 추가되었습니다.
+        let [todo, doing, done] = this.findStatus();
+        console.log(`id : ${task.id}, "${task.name}" 항목이 새로 추가되었습니다.
 현재 상태 - todo: ${todo}개, doing: ${doing}개, done: ${done}개 `);
     },
 
@@ -37,19 +30,19 @@ const todo = {
         idList.splice(idList.indexOf(taskToRemove), 1);
     },
 
-    findStatus(statusList) {
-        let todo = 0;
-        let doing = 0;
-        let done = 0;
-        for (i = 0; i < statusList.length; i++) {
-            if(statusList[i] === 'todo'){
-                todo++;
-            } else if(statusList[i] === 'doing'){
-                doing++;
-            } else if (statusList[i] === 'done'){
-                done++;
+    findStatus() {
+        let [todo, doing, done] = [0, 0, 0];
+        for (const values of this.taskList) {
+                if (values.status === 'todo') {
+                    todo++;
+                }
+                if (values.status === 'doing') {
+                    doing++;
+                }
+                if (values.status === 'done') {
+                    done++;
+                }
             }
-        }
         return [todo, doing, done];
     }
 }
