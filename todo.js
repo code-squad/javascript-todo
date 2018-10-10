f4813a8418c88b9db5919b2e8606322d2575d456
Head <mrdoo309@gmail.com>
2018-10-10 15:30:31 +0900

    모든 메서드를 좀더 간단하게 표현

--- todo.js
+++ todo.js
@@ -1,7 +1,7 @@
 const todo = {
     task: [],
-    
-    add: function (objToAdd) {
+
+    add(objToAdd) {
         const notAddedLength = this.task.length
         const newTodo = {
             id: this.getRanNum(),
@@ -15,8 +15,8 @@ const todo = {
         this.printChangeThing(newTodo, notAddedLength)
         this.printStatusNum(statusNum)
     },//해야할일과 id값을 추가해주는 함수
-    
-    getRanNum: function () {
+
+    getRanNum() {
         const ranNum = Math.floor(Math.random() * 100)
         const idArrays = this.task.map(obj => obj.id)
         if (idArrays.includes(ranNum)) {
@@ -24,8 +24,8 @@ const todo = {
         }
         return ranNum;
     },//중복되지 않는 랜덤한 숫자를뽑아내는 함수
-    
-    getStatusNum: function (accumulatedTask) {
+
+    getStatusNum(accumulatedTask) {
         const statusNum = {
             todo: 0,
             doing: 0,
@@ -36,12 +36,12 @@ const todo = {
         })
         return statusNum
     },//상태를 초기화 시켜주는 함수
-    
-    printStatusNum: function (statusNum) {
+
+    printStatusNum(statusNum) {
         console.log(`현재상태 todo : ${statusNum.todo}, doing: ${statusNum.doing}, done : ${statusNum.done}`)
     },//상태를 출력해주는 함수
-    
-    printChangeThing: function (objToPrint, beforeTaskLength, beforeTaskStatus) {
+
+    printChangeThing(objToPrint, beforeTaskLength, beforeTaskStatus) {
         if (this.task.length > beforeTaskLength) {
             console.log(`ID : ${objToPrint.id}, ${objToPrint.name} 항목이 추가되었습니다.`);
         } else if (this.task.length < beforeTaskLength) {
@@ -50,8 +50,8 @@ const todo = {
             console.log(`ID: ${objToPrint.id}, ${objToPrint.name} 항목이 ${beforeTaskStatus} => ${objToPrint.status} 상태로 업데이트 되었습니다.`)
         }
     },//할일이 추가되거나 제거되거나 업데이트 될 때 적합한 내용을 출력해 주는 함수
-    
-    update: function (objToUpdate) {
+
+    update(objToUpdate) {
         let beforeTaskStatus = []
         if (objToUpdate.nextstatus === 'doing') {
             this.updateDoingTime(objToUpdate)
@@ -75,24 +75,24 @@ const todo = {
         this.printChangeThing(changedTask[0], this.task.length, beforeTaskStatus[0])
         this.printStatusNum(statusNum)
     },//상태 업데이트 함수//주어진 정보의 시간을 넣을 수 있도록 수정 요망
-    
-    updateDoingTime: function (objToUpdate) {
+
+    updateDoingTime(objToUpdate) {
         this.task.forEach(taskObj => {
             if (taskObj.id === objToUpdate.id) {
                 taskObj.timeData = Date.now();
             }
         })
     },//업데이트할 객체를 인자로 받아 task내의 timeData값을 변경.
-    
-    updateTakeTime: function (objToUpdate) {
+
+    updateTakeTime(objToUpdate) {
         this.task.forEach(taskObj => {
             if (taskObj.id === objToUpdate.id) {
                 taskObj.timeData = this.getTakeTime(taskObj.timeData, Date.now())
             }
         })
     },//업데이트할 객체를 인자로 받아 task내의 timeData의 값을 걸린 시간으로 변경.
-    
-    getTakeTime: function (doingTime, currentTime) {
+
+    getTakeTime(doingTime, currentTime) {
         let takenTime = ''
         let takenMsecTime = currentTime - doingTime
         const msecPerMinute = 1000 * 60, msecPerHour = msecPerMinute * 60, msecPerDay = msecPerHour * 24
@@ -106,7 +106,7 @@ const todo = {
         return takenTime;
     },//걸린 시간을 계산해주는 함수
 
-    remove: function (objToRemove) {
+    remove(objToRemove) {
         const notRemovedLength = todo.task.length
         let filteredTask = this.task.filter(taskObj => taskObj.id === objToRemove.id)
         let removedTask = this.task.filter(taskObj => taskObj.id !== objToRemove.id)
@@ -114,7 +114,7 @@ const todo = {
         this.printChangeThing(filteredTask[0], notRemovedLength)
     },//할 일과 id값을 제거해주는 함수
 
-    show: function (status) {
+    show(status) {
         console.log(`[${status} 상태인 할 일들]`)
         this.task.forEach(taskObj => {
             if (status === 'done' && taskObj.status === 'done') {
@@ -124,8 +124,8 @@ const todo = {
             }
         })
     },//인자로 입력받은 상태의 정보들을 출력해주는 함수
-    
-    showTag: function (tag) {
+
+    showTag(tag) {
         const todoNum = this.getSameTagAndStatusNum(tag, 'todo')
         console.log(`[todo, 총 ${todoNum}개]`)
         this.printByTag(tag, 'todo');
@@ -136,8 +136,8 @@ const todo = {
         console.log(`[done, 총 ${doneNum}개]`)
         this.printByTag(tag, 'done');
     },//수정필요, 여기에 showTags기능까지 넣어볼 것.//함수는 한가지의 일만 하는게 맞는듯
-    
-    printByTag: function (tag, status) {
+
+    printByTag(tag, status) {
         this.task.forEach(taskObj => {
             if (taskObj.tag === tag && taskObj.status === status) {
                 if (status === 'done') {
@@ -148,8 +148,8 @@ const todo = {
             }
         })
     },//tag의 값과 상태의 값을 인자로 받아 출력해주는 함수
-    
-    getSameTagAndStatusNum: function (tag, status) {
+
+    getSameTagAndStatusNum(tag, status) {
         let sameTagAndStatusNum = 0
         this.task.forEach(taskObj => {
             if (taskObj.tag === tag && taskObj.status === status) {
@@ -159,7 +159,7 @@ const todo = {
         return sameTagAndStatusNum
     },//태그와 상태가 같은 것들의 개수를 세어주는 함수
 
-    showTags: function () {
+    showTags() {
         const taggedTask = this.task.filter(obj => {
             return obj.tag !== undefined
         })
@@ -169,37 +169,37 @@ const todo = {
             this.printSameTag(tag, taggedTask)
         })
     },//태그에 따라 모든 값을 출력해주는 함수
-    
-    getTagArrays: function(taggedTask) {
+
+    getTagArrays(taggedTask) {
         const sameTagArrays = [];
         taggedTask.forEach(taggedTaskObj => {
-            if(!sameTagArrays.includes(taggedTaskObj.tag)) {
+            if (!sameTagArrays.includes(taggedTaskObj.tag)) {
                 sameTagArrays.push(taggedTaskObj.tag)
             }
         })
         return sameTagArrays
     },//현재 task배열 내에있는 모든 tag값들을 중복 없이 따로 모아놓는 배열을 만드는 함수
-    
-    printSameTag: function(tag, taggedTask) {
+
+    printSameTag(tag, taggedTask) {
         console.log(`${tag}, 총 ${sameTagNum}개`)
         taggedTask.forEach(taggedTaskObj => {
-            if(tag === taggedTaskObj.tag) {
+            if (tag === taggedTaskObj.tag) {
                 console.log(`ID : ${taggedTaskObj.id}, ${taggedTaskObj.name}, [${taggedTaskObj.status}]`)
             }
         })
     },//tag의 값에 따라서 출력해주는 함수
-    
-    getSameTagNum: function(tag, taggedTask) {
+
+    getSameTagNum(tag, taggedTask) {
         sameTagNum = 0
         taggedTask.forEach(taggedTaskObj => {
-            if(tag === taggedTaskObj.tag) {
+            if (tag === taggedTaskObj.tag) {
                 sameTagNum++
             }
         })
         return sameTagNum
     },//같은 태그의 개수를 세어주는 함수
-    
-    showAll: function () {
+
+    showAll() {
         const statusNum = this.getStatusNum(this.task)
         console.log(`총 ${this.task.length}개의 리스트를 가져왔습니다.
     지금부터 2초뒤에 todo내역을 출력합니다........`)
@@ -222,7 +222,7 @@ const todo = {
 }//해야 할일 객체
 // 테스트
 
-todo.add({ name: '자바스크립트', tag: 'programming'});
+todo.add({ name: '자바스크립트', tag: 'programming' });
 todo.add({ name: 'C++', tag: 'programming' });
 todo.add({ name: '회식', tag: '회사' });
 todo.add({ name: '노래연습', tag: '자기개발' });
