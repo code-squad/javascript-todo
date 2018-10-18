8eac2d92db88a9e0527a1c7f7903b363b4ecb01e
Head <mrdoo309@gmail.com>
2018-10-18 15:06:00 +0900

    설계에 맞게 add메서드 구현

--- todo.js
+++ todo.js
@@ -1,9 +1,51 @@
 const todo = {
     todoList: [],
 
-    add(objToAdd) {
+    statusNum: {
+        todo: 0,
+        doing: 0,
+        done: 0
+    },//statusNum
 
-    },
+    initStatusNum() {
+        this.statusNum.todo = 0;
+        this.statusNum.doing = 0;
+        this.statusNum.done = 0;
+    },//for statusNum
+
+    getStatusNum(accumulatedTask) {
+        this.initStatusNum();
+        accumulatedTask.forEach(obj => {
+            this.statusNum[obj.status]++
+        })
+    },//for statusNum
+
+    add(objToAdd) {
+        const newTodo = {
+            id: this.getRanNum(this.todoList),
+            name: objToAdd.name,
+            status: 'todo',
+            tag: objToAdd.tag,
+            timeData: 0,
+        }
+        todoTask.push(this.checkTag(newTodo));
+    },//add
+
+    getRanNum(todoList) {
+        const ranNum = Math.floor(Math.random() * 6)
+        const idArrays = todoList.map(obj => obj.id)
+        if (idArrays.includes(ranNum)) {
+            return this.getRanNum(todoTask)
+        }
+        return ranNum;
+    },//for add1
+
+    checkTag(newTask) {
+        if (newTask.tag === undefined) {
+            newTask.tag = 'nothing'
+        }
+        return newTask
+    },//for add2
 
     remove(objToRemove) {
 
@@ -51,6 +93,14 @@ const history = {
 
 const show = {
     showingList: [],
+    showStatusNum() {
+
+    },
+
+    showChanges(todoMethod, changed, beforeChange) {
+
+    },
+
     status() {
 
     },
