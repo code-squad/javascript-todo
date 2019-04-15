942f1bf5a5c116f40d313660d4e41c8035f0710a
mukeunzi <eunji057@gmail.com>
2019-04-15 16:45:58 +0900

    add program design and implement code

--- todos.js
+++ todos.js
@@ -1,74 +1,98 @@
-const todos =  [ 
+const todos =  [
     {
-        'name' : '자바스크립트 공부하기', 
+        'name' : '자바스크립트 공부하기',
         'tags' : ['programming', 'javascript'],
         'status' : 'todo',
         'id' : 12123123
     },
     {
-        'name' : '그림 그리기', 
+        'name' : '그림 그리기',
         'tags' : ['picture', 'favorite'],
         'status' : 'doing',
         'id' : 35435345
     },
     {
-        'name' : '꽃구경하기', 
+        'name' : '꽃구경하기',
         'tags' : ['flower', 'favorite'],
         'status' : 'done',
         'id' : 7657
     },
     {
-        'name' : '저녁식사', 
+        'name' : '저녁식사',
         'tags' : ['dinner', 'food'],
         'status' : 'todo',
         'id' : 097989
     },
     {
-        'name' : '커피마시기', 
+        'name' : '커피마시기',
         'tags' : ['coffee', 'favorite'],
         'status' : 'doing',
         'id' : 65464
     }
-];
-
-const newTodoList = {'todo' : [], 'doing' : [], 'done' : []};
-
-const makeNewTodoList = function(){
+ ];
+ 
+ let newTodoList;
+ 
+ const makeNewTodoList = function(){
     todos.forEach(function(todo){
         let key = todo.status;
         let value = todo.name;
         newTodoList[key].push(value);
     })
-};
-
-const printAll = function(){
+ };
+ 
+ const printAll = function(){
     let result = [];
     for(key in newTodoList){
         result.push(key + ": " + newTodoList[key].length + "개");
     }
     console.log("현재상태 : ", result.join(', '));
-}
-
-const printStatus = function(args){
+ }
+ 
+ const printStatus = function(args){
     let result = [];
-
+ 
     for(key in newTodoList[args]){
         result.push(newTodoList[args][key]);
     }
-    console.log(`${args}리스트 : 총 : `+newTodoList[args].length + "건 : " + result.join(', '));
-}
-
-const show = (obj) => {
-    makeNewTodoList();
-
-    if (obj === "all") {
+    console.log(`${args}리스트 : 총 :` +newTodoList[args].length + "건 : " + result.join(', '));
+ }
+ 
+ const checkTags = function(tag){
+    let result = [];
+    todos.forEach(function(list){
+        if (list.tags.includes(tag)) {
+            result.push(list.name);
+        }
+    })
+ 
+    console.log(`${tag} 키워드 검색 결과 :`  + result.join(', '));
+ };
+ 
+ let printStatusAfterCheckKwd = function (searchKeyWord) {
+    if (searchKeyWord === 'all') {
         printAll();
     } else {
-        printStatus(obj);
+        printStatus(searchKeyWord);
     }
-}
-
-show("all");
-show("todo");
-show("doing");
-show("done");
+ }
+ 
+ 
+ const show = (keyWord, searchKeyWord) => {
+    newTodoList = {'todo' : [], 'doing' : [], 'done' : []};
+    makeNewTodoList();
+ 
+    if (keyWord == 'status') {
+        printStatusAfterCheckKwd(searchKeyWord);
+    } else {
+        checkTags(searchKeyWord);
+    }
+ }
+ 
+ show("status", "all");
+ show("status", "todo");
+ show("status", "doing");
+ show("status", "done");
+ show("tag", "favorite");
+ show("tag", "food");
+ show("tag", "javascript");
