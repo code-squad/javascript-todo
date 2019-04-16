2e6bd485cee07a2997880edadb216071f35eef78
mukeunzi <eunji057@gmail.com>
2019-04-16 14:13:03 +0900

    나머지 함수를 클래스의 메소드로 변경 show(), printStatus()

--- todos.js
+++ todos.js
@@ -1,35 +1,38 @@
 const todos = require('./data');
 
-let newTodoObject;
-
 class Todo {
-  constructor(todos) {
-    this.customTodos = todos.reduce(
-      (acc, cur) => {
-        acc[cur.status].push(cur.name);
-        return acc;
-      },
-      { todo: [], doing: [], done: [] }
-    );
-  }
+	constructor(todos) {
+		this.customTodos = todos.reduce(
+			(acc, cur) => {
+				acc[cur.status].push(cur.name);
+				return acc;
+			},
+			{ todo: [], doing: [], done: [] }
+		);
+	}
 
-  printAll() {
-    console.log(
-      Object.entries(this.customTodos).reduce((acc, cur) => {
-        return (acc += `${cur[0]}: ${cur[1].length}개 `);
-      }, '현재상태 : ')
-    );
-  }
+	show(status) {
+		if (status === 'all') {
+			this.printAll();
+		} else {
+			this.printStatus(status);
+		}
+	}
+
+	printAll() {
+		console.log(
+			Object.entries(this.customTodos).reduce((acc, cur) => {
+				return (acc += `${cur[0]}: ${cur[1].length}개 `);
+			}, '현재상태 : ')
+		);
+	}
+
+	printStatus(status) {
+		console.log(
+			`${status}리스트 총 ${this.customTodos[status].length}건 : ${this.customTodos[status]}`
+		);
+	}
 }
-
-//  const printStatus = (args) => {
-//     let result = [];
-
-//     for(key in newTodoObject[args]){
-//         result.push(newTodoObject[args][key]);
-//     }
-//     console.log(`${args}리스트 : 총 :` +newTodoObject[args].length + "건 : " + result.join(', '));
-//  }
 
 //  const checkTags = (tag) => {
 //     let result = [];
@@ -38,29 +41,13 @@ class Todo {
 //     }).map((obj) => { return obj.name });
 
 //     console.log(`${tag} 키워드 검색 결과 :`  + result.join(', '));
-//  };
-
-//  let printStatusAfterCheckKwd = (searchKeyWord) => {
-//     if (searchKeyWord === 'all') {
-//
-//         printAll();
-//     } else {
-//         printStatus(searchKeyWord);
-//     }
 //  }
 
-const show = (keyWord, searchKeyWord) => {
-  newTodoObject = { todo: [], doing: [], done: [] };
-  printAll();
-  // if (keyWord === 'status') {
-  //printStatusAfterCheckKwd(searchKeyWord);
-  // } else {
-  // checkTags(searchKeyWord);
-  // }
-};
-
 const todo = new Todo(todos);
-todo.printAll();
+todo.show('all');
+todo.show('done');
+todo.show('doing');
+todo.show('todo');
 // show('status', 'all');
 //  show("status", "todo");
 //  show("status", "doing");
