6e42f0965573a922544cf15ce7b60008262c06de
Min-92 <okssud1@naver.com>
2019-04-16 14:45:49 +0900

    명령어 실행 함수 작성
    입력받은 명령어에 해당하는 함수를 호출해주는 함수 excuteCommand 작성

--- app.js
+++ app.js
@@ -1,25 +1,57 @@
 const todoList = require("./todo");
 const myTodoList = new todoList();
 
-const readline = require("readline");
-const rl = readline.createInterface({
-    input : process.stdin,
-    output: process.stdout
-});
 
-rl.on("line", function(line){
-    console.log("hello !", line);
-    // rl.close();
+const getCommand = () =>{
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input : process.stdin,
+        output: process.stdout
+    });
+       
+    
+    rl.on("line", function(line){
 
-}).on("close", function() {
-    process.exit();
-});
+        // rl.close();
+        const commandArray = line.split("$");
+        executeCommand(commandArray);
+    }).on("close", function() {
+        process.exit();
+    });
+}
 
 
+const executeCommand = (commandArray) => {
+    const action = commandArray[0];
+    if(action === "show"){
+        myTodoList.show(commandArray[1]);
+    }else if(action === "add"){
+        // name = commandArray[1];
+        // tag = commandArray[2];
 
+        // add(name,tag);
+        console.log(`add ${commandArray[1]}, ${commandArray[2]}`);
+    }else if(action === "delete"){
+        // id = commandArray[1];
 
+        // delete(id);
+        console.log(`delete ${commandArray[1]}`);
+    }else if(action === "update"){
+        // id = commandArray[1];
+        // status = commandArray[2];
 
+        // update(id,status);
+        console.log(`update ${commandArray[1]}, ${commandArray[2]}`);
+    }else{
+        console.log("명령어가 유효하지 않습니다.");
+    }
+}
 
+const main = () => {
+    getCommand();
+}
+
+main();
 
 
 
