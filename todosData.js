bb73f5ef0c245980ca29b4993a3c1a6c9755b730
aftersnowing <gurcks2@gmail.com>
2019-04-16 21:42:30 +0900

    todosData수정

--- todosData.js
+++ todosData.js
@@ -3,43 +3,43 @@ const todos = [
         'name': '자바스크립트 공부하기',
         'tags': ['programming', 'javascript'],
         'status': 'doing',
-        'id': 12123123
+        'id': 43532
     },
     {
         'name': '그림 그리기',
         'tags': ['picture', 'favorite'],
         'status': 'doing',
-        'id': 312323
+        'id': 64321
     },
     {
         'name': '샤워',
         'tags': ['health', 'favorite'],
         'status': 'todo',
-        'id': 3497343
+        'id': 34536
     },
     {
         'name': '점심 먹기',
         'tags': ['food', 'favorite'],
         'status': 'done',
-        'id': 123483
+        'id': 12348
     },
     {
         'name': '귀 파기',
         'tags': ['living'],
         'status': 'todo',
-        'id': 3487233
+        'id': 34872
     },
     {
         'name': '운동하기',
         'tags': ['exercie', 'effort'],
         'status': 'todo',
-        'id': 580345
+        'id': 58034
     },
     {
         'name': '명상',
         'tags': ['ritual', 'mindcontorl'],
         'status': 'done',
-        'id': 948354
+        'id': 94835
     }
 ];
 
