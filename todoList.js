cbcb01307d7100d38284a553065af48244ca4994
aftersnowing <gurcks2@gmail.com>
2019-04-16 21:46:18 +0900

    add 기능 구현

--- todoList.js
+++ todoList.js
@@ -19,3 +19,19 @@ const showEachData = function (order) {
 // console.log(showEachData('show$doing'))
 // 명령하세요 : show$todo
 // todo리스트 :  총3건 : ' 자바스크립트 공부하기, 1822번' , 'iOS공부하기, 9933번'
+
+
+const addData = function (order) {
+    const objToAdd = {};
+    objToAdd['name'] = order.match(/(?<=\$)\D*(?=\$)/)[0];
+    objToAdd['tag'] = order.match(/(?<=\'|\")[a-z]*(?=\'|\")/)[0];
+    objToAdd['status'] = 'todo';
+    objToAdd['id'] = Math.floor(Math.random() * 100000);
+    todos.push(objToAdd);
+    console.log(`${objToAdd['name']} 1개가 추가됐습니다.(id : ${objToAdd['id']})`)
+    setTimeout(() => showAll(), 1000);
+}
+// console.log(addData('add$운동하기$["favorite"]'))
+// 명령하세요 : add$sleep$["favorite"]
+// 공부하기 1개가 추가됐습니다.(id : 7788)
+// 현재상태 :  todo: 3개, doing:2개, done:4개
