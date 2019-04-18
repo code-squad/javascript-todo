cfb50dfcb68e7bd632c4af92902dffac85471fd3
PAPICO <ocipap0531@gmail.com>
2019-04-18 14:22:59 +0900

    Add class todo
    - Todos Class 생성
    - Todos Class method 정의
    - 외부함수 구조 설계

---
+++ todo.js
@@ -0,0 +1,44 @@
+const rl = require("readline")
+
+const generateId = () => {
+    let id = 0
+    return id;
+}
+
+const getTodoIndex = (id) => {
+    let index = -1;
+    return index
+}
+
+const splitStringByChar = (string, s) => {
+    return []
+}
+
+class Todos {
+    constructor() {
+        this.todos = []
+    }
+
+    start(){
+        
+    }
+
+    show() {
+
+    }
+
+    add() {
+
+    }
+
+    delete() {
+
+    }
+
+    update() {
+
+    }
+}
+
+const todo = new Todos()
+todo.start()
