e451d12690ab2de8878fe7ea131d8bd25f56ea06
PAPICO <ocipap0531@gmail.com>
2019-04-18 14:55:18 +0900

    feat : Create Todos start() method
    - Todos의 내부 메서드 start() 작성
    - 외부함수 splitStringByChar() 작성

--- todo.js
+++ todo.js
@@ -1,4 +1,9 @@
-const rl = require("readline")
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 const generateId = () => {
     let id = 0
@@ -10,16 +15,40 @@ const getTodoIndex = (id) => {
     return index
 }
 
-const splitStringByChar = (string, s) => {
-    return []
+const splitStringByChar = (inst, s) => {
+    let instruction = inst.split(s)
+    return instruction
 }
 
 class Todos {
     constructor() {
         this.todos = []
+        this.INSTRUCTION  = {
+            "add" : this.add,
+            "show" : this.show,
+            "update" : this.update,
+            "delete" : this.delete
+        }
     }
 
     start(){
+        rl.question("명령하세요 : ", (inst)=> {
+            if (inst === "quit" || inst === "q") {
+                console.log("프로그램을 종료합니다.")
+                rl.close()
+                return;
+            }
+            inst = splitStringByChar(inst, "$")
+            let instruction_type = inst.shift()
+            try {
+                this.INSTRUCTION[instruction_type](...inst)
+            } catch {
+                console.log("\x1b[31m%s\x1b[0m", "올바르지 않은 명령어입나다.")
+                console.log("\x1b[31m%s\x1b[0m", "사용할 수 있는 명령어 : show | add | update | delete")
+            } finally {
+                this.start()
+            }
+        })
         
     }
 
@@ -28,7 +57,6 @@ class Todos {
     }
 
     add() {
-
     }
 
     delete() {
