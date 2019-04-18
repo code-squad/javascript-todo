da94e3a2c4b8aebdd1940b829c7f456b5c88ea92
eugene94 <eugenekang94@gmail.com>
2019-04-18 14:30:16 +0900

    Todo Class 외부 파일로 분기
    - Class 분리로 이름이 중복되어 헷갈림
      - todos => data로 변경

---
+++ todo.js
@@ -0,0 +1,51 @@
+class Todo {
+  constructor(data) {
+    this.data = data;
+    this.todoCount = this.setTodoCount(data);
+  }
+
+  setTodoCount(data) {
+    return data.reduce((acc, cur) => {
+      acc[cur.status] =
+        acc[cur.status] === undefined ? [cur.name] : acc[cur.status].concat(cur.name);
+      return acc;
+    }, {});
+  }
+
+  hello() {
+    console.log('hello');
+  }
+
+  show(type, condition) {
+    let result;
+    if (type === 'status') {
+      if (condition === 'all') {
+        result = this.printAll();
+      } else {
+        result = this.printStatus(condition);
+      }
+    } else {
+      result = this.printTags(condition);
+    }
+    console.log(result);
+  }
+
+  printAll() {
+    return Object.entries(this.todoCount).reduce((acc, cur) => {
+      return (acc += `${cur[0]}: ${cur[1].length}개 `);
+    }, '현재상태 : ');
+  }
+
+  printStatus(status) {
+    return `${status}리스트 총 ${this.todoCount[status].length}건 : ${this.todoCount[status].join(
+      ', '
+    )}`;
+  }
+
+  printTags(tag) {
+    const tagArr = this.data.filter(todo => todo.tags.includes(tag)).map(obj => obj.name);
+    return `${tag} 키워드 검색 결과 : ${tagArr.join(', ')}`;
+  }
+}
+
+module.exports = Todo;
