1bb8dd0ae6ccf29f9821e8eb7710f8cce6ab1c52
janghyoseog <bestdevhyo1225@gmail.com>
2019-04-19 15:30:32 +0900

    Implement the ability to simulator

---
+++ step4-2mission/simulator.js
@@ -0,0 +1,30 @@
+const controlCommand = require('./control_command');
+const readLine = require('readline');
+
+const rl = readLine.createInterface( {
+    input:process.stdin,
+    output:process.stdout,
+} );
+
+( () => {
+
+    rl.setPrompt('명령하세요 : ');
+    rl.prompt();
+    rl.on( 'line', (query) => {
+        if (query === 'exit') rl.close();
+        const splitQuery = query.replace(/\s/gi, "").split('$');
+        const executionCommand = splitQuery[0];
+        const nonCommand = splitQuery.splice(1);
+        const isComplete = controlCommand[executionCommand](nonCommand);
+
+        if (isComplete) {
+            let millisecond = 1000;
+            if (executionCommand === 'update') millisecond = 4010;
+            else if (executionCommand === 'show') millisecond = 0;
+            setTimeout( () => { rl.prompt(); },  millisecond );
+        } else {
+            rl.prompt();
+        }
+    } ).on( 'close', () => { process.exit(); } );
+
+} )();
