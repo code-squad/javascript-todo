bfd9af60049d67571782bff268d1de71bd894f73
andole87 <andole87@gmail.com>
2019-04-19 11:05:47 +0900

    Create Step4-2 template

---
+++ app.js
@@ -0,0 +1,31 @@
+const todo_shell = {
+    COMMAND_DICT :{
+
+    },
+
+    
+    run: function(){
+
+    },
+
+    parseCommand : function(){
+
+    },
+
+    addTodo : function(){
+
+    },
+
+    updateTodo: function(){
+
+    },
+
+    deleteTodo: function(){
+
+    },
+
+    showTodo: function(){
+
+    }
+}
+
