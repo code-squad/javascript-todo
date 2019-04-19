3da50c51c7041009e71f9eb9e77ee585c0cd1628
PAPICO <ocipap0531@gmail.com>
2019-04-19 12:15:07 +0900

    refactor : 외부함수 -> Class 내부 함수 - todos.js 에 전역 함수를 Todos Class 내부 메서드로 변경 - let 으로 선언된 변수 const 변경 - ERROR 객체 이름 변경 - printResultAndStatus 메서드 구현

--- todo.js
+++ todo.js
@@ -1,49 +1,11 @@
 const readline = require("readline")
-const ERROR = {
-    "LACKPARAM" : "인자의 개수가 부족합니다. 다시 입력해 주세요."
+const ERROR_MESSAGE = {
+    "LACKPARAM": "인자의 개수가 부족합니다. 다시 입력해 주세요."
 }
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-const generateId = () => {
-    let id = ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1)
-    return id;
-}
-
-const getIndexById = (todos, id) => {
-    let index = todos.findIndex((el) => {
-        return el.id === id
-    })
-    return index
-}
-
-const getParsedCommand = (command, char) => {
-    command = command.split(char)
-    return command
-}
-
-const getCountbyStatus = (todos, status) => todos.filter(v => v.status == status).length
-
-const getStatus = (todos) => {
-    let str = ""
-    let counts = {
-        "todo": getCountbyStatus(todos,"todo"),
-        "doing": getCountbyStatus(todos,"doing"),
-        "done": getCountbyStatus(todos,"done"),
-    }
-    str += "현재상태 : " + Object.entries(counts).map(([k, v]) => `${k}: ${v}개`).join(", ")
-    return str
-}
-
-const getListByStatus = (todos, status) => {
-    let str = ""
-    let count = getCountbyStatus(todos, status)
-    str += `${status}리스트 : 총 ${count}건 : ` + todos.filter(v => v.status == status).map(v => `'${v.name}, ${v.id}번'`).join(", ")
-    return str
-}
 
 class Todos {
     constructor() {
@@ -57,13 +19,14 @@ class Todos {
             if (command === "quit()" || command === "q()") {
                 rl.close()
             }
-            command = getParsedCommand(command, "$")
-            let command_type = command.shift()
+            command = this.getParsedCommand(command, "$")
+            const command_type = command.shift()
             try {
-                this[command_type](...command) 
-            } catch(e) {
-                if (ERROR[e.message]) {
-                    console.log("\x1b[31m%s\x1b[0m", ERROR[e.message])
+                this[command_type](...command)
+            } catch (e) {
+                console.log(e.message)
+                if (ERROR_MESSAGE[e.message]) {
+                    console.log("\x1b[31m%s\x1b[0m", ERROR_MESSAGE[e.message])
                 } else {
                     console.log("\x1b[31m%s\x1b[0m", "올바르지 않은 명령어입나다.\n사용할 수 있는 명령어 : show | add | update | delete")
                 }
@@ -77,60 +40,103 @@ class Todos {
 
 
     show(option) {
-        let options = {
-            "all" : getStatus,
-            "todo" : getListByStatus,
-            "doing" : getListByStatus,
-            "done" : getListByStatus,
+        const options = {
+            "all": "getStatus",
+            "todo": "getListByStatus",
+            "doing": "getListByStatus",
+            "done": "getListByStatus",
         }
 
-        let answer = options[option](this.todos, option)
+        const answer = this[options[option]](this.todos, option)
         console.log(answer)
         setTimeout(() => {
-            rl.prompt()}, 0)
+            rl.prompt()
+        }, 0)
     }
 
     add(name, tags) {
-        let id = generateId()
-        let status = "todo"
+        const id = this.generateId()
+        const status = "todo"
         let str = ""
-        if(arguments.length !== this.add.length){
+        if (arguments.length !== this.add.length) {
             throw Error("LACKPARAM")
         }
         tags = tags.replace(/\[|\]|\"|\'/g, "").split(",")
 
-        this.todos.push(
-            {
-                id,
-                name,
-                tags,
-                status
-            }
-        )
+        this.todos.push({
+            id,
+            name,
+            tags,
+            status
+        })
         str = `${name}이(가) 1개 추가하였습니다. (id : ${id})`
-        console.log("\x1b[32m%s\x1b[0m", str)
-        setTimeout(() => this.show("all") ,1000)
+        this.printResultAndStatus(str)
     }
 
     delete(id) {
-        let index = getIndexById(this.todos, id)
+        const index = this.getIndexById(this.todos, id)
         let str = ""
-        let {name, status} = this.todos[index]
+        const {
+            name,
+            status
+        } = this.todos[index]
         this.todos.splice(index, 1)
         str = `${name} ${status}이(가) 목록에서 삭제되었습니다.`
-        console.log("\x1b[32m%s\x1b[0m", str)
-        setTimeout(() => this.show("all") ,1000)
+        this.printResultAndStatus(str)
     }
 
     update(id, status) {
-        let index = getIndexById(this.todos, id)
+        const index = this.getIndexById(this.todos, id)
         let str = ""
         this.todos[index].status = status
         setTimeout(() => {
             str = `${this.todos[index].name} ${status}이(가) 목록에서 변경되었습니다.`
-            console.log("\x1b[32m%s\x1b[0m", str)
-            setTimeout(() => this.show("all") ,1000)
-        },3000)
+            this.printResultAndStatus(str)
+        }, 3000)
+    }
+
+    printResultAndStatus(str) {
+        console.log("\x1b[32m%s\x1b[0m", str)
+        setTimeout(() => this.show("all"), 1000)
+    }
+
+    generateId() {
+        const id = ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1)
+        return id;
+    }
+
+    getIndexById(todos, id) {
+        const index = todos.findIndex((el) => {
+            return el.id === id
+        })
+        return index
+    }
+
+    getParsedCommand(command, char) {
+        command = command.split(char)
+        return command
+    }
+
+    getCountbyStatus(todos, status) {
+        return todos.filter(v => v.status == status).length
+    }
+
+    getStatus (todos) {
+        let str = ""
+        const counts = {
+            "todo": this.getCountbyStatus(todos, "todo"),
+            "doing": this.getCountbyStatus(todos, "doing"),
+            "done": this.getCountbyStatus(todos, "done"),
+        }
+        str += "현재상태 : " + Object.entries(counts).map(([k, v]) => `${k}: ${v}개`).join(", ")
+        return str
+    }
+
+    getListByStatus (todos, status) {
+        let str = ""
+        const count = this.getCountbyStatus(todos, status)
+        str += `${status}리스트 : 총 ${count}건 : ` + todos.filter(v => v.status == status).map(v => `'${v.name}, ${v.id}번'`).join(", ")
+        return str
     }
 }
 
