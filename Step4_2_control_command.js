e8c8ba3a926424da476a274903a246e5aa2bafc8
janghyoseog <bestdevhyo1225@gmail.com>
2019-04-21 13:57:11 +0900

    show, add, delete, update 주요함수들을 하위 함수로 분리

--- Step4_2_control_command.js
+++ Step4_2_control_command.js
@@ -24,6 +24,56 @@ const checkIdForm = (id) => { return isNaN(id) ? false : true; }
 
 const checkTagForm = (tag) => { return tag.match(/(?<=\[\")[a-z]*(?=\"\])/i) !== null; }
 
+const showTodoList = (paramArr) => {
+    const viewType   = paramArr[0];
+    const statusList = processData.getStatusListByTodoList();
+    print.completeShowMessage(viewType, statusList);
+    return true;
+}
+
+const addTodo = (paramArr) => {
+    const randomId = processData.getRandomID();
+    if (randomId === undefined) return print.errorMessage('Id Full');
+    const inputName = paramArr[0];
+    const inputTag  = paramArr[1].split('["').join('').split('"]').join('');
+    const inputData = {'name': inputName, 'tags': [inputTag], 'status': 'todo', 'id': randomId};
+    processData.saveTodoList(inputData, 'add');
+    print.completeAddMessage(inputName, randomId);
+    showCompletionStatus(1000);
+    return true;
+}
+
+const deleteTodo = (paramArr) => {
+    const inputId = parseInt(paramArr[0]);
+    const findId = processData.getTodoId(inputId);
+    if (findId === undefined) {
+        return print.errorMessage('Not Exist Id');
+    } else {
+        const [index, name, status] = processData.getNameAndStatusById(findId);
+        processData.saveTodoList(index, 'delete');
+        print.completeDeleteMessage(name, status);
+        showCompletionStatus(1000);
+        return true;
+    }
+}
+
+const updateTodo = (paramArr) => {
+    const inputId = parseInt(paramArr[0]);
+    const status = paramArr[1];
+    const findId = processData.getTodoId(inputId);
+    if (findId === undefined) {
+        return print.errorMessage('Not Exist Id');
+    } else {
+        const [index, name] = processData.getNameAndStatusById(findId);
+        processData.saveTodoList([index, status], 'update');
+        setTimeout( () => { 
+            print.completeUpdateMessage(name, status);
+            showCompletionStatus(1000); 
+        } , 3000 );
+        return true;
+    }
+}
+
 const processQuery = {
     'show': (paramArr) => {
         if (!checkparamArrLength(paramArr, 1)) {
