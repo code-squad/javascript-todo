391ca85fec8c891ee48bef83971db082a5837950
janghyoseog <bestdevhyo1225@gmail.com>
2019-04-21 13:55:45 +0900

    명령어 및 인자 값들을 체크하는 함수 추가 및 수정

--- Step4_2_control_command.js
+++ Step4_2_control_command.js
@@ -5,13 +5,24 @@ const showCompletionStatus = (milliSecond) => {
     setTimeout( () => { controlCommand('show', ['all']); }, milliSecond );
 }
 
-const checkStatusCommand = (currentStatus) => {
+const checkparamArr = (command, paramArr) => { 
+    switch(command) {
+        case 'show': case 'delete': return paramArr.length === 1;
+        case 'add':  case 'update': return paramArr.length === 2;
+    }
+}
+
+const checkCommand = (currentCommand) => {
+    return ['show', 'add', 'delete', 'update'].some( (command) => { return currentCommand === command } );
+}
+
+const checkStatus = (currentStatus) => {
     return ['all', 'todo', 'doing', 'done'].some( (status) => { return currentStatus === status } );
 }
 
-const checkparamArrLength = (paramArr, checkLength) => { return paramArr.length === checkLength; }
-
 const checkIdForm = (id) => { return isNaN(id) ? false : true; }
+
+const checkTagForm = (tag) => { return tag.match(/(?<=\[\")[a-z]*(?=\"\])/i) !== null; }
 
 const processQuery = {
     'show': (paramArr) => {
