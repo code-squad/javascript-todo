962459b192678491637cfdbc5e5cbbe707edb188
janghyoseog <bestdevhyo1225@gmail.com>
2019-04-21 13:59:15 +0900

    show, add, delete, update를 제어하는 controlCommand 함수 추가

--- Step4_2_control_command.js
+++ Step4_2_control_command.js
@@ -74,118 +74,33 @@ const updateTodo = (paramArr) => {
     }
 }
 
-const processQuery = {
-    'show': (paramArr) => {
-        if (!checkparamArrLength(paramArr, 1)) {
-            console.log(`올바른 명령어가 아닙니다.`);
-            return false;
+const controlCommand = (command, paramArr) => {
+    if (!checkCommand(command))             return print.errorMessage('command');
+    if (!checkparamArr(command, paramArr))  return print.errorMessage('paramArr'); 
+
+    switch(command) {
+        case 'show': 
+        {
+            if (!checkStatus(paramArr[0])) return print.errorMessage('status'); 
+            return showTodoList(paramArr);
         }
-
-        if (!checkStatusCommand(paramArr[0])) {
-            console.log(`${paramArr[0]} 는 존재하지 않는 상태입니다.`);
-            return false;
+        case 'add': 
+        {
+            if(!checkTagForm(paramArr[1])) return print.errorMessage('tag'); 
+            return addTodo(paramArr); 
         }
-
-        let strTodoList  = "";
-        const viewType   = paramArr[0];
-        const statusList = processData.getStatusListByTodoList();
-        if (viewType === 'all') {
-            strTodoList = `현재 상태 : todo: ${statusList['todo'].length}개,`
-                        + `doing: ${statusList['doing'].length}개,`
-                        + `done: ${statusList['done'].length}개`;
-        } else {
-            strTodoList = `${viewType} 리스트 : 총 ${statusList[viewType].length}건:`
-                        + `${statusList[viewType]}`;
+        case 'delete': 
+        {
+            if (!checkIdForm(paramArr[0])) return print.errorMessage('id'); 
+            return deleteTodo(paramArr); 
         }
-
-        console.log(strTodoList);
-        return true;
-    },
-
-    'add': (paramArr) => {
-        if (!checkparamArrLength(paramArr, 2)) {
-            console.log(`올바른 명령어가 아닙니다.`);
-            return false;
+        case 'update': 
+        {
+            if (!checkIdForm(paramArr[0])) return print.errorMessage('id'); 
+            if (!checkStatus(paramArr[1])) return print.errorMessage('status');
+            return updateTodo(paramArr); 
         }
-
-        if(paramArr[1].match(/(?<=\[\")[a-z]*(?=\"\])/i) === null) {
-            console.log(`tag 입력 형식이 잘못되었습니다.`);
-            return false;
-        }
-
-        const id = processData.getRandomID();
-        const inputName = paramArr[0];
-        const inputTag  = paramArr[1].split('["').join('').split('"]').join('');
-        const inputData = {'name': inputName, 'tags': [inputTag], 'status': 'todo', 'id': id};
-        console.log(`${inputName} 1개가 추가되었습니다.(id : ${id})`);
-        processData.saveTodoList(inputData, 'add');
-        // 비동기로 1초 뒤에 show all
-        showCompletionStatus();
-        return true;
-    },
-
-    'delete': (paramArr) => {
-        if (!checkparamArrLength(paramArr, 1)) {
-            console.log(`올바른 명령어가 아닙니다.`);
-            return false;
-        }
-
-        if (!checkIdForm(paramArr[0])) {
-            console.log(`id 형식의 입력이 잘못되었습니다. ( ${paramArr[0]} )`);
-            return false;
-        }
-
-        const id = parseInt(paramArr[0]);
-        const idList = processData.getIdListByTodoList();
-        for(let index = 0; index < idList.length; index++) {
-            if (id === idList[index]) {
-                const [name, status] = processData.getNameAndStatusById(id);
-                console.log(`${name} ${status} 가 목록에서 삭제됐습니다.`);
-                processData.saveTodoList(index, 'delete');
-                // 비동기로 1초 뒤에 show all
-                showCompletionStatus();
-                return true;
-            }
-        }
-
-        console.log(`id ${id} 은(는) 존재하지 않습니다.`);
-    },
-
-    'update': (paramArr) => {
-        if (!checkparamArrLength(paramArr, 2)) {
-            console.log(`올바른 명령어가 아닙니다.`);
-            return false;
-        }
-
-        if (!checkIdForm(paramArr[0])) {
-            console.log(`id 형식의 입력이 잘못되었습니다. ( ${paramArr[0]} )`);
-            return false;
-        }
-
-        if (!checkStatusCommand(paramArr[1])) {
-            console.log(`${paramArr[1]} 는 존재하지 않는 상태입니다.`);
-            return false;
-        }
-
-        const id = parseInt(paramArr[0]);
-        const currentStatus = paramArr[1];
-        const idList = processData.getIdListByTodoList();
-        for (let index = 0; index < idList.length; index++) {
-            if (id === idList[index]) {
-                const [name] = processData.getNameAndStatusById(id);
-                processData.saveTodoList([index, currentStatus], 'update');
-                // 비동기로 3초 뒤에 결과 메시지 출력
-                setTimeout( () => { 
-                    console.log(`${name}가 ${currentStatus} (으)로 상태가 변경됐습니다.`);
-                    // 비동기로 1초 뒤에 show all
-                    showCompletionStatus();
-                } , 3000 );
-                return true;
-            }
-        }
-
-        console.log(`id ${id} 은(는) 존재하지 않습니다.`);
-    },
+    }
 }
 
-module.exports = processQuery;
+module.exports = controlCommand;
