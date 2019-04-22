86b3e6b599694506f0fbc38f06da42ab40ddecf3
seldev <35516239+sel-dev@users.noreply.github.com>
2019-04-22 17:45:27 +0900

    Step4-2 잉글비&Seldev (#97)
    * Fix - show 함수 수정
    
    - show 함수 내부에 init 함수 추가
    - show 함수 내부 res, arrangedData 변수 추가
    - arrangedData에는init()의 결과가 할당됨
    
    * Creat - getRandomInt function
    
    - 10000 이하의 임의의 정수값을 리턴하는 함수 생성
    
    * Creat - isExist function
    
    - todo 데이터 내부에새로생성한 ID와
    동일 ID가 있는지 점검하는 함수
    
    * Fix - isExist -> isIdExistInTodos
    
    * Create - add, delete 함수 추가
    
    - todos 에 객체를 add, delete 하는 함수 추가
    
    * Create - update function
    
    * Create - readline flow
    
    * Fix - data 모듈화
    
    * Fix - 예외처리
    
    * Fix - idCheck, getRandomInt, getTodosByStatus ...
    
    - idCheck -> checkId
    - getRandomInt(max) ,  get max as parameter
    - getTodosByStatus , for of loop -> array filter use
    - show, res 를 block scope 변경, 네이밍 변경(resultOfAll,
        resultOfStatus)
    - deletTodo, forEach 를 find 함수로 변경
    
    * Fix - add 함수 생성자 패턴 적용
    
    - add 내부 함수에 생성자 패턴적용
    
    * Fix - setTimeout 함수 모듈화
    
    - callShowAllAfterSetTime -> 특정시간 이후 showAll을 출력하는 함수
    - printMsgAfterSetTime -> 특정 시간이후 메시지와 함께 결과를 출력하는
    함수
    
    * Fix - typo Mssage -> Msg

---
+++ data.js
@@ -0,0 +1,21 @@
+const todos =  [ 
+    {
+        'name' : '자바스크립트 공부하기', 
+        'tags' : ['programming', 'javascript'],
+        'status' : 'todo',
+        'id' : 12123123
+    },
+                    {
+        'name' : '그림 그리기', 
+        'tags' : ['picture', 'favorite'],
+        'status' : 'doing',
+        'id' : 312323
+    },
+    {
+        'name' : '할게 많다', 
+        'tags' : ['programming', 'javascript'],
+        'status' : 'todo',
+        'id' : 12123143
+    }
+];
+module.exports = todos;
--- todo.js
+++ todo.js
@@ -1,53 +1,145 @@
-//todos.js
+const todos = require("./data")
 
-const todos =  [ 
-    {
-        'name' : '자바스크립트 공부하기', 
-        'tags' : ['programming', 'javascript'],
-        'status' : 'todo',
-        'id' : 12123123
-    },
-                    {
-        'name' : ' 그림 그리기', 
-        'tags' : ['picture', 'favorite'],
-        'status' : 'doing',
-        'id' : 312323
-    }
-    // ....
-];
+const readline=require("readline");
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+    prompt: "명령하세요: "
+});
 
-// const show = (obj) => {
-// ...
-// }
-
-const res = {
-    
-};
-function init(todos){
-    for( obj of todos) {
-       if(res[obj.status] === undefined) {
-           res[obj.status] = [];
-           res[obj.status].push(obj);
-           continue;
-       } 
-        res[obj.status].push(obj);      
-    } 
-    return res;
+const getRandomInt = (max)=> {
+    return Math.floor(Math.random() * Math.floor(max));
 }
-init(todos);
+
+const isIdExistInTodos = (randomInt) => {
+    const checkId = (obj) => obj.id === randomInt;
+    return todos.some(checkId);
+} 
+
+const getId = (max) => {
+    let ID = getRandomInt(max);
+    while(isIdExistInTodos(ID)) {
+        ID = getRandomInt(max);
+    }
+    return ID;
+}
+
+function getTodosSortedByStatus(todos){
+    return {
+        todo : todos.filter(todo => todo.status === "todo"),
+        doing : todos.filter(todo => todo.status === "doing"),
+        done : todos.filter(todo => todo.status === "done")
+    };
+}
+
+const isTodosEmpty = ()=>{
+    if(todos.length === 0) return true;
+    return false  
+}
+
 
 function show(status){
+    const todosSortedByStatus = getTodosSortedByStatus(todos);
     if (status === "all") {
-        for (key in res ) {
-            console.log( key, ": ", res[key].length)
+        if(isTodosEmpty()) console.log("현재 todolist 가 비어있습니다");
+        else {
+            let resultOfAll = "현재상태는"; 
+            for (let key in todosSortedByStatus ) {
+                resultOfAll += " " + key + ": " + todosSortedByStatus[key].length;
+            }
+            console.log(resultOfAll);
         }
+    } else if (!(todosSortedByStatus[status]===undefined)) {
+        const resultOfStatus = todosSortedByStatus[status].map(v => v.name);
+        console.log(resultOfStatus.join(", "));
+    } else {
+        console.log(`${status}가 목록에 없습니다`);
     }
-    if (!(res[status]===undefined)) {
-        console.log(res[status].map(v => v.name).join(", "))
+    rl.prompt()
+}
+
+const callFuncWithMessage = (setTime) => {
+    setTimeout(() => {
+        show("all"); 
+    } ,setTime)
+}
+
+class MakeTodo {
+    constructor(name, id, tag){
+        this.name = name;
+        this.status = "todo";
+        this.id = id;
+        if(!(tag === undefined)) this.tag = tag.match(/[a-z0-9]+/g);
     }
 }
 
-show("all");
-show("todo")
-// show("all");
-// show("todo");
+const add = (inputName, inputTag) => {
+    const todo = new MakeTodo(inputName, getId(10000), inputTag)
+    todos.push(todo);
+    console.log(`${todo.name} 이 1개가 추가되었습니다`)
+    callShowAllAfterSetTime(1000);
+}
+
+const deleteTodo = (id) => {
+    const todo2delete = todos.find(todo => todo.id === Number(id));
+    if(todo2delete === undefined) { 
+        console.log("ID가 목록에 없습니다."); 
+    }else{
+        todos.splice(todos.indexOf(todo2delete),1)[0];
+        console.log(`${todo2delete.name}가 ${todo2delete.status} 목록에서 삭제됐습니다.`)
+    }
+    callShowAllAfterSetTime(1000);
+}
+const seTimeoutWithMessage = (message, setTime)=>{
+        console.log(message);
+        callShowAllAfterSetTime(1000)
+}
+
+const printResultMsgAfterSetTime = (msg,setTime) => {
+    setTime(() => {
+        console.log(msg);
+        callShowAllAfterSetTime(1000);
+    }, setTime)
+}
+
+const update = (id, inputStatus, setTime = 3000) => {
+    if(!/todo|doing|done/.test(inputStatus)){
+        printResultMsgAfterSetTime("변경가능한 status는 todo, doing, done 입니다", setTime);
+        return;
+    }    
+    let todo2update;
+    todos.forEach((todo) => {
+        if(todo.id === Number(id)) {
+            todo.status = inputStatus; 
+            todo2update = todo;
+        }
+    });
+    if(!todo2update === undefined) {
+        printResultMsgAfterSetTime(`${todo2update.name} 가 ${todo2update.status}(으)로 변경되었습니다.`, setTime);
+    } else {
+        printResultMsgAfterSetTime("ID가 목록에 없습니다.", setTime);
+    }
+}
+
+const parseInput = input => input.split("$");
+
+const listenUserInput =  (data) => {
+    const input = parseInput(data);
+    switch (input[0]) {
+        case "add" : add(input[1], input[2]);
+        break;
+        case "update" : update(input[1], input[2]);
+        break;
+        case "delete": deleteTodo(input[1]);
+        break;
+        case "show": show(input[1]);
+        break;
+        default: console.log("입력가능한 명령어는 add, updtade, delete show 입니다"); rl.prompt();
+    }
+}
+// listenUserInput('add$sleep$["favorite", "nodoad"]')
+
+
+
+rl.prompt();
+rl.on("line", listenUserInput);
