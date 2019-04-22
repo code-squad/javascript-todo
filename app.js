fe29963646df07ae681fef0e9927e9aedf87a71a
Minjae Lee <okssud1@naver.com>
2019-04-22 14:53:55 +0900

    수정) Gren&왕민 step4-2 미션 (#87)
    * todo.js refacotiroingTodo.js 파일 통합
    
    * 명령어 입력 구현
    
    * 명령어 실행 함수 작성
    
    입력받은 명령어에 해당하는 함수를 호출해주는 함수 excuteCommand 작성
    
    * add함수 추가
    
    todos 배열에 새로운할일을 추가해주고 랜덤id를 부여해주는 add함수 작성
    
    * delete 함수 구현
    
    todo 목록에서 입력한 id에 해당하는 데이터를 삭제하는 delete 함수 구현
    
    * update 함수 추가
    
    데이터의 id를 조회해 status를 변경해주는 update함수 구현
    
    * executeCommand 함수 개선
    
    함수 add, deleteData, updateData 에 맞게 개선
    
    * file deleted
    
    * excuteCommand 메소드에 정규표현식 적용
    
    조건문을 개선하기위해 [] 리터럴 사용
    정규표현식을 이용하여 명령어 판단
    
    * 함수명 변경
    
    show, add, delete, update 메소드 이름에 data 추가
    
    * 불량인자로인한 에러출력
    
    인자가 잘못되었습니다. 출력후 다시 명령어 입력가능하게 수정
    
    * 랜덤id 로직을 함수화
    
    * 정규표현식 ^ $추가
    
    정확한 명령어를 받기 위해 정규표현식 수정
    
    * 명령어 올바르지못한 인자 에러문구 출력 처리
    
    정규표현식을 이용해서 명령어가 올바르지 않을시 에러문구 출력하도록 수정
    
    * 함수 코드 단순화
    
    arrowfucntion 이용해서 코드 길이 단축
    
    * 최댓값 파라미터로 수정
    
    * id 중복체크 방법 변경
    
    filter 를 이용해서 id중복체크 방식 변경
    목록이 가득 찬 경우 에러메시지 출력
    
    * 일반 함수 arrowfunction 으로 변경
    
    * 일반 함수 arrow function 으로변경
    
    * forEach => reduce로 개선
    
    * 정규표현식 검증 외부 함수 작성
    
    함수를 새로 생성하여 중복되는 코드 제거
    
    * error 메세지 객체 매핑 활용
    
    error 메세지를 객체 매핑을 통해 출력하도록 수정
    
    * forEach 메소드 대신 filter메소드 활용

---
+++ app.js
@@ -0,0 +1,42 @@
+const { log } = console;
+const todoList = require("./todo");
+const myTodoList = new todoList();
+
+const getCommand = () => {
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.on("line", line => {
+        const commandArray = line.split("$");
+        executeCommand(commandArray);
+    }).on("close", () => {
+        process.exit();
+    });
+}
+
+
+const executeCommand = (commandArray) => {
+    const action = commandArray[0];
+    commandArray.shift();
+    const commandParameters = commandArray;
+    const actionReg = /^show$|^add$|^delete$|^update$/;
+    let actionMatchResult = action.match(actionReg);
+    if(actionMatchResult === null){
+        log('명령어가 올바르지 않습니다.');
+        return ;
+    }
+    try {
+        myTodoList[`${actionMatchResult[0]}Data`](...commandParameters);
+    } catch (error) {
+        log(error);
+    }
+};
+
+const main = () => {
+    getCommand();
+}
+
+main();
--- todo.js
+++ todo.js
@@ -1,100 +1,201 @@
+const { log } = console;
 const todos = [
-    {
-        'name': '자바스크립트 공부하기',
-        'tags': ['programming', 'javascript'],
-        'status': 'todo',
-        'id': 12123123
-    },
-    {
-        'name': '그림 그리기',
-        'tags': ['picture', 'favorite'],
-        'status': 'doing',
-        'id': 312323
-    },
-    {
-        'name': '라면 끓여먹기',
-        'tags': ['food', 'favorite'],
-        'status': 'doing',
-        'id': 1541515132
-    },
-    {
-        'name': '칼퇴하기',
-        'tags': ['wolibal', 'favorite'],
-        'status': 'todo',
-        'id': 15132213251
-    },
-    {
-        'name': '알고리즘풀기',
-        'tags': ['study', 'hell'],
-        'status': 'done',
-        'id': 123123312323
-    },
-    {
-        'name': '운동하기',
-        'tags': ['workout', 'hell'],
-        'status': 'todo',
-        'id': 3121234323
-    }
+  {
+    name: "자바스크립트 공부하기",
+    tags: ["programming", "javascript"],
+    status: "todo",
+    id: 423
+  },
+  {
+    name: "그림 그리기",
+    tags: ["picture", "favorite"],
+    status: "doing",
+    id: 1535
+  },
+  {
+    name: "라면 끓여먹기",
+    tags: ["food", "favorite"],
+    status: "doing",
+    id: 9999
+  },
+  {
+    name: "칼퇴하기",
+    tags: ["wolibal", "favorite"],
+    status: "todo",
+    id: 512
+  },
+  {
+    name: "알고리즘풀기",
+    tags: ["study", "hell"],
+    status: "done",
+    id: 2
+  },
+  {
+    name: "운동하기",
+    tags: ["workout", "hell"],
+    status: "todo",
+    id: 77
+  }
 ];
-const show = (status) => {
-    
-    const count = () => {
-        const arr = [0,0,0];
 
-        todos.forEach(function (element) {
-            if (element.status === 'todo') {
-                arr[0]++;
-            } else if (element.status === 'doing') {
-                arr[1]++;
-            } else if (element.status === 'done') {
-                arr[2]++;
-            }
-        });
-        return arr;
-    }
-
-    const getNames = (status) => {
-        const namesArr = [];
-        todos.forEach(
-            function (element) {
-                if (status === element.status) namesArr.push(element.name);
-            }
-        );
-        return namesArr;
-    }
-
-    const printAll = (countArr) => {
-        console.log(`현재상태 : todo : ${countArr[0]}개, doing : ${countArr[1]}, done : ${countArr[2]}`);
-    }
-
-    const printList = (namesArr, status) => {
-        let resultStr =  `${status} 리스트 : 총 ${namesArr.length}건 : `
-        namesArr.forEach(function (element, index) {
-            resultStr += `'${element}'`
-            if (index < namesArr.length - 1) {
-                resultStr += `, `
-            }
-        })
-        console.log(resultStr);
-    }
+const MAX_ID_NUMBER = 10000;
 
 
-    if (status === 'all') {
-        const countArr = count();
-        printAll(countArr);
-    } else if (status === 'doing' || status === 'done' || status === 'todo') {
-        const namesArr = getNames(status);
-        printList(namesArr, status);
-    } else {
-        console.log(`show 의 인자는 doing ,todo, done, all 만 가능합니다.`);
-        return;
-    }
+const getRandomId = maxIdNumber => {
+  let id;
+  let loopCount = 0;
+  while (true) {
+
+    id = Math.floor(Math.random() * maxIdNumber) + 1;
+    if (todos.filter(element => element.id === id).length === 0) break;
+    if (++loopCount > MAX_ID_NUMBER) throw Error("할일 목록이 가득 찼습니다.");
+
+  }
+  return id;
 }
 
+const getMatchResult = (string ,regExp) => string.match(regExp);
 
+const MSG = {
+  SHOW_PARAM_ERR : `show 명령어 인자가 잘못 되었습니다.`,
+  ADD_PARAM_NAME_ERR : `add 명령어의 인자 name이 잘못 되었습니다.`,
+  ADD_PARAM_TAG_ERR : `add 명령어의 인자 tag형태가 잘못 되었습니다. example: ["tagname"]`,
+  DELETE_PARAM_ERR : `delete명령어의 인자가 잘못 되었습니다.`,
+  UPDATE_PARAM_ERR : `update명령어의 인자가 잘못 되었습니다.`
+}
 
-show('all');
-show('todo');
-show('doing');
-show('done');
-show('too');
+module.exports = class TodoList {
+  count() {
+    const countObj = {
+      todo: 0,
+      doing: 0,
+      done: 0
+    }
+    todos.forEach(element => {
+      const { status } = element;
+      countObj[status] += 1;
+    });
+    return countObj;
+  }
+
+  printAll(countObj) {
+    let resultStr;
+    resultStr = `현재상태 : todo : ${countObj.todo}개, doing : ${
+      countObj.doing
+      }개, done : ${countObj.done}개`;
+    log(resultStr);
+  }
+  getNames(status) {
+    const namesArr = todos
+      .filter(element => status === element.status)
+      .map(element => element.name);
+    return namesArr;
+  }
+  printList(namesArr, status) {
+    let baseStr = `${status} 리스트 : 총 ${namesArr.length}건 : `;
+    let resultStr = namesArr.reduce((accumulator, currentName) => {
+      if(baseStr === accumulator){
+        return accumulator + currentName;
+      }else{
+        return accumulator +" , "+currentName;
+      }
+    }, baseStr);
+    log(resultStr);
+  }
+  
+  showData(status) {
+    const statusReg = /^all$|^todo$|^doing$|^done$/;
+    const matchResult = getMatchResult(`${status}`,statusReg);
+    if (matchResult === null) {
+      log(MSG.SHOW_PARAM_ERR);
+      return;
+    }
+
+    if (matchResult[0] === 'all') {
+      const countObj = this.count();
+      this.printAll(countObj);
+    } else {
+      const namesArr = this.getNames(status);
+      this.printList(namesArr, status);
+    }
+  }
+
+  addData(name, tags) {
+    const nameReg = /^\s+$/;
+    const tagsReg = /^\[\"\S+\"\]$/;
+    const nameMatchResult = getMatchResult(`${name}`,nameReg);
+    const tagsMatchResult = getMatchResult(`${tags}`,tagsReg);
+
+    if (nameMatchResult !== null || name === "") {
+      log(MSG.ADD_PARAM_NAME_ERR);
+      return;
+    }
+    if (tagsMatchResult === null) {
+      log(MSG.ADD_PARAM_TAG_ERR);
+      return;
+    }
+    const id = getRandomId(MAX_ID_NUMBER);
+
+    const todoObj = {
+      "name": name,
+      "tags": tags,
+      "status": "todo",
+      "id": id
+    }
+    todos.push(todoObj);
+    log(`${name} 1개가 추가됐습니다.(id : ${id})`);
+    setTimeout(() => {
+      this.showData("all");
+    }, 1000);
+  }
+
+  deleteData(id) {j
+    const numId = parseInt(id);
+    if (!Number.isFinite(numId)) {
+      log(MSG.DELETE_PARAM_ERR);
+      return;
+    }
+    let indexOfDeletingObj;
+
+    const deletingObj = todos.filter((element,index) => {
+      indexOfDeletingObj = index;
+      return element.id === numId
+    })[0];
+
+    if(deletingObj === undefined){
+      log(MSG.DELETE_PARAM_ERR);
+      return;
+    }
+    todos.splice(indexOfDeletingObj, 1);
+    
+    log(`${deletingObj.name}이(가) ${deletingObj.status} 목록에서 삭제됐습니다.`);
+    setTimeout(() => {
+      this.showData("all");
+    }, 1000);
+  };
+
+  updateData(id, status) {
+    const statusReg = /^todo$|^doing$|^done$/;
+    const matchResult = getMatchResult(`${status}`,statusReg);
+    const numId = parseInt(id);
+    if (!Number.isFinite(numId) || matchResult === null) {
+      log(MSG.UPDATE_PARAM_ERR);
+      return;
+    }
+    let name;
+    todos.forEach(element => {
+      if (element.id === numId) {
+        element.status = status;
+        name = element.name;
+      }
+    });
+
+    setTimeout(() => {
+      log(`${name}이(가) ${status}로 상태가 변경됐습니다.`)
+      setTimeout(() => {
+        this.showData("all");
+      }, 1000);
+    }, 3000);
+  }
+}
+
