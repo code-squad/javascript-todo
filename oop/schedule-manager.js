7314e6b93bb6631d21432a52c3911c58486a5848
shun <koownusmik@gmail.com>
2019-04-23 22:53:21 +0900

    feat: 설계를 바탕으로 수도코드 작성

---
+++ oop/schedule-manager.js
@@ -0,0 +1,87 @@
+// 데이터베이스에서 스케쥴배열을 가져온다.
+const schedule_list = require('./data');
+
+function App(){
+
+}
+
+App.prototype.run = function(){
+    // 콘솔에서 입력받아서 parseCommand 함수를 실행하도록 한다.
+    // 파즈커맨드에서 반환받은 값을 받탕으로 컨트롤러를 실행한다.
+    
+    // exit을 입력받을때까지 끊임없이 동작하도록 한다.
+}
+
+// show$all
+// show$todo
+// add$name$tag
+// update$id$status
+// delete$id
+
+App.prototype.parseCommand = function(input){
+    // run 에서 입력값을 받아서 입력값이 첫번쨰 커맨드를 key로 받고, 나머지를 key에 들어갈 인자로 분류한다.
+    // key에 맞는 명령을 컨트롤러에서 실행하도록 하고, 이떄 분류한 나머지를 컨트롤러의 함수의 인자로 들어가도록한다. 
+}
+
+
+
+
+function Controller(){
+}
+
+Controller.prototype.todoObject(name,tag) = {
+    id : this.getUniqueId(),
+    name: name,
+    tag: tag,
+    status: todo
+}
+
+//show$all
+//show$todo
+Controller.prototype.showTodo = function (status){
+    // 인자를 분류하여 Show 객체의 showAll 또는 showFilter를 실행시켜준다.
+    // showFilter에는 status를 인자로 넘겨준다.
+}
+
+// add$name$tag명
+Controller.prototype.addTodo = function(name,tag){
+    // parseCommand로 넘어온 입력값을 name, tag로 받아서 새로운 newtodoObject를 생성한다.
+    const newTodoObject = new Controller.prototype.todoObject(name,tag); 
+    // database에서 가져온 데이터 배열에 push한다. 
+}
+
+//update$id$status
+Controller.prototype.updateTodo = function(id,status){
+    // 넘어온 id에 맞는 schedule배열의 인자객체를 찾는다.
+    //  그 인자객체의 status를 변경한다. 
+}
+
+Controller.prototype.deleteTodo = function(id){
+    // 넘어온 id에 맞는 schedule 배열의 인자객체를 찾는다.
+    // 그 인자객체를 삭제한다. 
+}
+
+Controller.prototype.getUniqueId = function(){
+    // 유니크한  숫자를 만들고 반환한다. 
+}
+
+function Show(){
+
+}
+
+Show.prototype.showAll = function(){
+    // schdule_list에서 상태(todo,doing,done)에 맞게 값을 가지고 있는 객체,또는 배열을 만든다.
+    // 객체또는 배열의 인자를  한줄로 출력한다. 
+}
+
+// show$status
+Show.prototype.showFiltered = function(status){
+    // scheduled_list에서 넘겨받은 status를 가지고 있는 객체를 찾아 배열로 만든다.
+    // 배열의 인자들을 한줄로 출력한다. 
+}
+
+const show = new Show();
+const controller = new Controller();
+const schedule_manager = new App();
+
+schedule_manager.run();
