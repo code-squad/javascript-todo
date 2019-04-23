03292ef26dab39a1cc005495e88f98508087df0c
mukeunzi <eunji057@gmail.com>
2019-04-23 17:40:45 +0900

    todo class 수도코드 작성
    - 수도코드 작성
    - exports

--- oop/todo.js
+++ oop/todo.js
@@ -1,21 +1,107 @@
-const Todo = class {
+module.exports = class todo {
 	// show
 	//매개변수 all, status(todo,done,doing)
 	// 매개변수에 해당하는 데이터 출력
+	show([element], rl) {
+		reg = /all todo doing done/
+		match == null
+		console error();
+		
+		if element === all{
+			printall();
+		}
+		printlist(element);
+
+	}
+
+	printall(){
+		console.log(todo, doing, done)
+	}
+
+	printList(status){
+		console.log(status)
+	}
 	// add
 	// 매개변수 name,tag
 	// id생성
 	// name과 id tag 로 만들어진 데이터 추가
 	// showall 1초뒤에 실행
+	add([name, tag],rl){
+		add$name$["tag1"]
+		tag !==tag 
+		console err
+
+		id = getid();
+		data.push({
+			name
+			id
+			tag
+		}
+
+		console.log(name, id, tag)
+
+		settimeout(){
+			printall();
+		1000}
+
+	}
+
+	getid(){
+		return Math.random();
+	}
+
+
 	// delete
 	// 매개변수 id
 	// id에 해당하는 데이터 삭제
 	// showall 1초뒤에 실행
 	// 존재하지 않는 id경우 출력
+
+	delete([id],rl){
+		index = findIndex(id)
+		if(id === undefined){
+			err
+			console.log(존재하지 않음)
+			rl.prompt()
+		}
+		name = data.splice(index)
+
+		console.log(name, id)
+
+		settimeout(){
+			printall();
+		1000}
+	}
+
+	findIndex(id){
+		if(data.id === id){
+			return indexOfData;
+		}
+	}
+
 	// update
 	// 매개변수 id, status
 	// id에 해당하는 데이터를 status로 상태 변경
 	// 3초 뒤에 name 이 status 로 변경되었ㅅ 출력
 	// 1초뒤 show all
 	// 존재하지 않는 id경우, 같은상태로 update경우 안내 출력
+
+	update([id, status], rl){
+		index = findIndex(id)
+		if(id === undefined){
+			err
+			console.log(존재하지 않음)
+			rl.prompt()
+		}
+		data.index.status = status;
+
+		settimeout(3000);
+		console.log(name, status)
+		settimeout(1000)
+		showall()
+	}
+
+	printError(element){
+		console element;
+	}
 };
