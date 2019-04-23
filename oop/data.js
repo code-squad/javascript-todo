126986c2857e9e3540ba566874915a1f5a317367
shun <koownusmik@gmail.com>
2019-04-23 22:51:44 +0900

    feat: todos 데이터가 담긴 data.js파일 생성

---
+++ oop/data.js
@@ -0,0 +1,14 @@
+const todos =  [ 
+    {
+        name : '자바스크립트 공부하기', 
+        tags : ['programming', 'javascript'],
+        status : 'todo',
+        id : 12123123
+    },
+                    {
+        name : ' 그림 그리기', 
+        tags : ['picture', 'favorite'],
+        status : 'doing',
+        id : 312323
+    }
+];
