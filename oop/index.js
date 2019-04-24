a2bb69adbf893b77c103347cd55d3db8e2b50e10
eugene94 <eugenekang94@gmail.com>
2019-04-24 11:40:01 +0900

    fix dummy data
    - dummy Data를 요구사항의 데이터로 수정

--- oop/index.js
+++ oop/index.js
@@ -3,10 +3,11 @@ const ManagingTodo = require('./managingTodo');
 
 const managingTodo = new ManagingTodo();
 
+managingTodo.addTodo(new Todo('자바스크립트 공부하기', ['programming', 'javascript'], 'todo'));
+managingTodo.addTodo(new Todo('그림 그리기', ['picture', 'favorite'], 'doing'));
 managingTodo.addTodo(new Todo('꽃구경하기', ['flower', 'favorite'], 'done'));
 managingTodo.addTodo(new Todo('저녁식사', ['dinner', 'food'], 'todo'));
-managingTodo.addTodo(new Todo('저녁식사', ['dinner', 'food'], 'todo'));
-managingTodo.addTodo(new Todo('저녁식사', ['dinner', 'food'], 'todo'));
+managingTodo.addTodo(new Todo('커피마시기', ['coffee', 'favorite'], 'doing'));
 
-console.log(managingTodo.showStatus('todo'));
+console.log(managingTodo.showStatus('doing'));
 console.log(managingTodo.showAll());
