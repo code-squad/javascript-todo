a2bf5e16a1d1cf578daeedb3617be838c2669d69
sonypark <sonypark0204@gmail.com>
2019-04-24 21:52:35 +0900

    feat: Create addTodoExecutor in TodoUI.prototype

---
+++ oop/oopTodo.js
@@ -0,0 +1,40 @@
+TodoUI = () => {};
+
+
+TodoUI.prototype.updateStatus = (id, status) => {
+
+};
+
+
+TodoUI.prototype.showTodoList = (status) => {
+
+};
+
+
+TodoUI.prototype.deleteTodoList = (id) => {
+
+};
+
+
+TodoUI.prototype.addTodoList = (todoFactor, todoTag) => {
+
+
+};
+
+TodoUI.prototype.printResult = () => {
+
+
+};
+
+
+Tools = () =>{};
+
+Tools.prototype.CheckError = () => {
+
+}
+
+Tools.prototype.splitInputVal = () => {
+
+}
+
+
