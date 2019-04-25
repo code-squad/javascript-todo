06f0dc4705124d9848d6bca78390da6811c69b16
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-25 14:11:08 +0900

    add the program constructor

--- oop/app.js
+++ oop/app.js
@@ -3,3 +3,18 @@ const readLine = require('readline').createInterface( {
 	output:process.stdout,
 });
 
+Program.prototype = {
+
+    runProgram : (readline) => {
+       readline.setPrompt('명령하세요: ');
+       readline.prompt();
+       readline.on('line', (userInput) => {	           
+           readLine.prompt();
+
+       }).on('close', () => {
+           console.log("프로그램을 종료합니다.");
+           process.exit();
+       });
+   }
+
+};
