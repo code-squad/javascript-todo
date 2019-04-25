06a41e587d21c3c9a68d4b65b1dd7c045d02efd4
mukeunzi <eunji057@gmail.com>
2019-04-25 18:10:56 +0900

    에러출력 방식 변경
    - try catch throw 활용
    - errorMessage require

--- oop/app.js
+++ oop/app.js
@@ -1,12 +1,6 @@
 const todo = module.require('./todo.js');
-// 입력받는 함수
-// 프롬프트 사용
-// 데이터 파싱
-// $ 없는경우 에러처리
-// 파싱 불가능한 경우 에러처리
-// 명령호출을 호출
-// const inputCommand = () => {
 const readline = require('readline');
+const errorMessage = module.require('./errorMessage.js');
 
 const rl = readline.createInterface({
 	input: process.stdin,
@@ -25,21 +19,21 @@ rl.on('line', input => {
 	const action = inputArray.splice(0, 1)[0];
 	const condition = inputArray;
 
-	excuteTodo(action, condition, rl);
+	excuteTodo(action, condition);
 });
-// };
-// 명령어 호출해주는 함수
-// 입력받은 명령을 호출
 
 const excuteTodo = (action, condition) => {
 	const regExp = /^show$|^add$|^delete$|^update$/;
 	const matchRegExp = action.match(regExp);
-	if (matchRegExp === null) {
-		myTodo.printError('COMMAND_ERROR');
-	} else {
-		myTodo[action](...condition);
-	}
-	// readline.prompt();
-};
 
-// inputCommand();
+	try {
+		if (matchRegExp === null) {
+			throw new Error(errorMessage.COMMAND_ERROR);
+		} else {
+			myTodo[action](...condition);
+		}
+	} catch (e) {
+		console.log(e.message);
+		rl.prompt();
+	}
+};
