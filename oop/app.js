bda8af1bdf7ee562b5fd69a8c5b3d017aa204581
PAPICO <ocipap0531@gmail.com>
2019-04-25 14:13:50 +0900

    feat : Create command format

--- oop/app.js
+++ oop/app.js
@@ -1,1 +1,22 @@
+const readline = require("readline")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.setPrompt("명령어를 입력하세요 : ")
+rl.prompt()
+
+rl.on("line", (command) => {
+    if (command === "quit") {
+        rl.close()
+    } 
+    else {
+        console.log(command)
+        rl.prompt()
+    }
+}).on("close", () => {
+    process.exit()
+})
+
 
