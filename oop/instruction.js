132ad2d29709329252843b088ade3298ad8b6625
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-25 15:09:11 +0900

    add the show function to show status of data

--- oop/instruction.js
+++ oop/instruction.js
@@ -8,7 +8,32 @@ function Instruction() {
 
 Instruction.prototype = {
 
-    show : (status) => {console.log(status, "this is show method");},
+    show : (status) => {
+        const statusArr = ['all', 'todo', 'doing', 'done'];
+		
+		if (status === 'all') {
+			let [numOfTodos, numOfDoings, numOfDones] = [0,0,0];		
+			
+			convertedData.forEach((value) => {
+				
+				if (value.status === 'todo') numOfTodos++;
+				else if (value.status === 'doing') numOfDoings++;
+				else if (value.status === 'done') numOfDones++;				
+				
+			});
+			
+            console.log(`현재상태 : todo: ${numOfTodos}개, doing: ${numOfDoings}개, done: ${numOfDones}개`);
+
+		} else if (statusArr.includes(status)) {
+			const tasks = Utils.prototype.getArrByCondition(convertedData, (val) => { return (status === val.status);});
+			let message = `${status}리스트 총 ${tasks.length}건 : `;
+			tasks.forEach( obj => {
+				 message += `'${obj.name}, ${obj.id}번,' `;
+			});
+			
+			console.log(message);
+		}
+    },
     add : () => {},
     delete : () => {},
     update :() => {}
