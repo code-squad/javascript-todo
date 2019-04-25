2fb6fa8b4bfc2b010c4d638b711c708b3eb9a5f6
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-25 14:14:43 +0900

    add the executeCmd function Object : to execute the command line

--- oop/commandParser.js
+++ oop/commandParser.js
@@ -3,10 +3,22 @@ function CommandParser () {
 }
 
 CommandParser.prototype = {
-	getCmdList : (input) => {
+    
+    getCmdList : (input) => {
     const regexp = /[^\$]+/g;
     return input.match(regexp);
-	},
+    },
+    
+	executeCmd : (command) => {
+		
+		if (command.length === 2) {
+			Instruction.prototype[command[0]](command[1]);
+		} else if (command.length === 3) {
+			Instruction.prototype[command[0]](command[1], command[2]);
+		} else {
+			// 예외처리
+		}
+	}
 };
 
 module.exports = CommandParser;
