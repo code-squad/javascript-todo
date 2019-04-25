c6c18dcfbf917c265b3413555c8f7e7222657eff
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-25 18:26:36 +0900

    add the delay function : utils.js
    add the Promise function to delay the function

--- oop/utils.js
+++ oop/utils.js
@@ -13,9 +13,13 @@ Utils.prototype = {
 
     getRadomId : (max, min) => {
         Math.floor(Math.random() * (max-min)) + 1;
-    } 
+    },
 
-
+    delay : (time) => {
+		return new Promise(function(resolve, reject){
+			setTimeout(resolve, time);
+		});
+	},
 };
 
 module.exports = Utils;
