bc8f795f738b162db2e4bd5faee50ee612517376
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-25 16:35:26 +0900

    add the adding object function

--- oop/instruction.js
+++ oop/instruction.js
@@ -3,41 +3,54 @@ const convertedData = JSON.parse(JSON.stringify(originData)).data;
 const Utils = require('./utils.js');
 
 function Instruction() {
-	
+
 }
 
 Instruction.prototype = {
 
-    show : (status) => {
+    show: (status) => {
         const statusArr = ['all', 'todo', 'doing', 'done'];
-		
-		if (status === 'all') {
-			let [numOfTodos, numOfDoings, numOfDones] = [0,0,0];		
-			
-			convertedData.forEach((value) => {
-				
-				if (value.status === 'todo') numOfTodos++;
-				else if (value.status === 'doing') numOfDoings++;
-				else if (value.status === 'done') numOfDones++;				
-				
-			});
-			
+
+        if (status === 'all') {
+            let [numOfTodos, numOfDoings, numOfDones] = [0, 0, 0];
+
+            convertedData.forEach((value) => {
+
+                if (value.status === 'todo') numOfTodos++;
+                else if (value.status === 'doing') numOfDoings++;
+                else if (value.status === 'done') numOfDones++;
+
+            });
+
             console.log(`현재상태 : todo: ${numOfTodos}개, doing: ${numOfDoings}개, done: ${numOfDones}개`);
 
-		} else if (statusArr.includes(status)) {
-			const tasks = Utils.prototype.getArrByCondition(convertedData, (val) => { return (status === val.status);});
-			let message = `${status}리스트 총 ${tasks.length}건 : `;
-			tasks.forEach( obj => {
-				 message += `'${obj.name}, ${obj.id}번,' `;
-			});
-			
-			console.log(message);
-		}
+        } else if (statusArr.includes(status)) {
+            const tasks = Utils.prototype.getArrByCondition(convertedData, (val) => {
+                return (status === val.status);
+            });
+            let message = `${status}리스트 총 ${tasks.length}건 : `;
+            tasks.forEach(obj => {
+                message += `'${obj.name}, ${obj.id}번,' `;
+            });
+
+            console.log(message);
+        }
     },
-    
-    add : () => {},
-    delete : () => {},
-    update :() => {}
+
+    add: (name, tags) => {
+        const id = Utils.prototype.getRadomId(99999, 1);
+        let obj = {
+            name,
+            tags,
+            status: 'todo',
+            id,
+        };
+        convertedData.push(obj);
+        console.log(`${obj.name} 1개가 추가됐습니다.(id : ${obj.id})`);
+    },
+
+    delete: () => {},
+    update: () => {}
 };
 
 module.exports = Instruction;
