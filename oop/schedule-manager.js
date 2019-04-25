19c15ba57a03304d9233d899a05c7a6bd1f19fac
shun <koownusmik@gmail.com>
2019-04-25 16:33:19 +0900

    feat: showAll 1초지연, update 3 지연 기능구현
    1. delayShow 함수 구현
    async,await을 이용해서 run함수 내부에서 순차적으로 살행되도록 구현
    이떄 await을 사용하기 위해 Promise객체를 return 하고  Promise 객체 내부에서 showAll, run 함수를 실행
    Promise객체 안에서 this가 Promise객체를 가리키는 문제가 발생하여 showAll, run을 실행하기 위해 this를 self변수에 미리 저장후 사용함.
    Viewer.prototype.showAll()과 App.prototype.run()을 통해 showAll, run을 실행하려고 했으나 실패함.
    
    2.updateMessage에서 Promise 객체를return 하도록 구현

--- oop/schedule-manager.js
+++ oop/schedule-manager.js
@@ -3,59 +3,55 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
-// 데이터베이스에서 스케쥴배열을 가져온다.
-const { log } = console;
+});
 
 function App() {
     this.editor = new Editor();
     this.viewer = new Viewer();
 }
 
-// 'show$todo'
-// 'add$운동하기$exercise'
-// 'update$7$doing'
-// 'delete$7'
+// show$todo
+// add$운동하기$exercise
+// update$7$doing
+// delete$7
+
 
 App.prototype = {
-    run() {
-        // 콘솔에서 입력받아서 parseCommand 함수를 실행하도록 한다.
-        // 파즈커맨드에서 반환받은 값을 받탕으로 컨트롤러를 실행한다.
-        
 
-        rl.question('명령을 입력하세요: ', (input)=>{
-            if(input === 'q') return rl.close();
-            [key, ...message] = this.parseCommand(input)
+    run() {
+        const self = this
+
+        function delayShow(){
+            return new Promise(function(resolve, reject){
+                setTimeout(() => {
+                    self.viewer.showAll()
+                    self.run();
+                    resolve();
+                }, 1000);
+
+            })
+        }
+
+        rl.question('명령을 입력하세요: ', async (input) => {
+            if (input === 'q') return rl.close();
+            [key, ...message] = self.parseCommand(input)
             if (key === "show") {
                 [status] = message
-                status === "all" ? this.viewer.showAll() : this.viewer.showFiltered(status);
-                return this.run();
+                status === "all" ? self.viewer.showAll() : self.viewer.showFiltered(status);
+                return self.run();
             }
+           
+            const selectedObject = self.editor[key + 'Todo'](...message);
             
-            const selectedObject =  this.editor[key + 'Todo'](...message);
-            this.viewer[key + 'Message'](selectedObject);
-            this.viewer.showAll()
-            // exit을 입력받을때까지 끊임없이 동작하도록 한다.
-            this.run();
+            await self.viewer[key + 'Message'](selectedObject)
+            await delayShow();
         });
     },
-    // show$all
-    // show$todo
-    // add$name$tag
-    // update$id$status
-    // delete$id
 
-    // run 에서 입력값을 받아서 입력값이 첫번쨰 커맨드를 key로 받고, 나머지를 key에 들어갈 인자로 분류한다.
-    // key에 맞는 명령을 컨트롤러에서 실행하도록 하고, 이떄 분류한 나머지를 컨트롤러의 함수의 인자로 들어가도록한다. 
     parseCommand(input) {
         return input.split('$');
     }
-
 }
-
-
-
-
 
 
 function Editor() { }
@@ -66,7 +62,6 @@ Editor.prototype = {
         this.tag = _tag
         this.status = 'todo'
         this.id = _newId
-
     },
 
     addTodo(_name, _tag) {
@@ -75,25 +70,23 @@ Editor.prototype = {
         schedule_list.push(newTodoObject);
         return newTodoObject
     },
-    //update$id$status
+
     updateTodo(_id, _status) {
-        // 넘어온 id에 맞는 schedule배열의 인자객체를 찾는다.
-        //  그 인자객체의 status를 변경한다. 
+
         schedule_list.some(todo => {
             if (todo.id === parseInt(_id)) return todo.status = _status
         });
 
-        return schedule_list.find(todo=>todo.id === parseInt(_id));
+        return schedule_list.find(todo => todo.id === parseInt(_id));
 
     },
 
     deleteTodo(_id) {
-        // 넘어온 id에 맞는 schedule 배열의 인자객체를 찾는다.
-        // 그 인자객체를 삭제한다.
-        const returnedObj = schedule_list.find(todo=>todo.id === parseInt(_id));
 
-        schedule_list.some((todo,index) => {
-            if (todo.id === parseInt(_id)) return schedule_list.splice(index,1)
+        const returnedObj = schedule_list.find(todo => todo.id === parseInt(_id));
+
+        schedule_list.some((todo, index) => {
+            if (todo.id === parseInt(_id)) return schedule_list.splice(index, 1)
         });
         return returnedObj;
 
@@ -101,24 +94,16 @@ Editor.prototype = {
     },
 
     getUniqueId() {
-        // 유니크한  숫자를 만들고 반환한다.
         return Date.now()
     }
 }
 
 
 
-
-
-
-
 function Viewer() { }
 
-//show$all
 Viewer.prototype = {
     showAll() {
-        // schdule_list에서 상태(todo,doing,done)에 맞게 값을 가지고 있는 객체,또는 배열을 만든다.
-        // 객체또는 배열의 인자를  한줄로 출력한다. 
         const statusBox = schedule_list.reduce((accum, curr) => {
             accum[curr.status] = ++accum[curr.status] || 1;
             return accum;
@@ -129,37 +114,27 @@ Viewer.prototype = {
         console.log(`현재상태 : ${showAllResult}`);
     },
 
-    // show$status
     showFiltered(_status) {
-        // run에서 넘겨받은 status를  scheduled_list에서 가지고 있는 객체를 찾아 배열로 만든다.
-
         const showFilteredResult = schedule_list.filter(todo => todo.status === _status)
             .map(todo => `'${todo.name}, ${todo.id}번'`);
-
-        // 배열의 인자들을 한줄로 출력한다. 
         console.log(`${_status}리스트 : 총${showFilteredResult.length}건 : ${showFilteredResult.join(', ')}`);
 
     },
     addMessage(_obj) {
-        // 인자로 넘어온 name에 맞는 id를 찾으면 됨. 
         console.log(`${_obj.name}가 추가되었습니다. (id :${_obj.id})`);
     },
     updateMessage(_obj) {
-        // 인자로 넘어온 id의 이름만 찾으면 됨
-        console.log(`${_obj.name}의 상태가 ${_obj.status}로 변경되었습니다.`);
+        return new Promise(function(resolve, reject){
+            setTimeout(() => {
+                console.log(`${_obj.name}의 상태가 ${_obj.status}로 변경되었습니다.`);
+                resolve();
+            }, 3000)
+        })
     },
     deleteMessage(_obj) {
-        // 인자로 넘어온 id에 맞는 name, status를 찾으면 됨
         console.log(`${_obj.name} ${_obj.status}가 목록에서 삭제 되었습니다.`);
     }
-
 }
 
 const schedule_manager = new App();
-
-// schedule_manager.run('show$all');
-// schedule_manager.run('show$todo');
-// schedule_manager.run('add$q운동하기$exercise');
-// schedule_manager.run('update$7$done');
-// schedule_manager.run('delete$7');
 schedule_manager.run();
