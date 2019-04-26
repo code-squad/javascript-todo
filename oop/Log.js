1e9992e0000169b7387fd06d0272cc6ca16c0a23
mukeunzi <eunji057@gmail.com>
2019-04-26 15:52:44 +0900

    feat : add addLog method
    - addLog 메소드 추가

--- oop/Log.js
+++ oop/Log.js
@@ -1,54 +1,51 @@
 class Log {
-  constructor(){
-    this.queue = [],
-    this.index = -1
-  }
+	constructor() {
+		(this.queue = []), (this.index = -1);
+	}
 
+	addLog(action, prevData, nextData) {
+		if (this.queue.length > 4) {
+			this.queue.shift();
+		}
 
-  push(stinrg action, obj prevdata, obj nextData){
-    if queue.length >4){
-      shift();
-    }
-    queue.push(action, prevData, nextData)
-    index = queue.length-1
-  }
+		while (this.queue.length > this.index + 1) {
+			this.queue.pop();
+		}
 
-  shift(){
-    queue.shift()
-  }
+		this.queue[++this.index] = {
+			action: action,
+			prevData: prevData,
+			nextData: nextData
+		};
+	}
 
-  undo(){
-    if( index < 0)
-    error 되돌릴수 없습니다
+	// undo(){
+	//   if( index < 0)
+	//   error 되돌릴수 없습니다
 
+	//   index -> queue{
+	//     action
+	//     prevData
+	//     nextData
+	//   }
 
-    index -> queue{ 
-      action
-      prevData
-      nextData
-    }
+	//   prevData -> todolist
 
-    prevData -> todolist 
-    
-    //delete일때
-    console.log( id, name, nextstatus, prevstatus)
-    
-    index --;
+	//   //delete일때
+	//   console.log( id, name, nextstatus, prevstatus)
 
-  }
+	//   index --;
 
-  redo(){
-    if index >= length-1
-    error redo 할수 없습니다
+	// }
 
-    index++
-    
-    nextData -> todolist
-    console.log( id, name, prevData , nextData);
+	// redo(){
+	//   if index >= length-1
+	//   error redo 할수 없습니다
 
+	//   index++
 
-  }
+	//   nextData -> todolist
+	//   console.log( id, name, prevData , nextData);
 
-
-  
+	// }
 }
