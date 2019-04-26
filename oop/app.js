1fe5f21cf186d21ecefa44293c435a5f2dce5678
nailerHeum <kingman330@gmail.com>
2019-04-26 10:15:36 +0900

    Get seldev's oop project
    셀데브의 파일 갖고옴
    app.js, data.json, IController.js, todo.js

--- oop/app.js
+++ oop/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const InputController = require("./IController");
 const TodoHandler = require("./todoHandler");
 const TodoChecker = require('./todoChecker')
@@ -39,3 +40,28 @@ process.on("userInput", async (command, args) =>{
 })
 
 ready();
+=======
+const IC = require("./IController");
+const Todo = require("./todo");
+const fs = require('fs');
+
+const data = fs.readFileSync("./data.json")
+const todos = JSON.parse(data)
+console.log(todos)
+
+const todoList = new Todo(todos);
+process.on("userInput", (userInput) =>{
+    console.log(userInput);
+    let command = userInput[0]
+    let args = userInput.slice(1)
+    todoList[command](...args)
+    console.log(todoList.toods)
+    IC.rl.prompt();
+    
+})
+
+
+
+//inputControll.inputHandler("show$all");
+IC.ready();
+>>>>>>> Get seldev's oop project
