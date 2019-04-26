c8b894d48f8dcdc44dacd335db7f015d02018feb
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-26 12:40:12 +0900

    show 함수 리팩토링 : 내부 분기를 everyStatus, sigleStatus function 분리

--- oop/instruction.js
+++ oop/instruction.js
@@ -11,63 +11,33 @@ function Instruction() {
 
 Instruction.prototype = {
 
-    show: (status) => {
-        const statusArr = ['all', 'todo', 'doing', 'done'];
-
-        if (status === 'all') {
-            let [numOfTodos, numOfDoings, numOfDones] = [0, 0, 0];
-
-            convertedData.forEach((value) => {
-
-                if (value.status === 'todo') numOfTodos++;
-                else if (value.status === 'doing') numOfDoings++;
-                else if (value.status === 'done') numOfDones++;
-
-            });
-
-            console.log(`현재상태 : todo: ${numOfTodos}개, doing: ${numOfDoings}개, done: ${numOfDones}개`);
-
-        } else if (statusArr.includes(status)) {
-            const tasks = Utils.prototype.getArrByCondition(convertedData, (val) => {
-                return (status === val.status);
-            });
-            let message = `${status}리스트 총 ${tasks.length}건 : `;
-            tasks.forEach(obj => {
-                message += `'${obj.name}, ${obj.id}번,' `;
-            });
-
-            console.log(message);
-        }
-    },
-
-    add: (name, tags) => {
-        const id = Utils.prototype.getRadomId(99999, 1);
-        let obj = {
-            name,
-            tags,
-            status: 'todo',
-            id,
-        };
-        convertedData.push(obj);
-        const message = `${obj.name} 1개가 추가됐습니다.(id : ${obj.id})`;
+    everyStatus (convertedData) {
+		let [numOfTodos, numOfDoings, numOfDones] = [0,0,0];		
+		convertedData.forEach((value) => {
+			if (value.status === 'todo') numOfTodos++;
+			else if (value.status === 'doing') numOfDoings++;
+			else if (value.status === 'done') numOfDones++;				
+		});
+		console.log(`현재상태 : todo: ${numOfTodos}개, doing: ${numOfDoings}개, done: ${numOfDones}개`);
+	},
+	
+	singleStatus (convertedData, status) {
+		const tasks = this.utils.getArrByCondition(convertedData, (val) => { return (status === val.status);});
+		let message = `${status}리스트 총 ${tasks.length}건 : `;
+		tasks.forEach( obj => {
+			 message += `'${obj.name}, ${obj.id}번,' `;
+		});
 		console.log(message);
-    },
+	},
 
-    delete : (id) => {
-		const targetObj = Utils.prototype.getArrByCondition(convertedData, (val) => { return id == val.id;})[0];
-		
-		try {
-			if (!targetObj) ExceptionHandling.prototype.notExistIdException();
-		} catch (e) {
-			console.error(e.message);
-			return;
+    show (status) {
+		const statusArr = ['all', 'todo', 'doing', 'done'];
+		if (status === 'all') {
+			this.everyStatus(convertedData);
+		} else if (statusArr.includes(status)) {
+			this.singleStatus(convertedData, status);
 		}
-		
-		convertedData.splice(convertedData.indexOf(targetObj), 1);
-		let message = `${targetObj.name}이 ${targetObj.status}에서 삭제되었습니다.`;
-		console.log(message);
-
-    },
+	},
     
     update :(id, status) => {
 		const targetObj = Utils.prototype.getArrByCondition(convertedData, (val) => { return id == val.id;})[0];	
