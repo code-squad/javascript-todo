5b94ce064ce79f5dd098e01d181cd9f1b3b1b340
janghyoseog <bestdevhyo1225@gmail.com>
2019-04-26 11:54:28 +0900

    controller.js require 제거
    - 필요한 객체들을 parameter로 받도록 변경 (의존성을 약화시키고 모듈을 주입하는 방식)

--- oop/app.js
+++ oop/app.js
@@ -1,12 +1,22 @@
 const Controller = require('./controller');
+const ErrorHandler = require('./errorHandler')
+const Printer = require('./printer')
+const Utility = require('./utility')
+const Model = require('./model')
 const readLine = require('readline');
+
 const rl = readLine.createInterface({
     input:process.stdin,
 });
 
 (() => {
+    const errorHandler = new ErrorHandler();
+    const utility = new Utility();
+    const printer = new Printer();
+    const model = new Model();
+
     rl.on('line', (input) => {
-        const controller = new Controller();
+        const controller = new Controller(model, utility, printer, errorHandler);
         controller.instruct(input);
     });
 })();
--- oop/controller.js
+++ oop/controller.js
@@ -1,18 +1,23 @@
-const ErrorHandler = require('./errorHandler')
-const Printer = require('./printer')
-const Utility = require('./utility')
-const Model = require('./model')
+// const ErrorHandler = require('./errorHandler')
+// const Printer = require('./printer')
+// const this.utility = require('./this.utility')
+// const Model = require('./model')
 
-const errorHandler = new ErrorHandler();
-const utility = new Utility();
-const printer = new Printer();
-const model = new Model();
+// const errorHandler = new ErrorHandler();
+// const this.utility = new this.utility();
+// const printer = new Printer();
+// const model = new Model();
 
-function Controller() {}
+function Controller(model, utility, printer, errorHandler) {
+    this.model = model;
+    this.utility = utility;
+    this.printer = printer;
+    this.errorHandler = errorHandler;
+}
 
 Controller.prototype.instruct = function (input) {
-    if(!(errorHandler.usageErrorCheck(input))) return;
-    const inputArray = utility.splitInput(input);
+    if(!(this.errorHandler.usageErrorCheck(input))) return;
+    const inputArray = this.utility.splitInput(input);
     const command = inputArray[0];
     switch(command) {
         case 'show' :
@@ -26,14 +31,14 @@ Controller.prototype.instruct = function (input) {
             break;
         case 'delete' :
             const idDelete = inputArray[1];
-            if(!(errorHandler.notExistIdErrorCheck(idDelete))) return;
+            if(!(this.errorHandler.notExistIdErrorCheck(idDelete))) return;
             this.delete(idDelete);
             break;
         case 'update' :
             const idUpdate = inputArray[1];
             const statusUpdate = inputArray[2];
-            if(!(errorHandler.notExistIdErrorCheck(idUpdate))) return;
-            if(!(errorHandler.sameStatusErrorCheck(idUpdate, statusUpdate))) return;
+            if(!(this.errorHandler.notExistIdErrorCheck(idUpdate))) return;
+            if(!(this.errorHandler.sameStatusErrorCheck(idUpdate, statusUpdate))) return;
             this.update(idUpdate, statusUpdate);
             break;
     }
@@ -41,29 +46,29 @@ Controller.prototype.instruct = function (input) {
 
 Controller.prototype.show = function (status) {
     if(status === 'all') {
-        const countEachStatus = model.getCountEachStatus();
-        printer.printMessageShowAll(countEachStatus);
+        const countEachStatus = this.model.getCountEachStatus();
+        this.printer.printMessageShowAll(countEachStatus);
         return;
     }
-    const listInStatus = model.getListInStatus(status);
-    printer.printMessageShowStatus(status, listInStatus);
+    const listInStatus = this.model.getListInStatus(status);
+    this.printer.printMessageShowStatus(status, listInStatus);
 }
 
 Controller.prototype.add = function (name, tag) {
-    const objToAdd = {'name': name, 'status': 'todo', 'tags': tag.match(/[a-z0-9]+/g), 'id': utility.getRandomID()};
-    model.addTodoObject(objToAdd);
-    printer.printMessageAdd(objToAdd);
+    const objToAdd = {'name': name, 'status': 'todo', 'tags': tag.match(/[a-z0-9]+/g), 'id': this.utility.getRandomID()};
+    this.model.addTodoObject(objToAdd);
+    this.printer.printMessageAdd(objToAdd);
     this.throwSetTimeForShowAll();
 }
 
 Controller.prototype.delete = function (id) {
-    const objToDelete = model.deleteTodoObject(id);
-    printer.printMessageDelete(objToDelete);
+    const objToDelete = this.model.deleteTodoObject(id);
+    this.printer.printMessageDelete(objToDelete);
     this.throwSetTimeForShowAll();
 }
 
 Controller.prototype.update = function (id, status) {
-    const objToUpdate = model.updateTodoObject(id, status);   
+    const objToUpdate = this.model.updateTodoObject(id, status);   
     this.throwSetTimeForUpdate(objToUpdate);
 }
 
@@ -74,7 +79,7 @@ Controller.prototype.throwSetTimeForShowAll = function (delayTime = 1000) {
 Controller.prototype.throwSetTimeForUpdate = function(objToUpdate, delayTime = 3000) { 
     setTimeout( 
         () => { 
-            printer.printMessageUpdate(objToUpdate);
+            this.printer.printMessageUpdate(objToUpdate);
             this.throwSetTimeForShowAll();
         }, delayTime);
 }
