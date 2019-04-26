79beb9feaaae70198cb07dedc606ecf0d02fba61
eugene94 <eugenekang94@gmail.com>
2019-04-26 16:22:28 +0900

    refactor prototype pattern => class pattern

--- oop/todoError.js
+++ oop/todoError.js
@@ -1,40 +1,39 @@
-const TodoError = function() {};
-TodoError.prototype = {
+class TodoError {
   includeSeperator(str, seperator) {
     const regex = new RegExp('\\' + seperator);
     if (str.match(regex) === null) {
       return false;
     }
     return true;
-  },
+  }
 
   compareStatus(originStr, changingStr) {
     if (originStr === changingStr) {
       return false;
     }
     return true;
-  },
+  }
 
   invalidId(id) {
     if (id === undefined) {
       return false;
     }
     return true;
-  },
+  }
 
   invalidInst(obj, inst) {
     if (obj[inst] === undefined) {
       return false;
     }
     return true;
-  },
+  }
 
   invalidStatus(conditionArr, condition) {
     if (!conditionArr.includes(condition)) {
       return false;
     }
     return true;
-  },
+  }
 
   isArray(inputStr) {
     const regex = /\[[\"\'\,\s\w\d]+\]/; // [로 시작하고 ]로 끝나면 통과
@@ -43,6 +42,6 @@ TodoError.prototype = {
     }
     return true;
   }
-};
+}
 
 module.exports = TodoError;
