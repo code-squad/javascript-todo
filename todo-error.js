033cc37a2db94d08a6b23555b612ab0e221caf76
park78951 <park78951@gmail.com>
2019-04-26 10:31:43 +0900

    created error file for exception handling and code try and catch in todo-proto file

--- todo-error.js
+++ todo-error.js
@@ -2,20 +2,92 @@ const TodoError = function() {
 
 }
 
+// '$' 문자가 없는 경우 에러 처리
 TodoError.prototype.findSeparator = function(action, input) {
     const splitOfInput = input.split('');
-
+    const countComma = splitOfInput.filter(n => {
+        return n === ','
+    })
     if(action === 'add' || action === 'update') {
-        const countComma = splitOfInput.filter(n => {
-            return n === ','
-        })
-        return countComma.length === 2 ? true : false
+        if(countComma.length !== 2) throw Error('인자값이 부족합니다. 확인해주세요.')
     }
     if (action === 'delete' || action == 'show') {
-        const countComma = splitOfInput.filter(n => {
-            return n === ','
-        })
-        return countComma.length === 1 ? true : false
+        if(countComma.length !== 1) throw Error('인자값이 부족합니다. 확인해주세요.')
+    }
+}
+
+TodoError.prototype.findWrongStatus = function(statusOfShow, statusOfUpdate) {
+    
+
+    if(statusOfUpdate !== undefined){
+        if(statusOfUpdate !== 'todo' && 
+           statusOfUpdate !== 'doing' &&
+           statusOfUpdate !== 'done') {
+                throw Error('Status를 확인해주세요.');
+        }
+    }
+
+    if(statusOfUpdate === undefined){
+        if(statusOfShow !== 'todo' && 
+           statusOfShow !== 'doing' &&
+           statusOfShow !== 'done' &&
+           statusOfShow !== 'all') {
+                throw Error('Status를 확인해주세요.');
+        }
     }
     
 }
+
+
+TodoError.prototype.actionCheck = function(action) {
+    if (action !== 'add' && action !== 'delete' && action !== 'update' && action !== 'show') {
+        throw Error ('처음값 add, delete, update, show 중 하나로 입력해주세요.')
+    }
+}
+
+
+TodoError.prototype.sameStatusCheck = function (that, id, inputStatus) {
+    const validId = parseInt(id);
+    const [matchedListById] = this.checkID(validId, that);
+    if (inputStatus === matchedListById.status) {
+        throw Error ('status가 동일합니다. 변경될 status를 입력해주세요.')
+    }
+}
+
+TodoError.prototype.idValidation = function (that, rest) {
+    const id = rest[0];
+    const validId = parseInt(id);
+    const [matchedListById] = this.checkID(validId, that);
+    if(matchedListById === undefined) {
+        throw Error ('ID값을 확인해주세요');
+    }
+}
+
+TodoError.prototype.printError = function (input, that) {
+    const [action, ...rest] = input.split(',');
+        this.actionCheck(action);
+        this.findSeparator(action,input);
+    if ( action === 'update') {
+        this.sameStatusCheck(that, ...rest);
+        this.findWrongStatus(...rest);
+        this.idValidation(that, rest);
+    }
+    if ( action === 'delete') {
+        this.idValidation(that, rest);
+    }
+    if (action === 'show') {
+        this.findWrongStatus(...rest);
+    }
+}
+
+TodoError.prototype.checkID = function (id, that) {
+    const matchedListById = that.data.filter(list => {
+        return list.id === id
+    })
+    return matchedListById
+}
+
+
+
+
+module.exports = TodoError;
--- todo-input.js
+++ todo-input.js
@@ -1,26 +1,26 @@
 let readline = require('readline');
 
-let r = readline.createInterface({
+let inputReadline = readline.createInterface({
     input:process.stdin,
     output:process.stdout
 });
 
-let todoList = require('./todo_proto.js');
+let todoList = require('./todo-proto.js');
 
 let todoTest = new todoList();
 
-r.setPrompt('명령하세요');
-r.prompt();
-r.on('line', (line) => {
+inputReadline.setPrompt('명령하세요');
+inputReadline.prompt();
+inputReadline.on('line', (line) => {
     
     if(line === 'exit') {
         r.close();
     }
 
-    todoTest.sortItem(line);
-    r.prompt();
+    todoTest.checkInput(line, inputReadline);
+    // r.prompt();
 })
-r.on('close', function(){
+inputReadline.on('close', function(){
     process.exit();
 })
 
