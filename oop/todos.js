bbff82edb2952c184757f7076c69c8363c21a236
aftersnowing <gurcks2@gmail.com>
2019-04-27 14:39:04 +0900

    todos 모듈 코드 리팩토링 랜덤 넘버 생성 메소드 코드 개선 변수명 개선

--- oop/todos.js
+++ oop/todos.js
@@ -3,26 +3,13 @@ module.exports = class Todos {
     constructor(data) {
         this._data = data;
         this.statusArr = ['todo', 'doing', 'done'];
-    }
+    };
 
     randomNum(digits) {
         const min = Math.pow(10, digits - 1);
         const max = Math.pow(10, digits) - 1;
         return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    isDuplicateNum(data, ranId) {
-        return data.some((el) => el['id'] === ranId)
-    }
-
-    makeSoleNum(digits, data) {
-        let ranId = this.randomNum(digits);
-        if (!this.isDuplicateNum(data, ranId)) {
-            return ranId;
-        } else {
-            return makeRanNum(digits, data);
-        }
-    }
+    };
 
     searchById(id) {
         let resultIndex = null;
@@ -30,6 +17,16 @@ module.exports = class Todos {
             if (value.id == id) resultIndex = index;
         });
         return resultIndex;
+    };
+
+    makeUniqueNum(digits) {
+        let generatedId = -1;
+        let indexOfId = -1;
+        do {
+            generatedId = this.randomNum(digits);
+            indexOfId = this.searchById(generatedId);
+        } while(indexOfId != null);
+        return generatedId;
     };
 
     countAllStatus() {
@@ -53,40 +50,40 @@ module.exports = class Todos {
     show(status) {
         if (status === "all") {
             const statusCountObject = this.countAllStatus();
-            return `현재상태 : todo: ${statusCountObject['todo']}개, doing: ${statusCountObject['doing']}개, done: ${statusCountObject['done']}개`
-        }
-        else if (this.statusArr.includes(status)) {
+            return `현재상태 : todo: ${statusCountObject['todo']}개, doing: ${statusCountObject['doing']}개, done: ${statusCountObject['done']}개`;
+        } else if (this.statusArr.includes(status)) {
             const statusArr = this.searchByStatus(status);
-            return `${status}리스트 :  총 ${statusArr.length}건 : ${statusArr.join(', ')}`
-        }
-    }
+            return `${status}리스트 :  총 ${statusArr.length}건 : ${statusArr.join(', ')}`;
+        };
+    };
 
     add(name, tags) {
-        let generatedId = this.makeSoleNum(5, this._data);
+        let generatedId = this.makeUniqueNum(5);
         let newObj = {
             name,
             tags,
             status: "todo",
             id: generatedId
-        }
-        this._data.push(newObj)
-        return `'${name}' 1개가 추가됐습니다.(id : ${generatedId})`
-    }
+        };
+        this._data.push(newObj);
+        return `'${name}' 1개가 추가됐습니다.(id : ${generatedId})`;
+    };
+
     delete(id) {
         const indexOfTarget = this.searchById(id);
-        let objOfTarget = this._data[indexOfTarget]
+        let objOfTarget = this._data[indexOfTarget];
         this._data.splice(indexOfTarget, 1);
-        return `'${objOfTarget.name}' '${objOfTarget.status}'가 목록에서 삭제됐습니다.`
+        return `'${objOfTarget.name}' '${objOfTarget.status}'가 목록에서 삭제됐습니다.`;
     }
 
     update(id, status) {
         const indexOfTarget = this.searchById(id);
-        let objOfTarget = this._data[indexOfTarget]
+        let objOfTarget = this._data[indexOfTarget];
         if (objOfTarget.status === status) {
-            return '바꾸려는 상태가 현재상태와 같습니다.'
+            return '바꾸려는 상태가 현재상태와 같습니다.';
         } else {
             objOfTarget.status = status;
-            return `'${objOfTarget.name}'이(가) '${status}'으로 상태가 변경됐습니다`
-        }
-    }
+            return `'${objOfTarget.name}'이(가) '${status}'으로 상태가 변경됐습니다`;
+        };
+    };
 }
