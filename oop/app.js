337e68cf2f9f6370c8b427a8f49bbd357c69516e
aftersnowing <gurcks2@gmail.com>
2019-04-27 14:40:28 +0900

    app 모듈 코드 리팩토링 show키워드를 prompt로 변경하여 함수 네이

--- oop/app.js
+++ oop/app.js
@@ -1,5 +1,4 @@
 //app.js
-
 const Todos = require('./todos');
 const InputParser = require('./inputParser');
 const Validator = require('./validator');
@@ -12,57 +11,58 @@ const convertedData = JSON.parse(JSON.stringify(originData)).data;
 
 todos = new Todos(convertedData);
 
-const showResult = (todosReturn, appWord) => {
+const promptResult = (resultOfTodos, appWord) => {
     return new Promise((resolve) => {
         let timeDelay = 0;
         if (appWord === 'update')
             timeDelay = 3000;
         setTimeout(() => {
-            resolve(todosReturn);
+            resolve(resultOfTodos);
         }, timeDelay);
     })
 };
-const showAll = () => {
+
+const promptAll = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(todos.show('all'));
         }, 1000);
     })
 };
-const promptRepeater = (fun) => {
-    if (fun) {
+
+const promptRepeater = (func) => {
+    if (func) {
         rl.prompt();
         return true;
     }
 };
-
 
 rl.setPrompt('명령하세요: ');
 rl.prompt();
 
 rl.on('line', (userInput) => {
     switch (userInput.toLowerCase().trim()) {
-        case 'q':
+        case 'exit':
             rl.close();
             break;
         default:
             const validator = new Validator(convertedData, userInput);
-            if (promptRepeater(validator.excute())) return;
+            if (promptRepeater(validator.execute())) return;
 
             const inputParser = new InputParser(userInput);
             const appWord = inputParser.splitedOrder[0];
 
-            showResult(inputParser.excute(todos), appWord)
-                .then((textResult) => {
-                    console.log(textResult);
-                    if (appWord != 'show') {
-                        showAll()
-                            .then((textAll) => {
-                                console.log(textAll);
-                                rl.prompt();
-                            })
-                    } else rl.prompt();
-                })
+            promptResult(inputParser.execute(todos), appWord)
+            .then((resultOfTodos) => {
+                console.log(resultOfTodos);
+                if (appWord != 'show') {
+                    promptAll()
+                    .then((resultOfAll) => {
+                        console.log(resultOfAll);
+                        rl.prompt();
+                    })
+                } else rl.prompt();
+            })
     }
 }).on('close', () => {
     console.log("프로그램을 종료합니다.");
