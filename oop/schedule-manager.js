47fed3d6a58046d5c72482b8cc06cc9124c31731
shun <koownusmik@gmail.com>
2019-04-27 14:51:29 +0900

    refactor: updateTodo,deleteTodo개선 Editor생성자함수 개선
    1. updateTodo , deleteTodo의 내부 연산들을
    findSelectedIdObj 와 filterNameStatusAway함수를 구현하여
    함수 내부에서 update와 delete가 일어나기위해 필요한 연산의 가독성을 높이고
     재사용성을 높임.
    
     2. Editor의 생성자 함수가 App의 App. errorChecker를 인자로 받아 새로운 ErrorChecker 객체를 생성하지 않도록 구현
    
    그런데 이렇게 Editor 생성자함수의 인자로 App.errorChecker를 인자로 넘기는것은 App.editor와 App.errorChecker의 의존성을 높이는 방법인것
    같음.  App.editor객체의 인자로 App.errorChecker를 넘기는것은 Editor 클래스와 ErrorChecker 클래스는 App객체안에 있고, Editor객체에서 발 생한 문제는 상위의 App객체안의 ErrorChecker 객체가 모두 해결해준다!는 의미를 가짐.

--- oop/schedule-manager.js
+++ oop/schedule-manager.js
@@ -6,24 +6,31 @@ const rl = readline.createInterface({
 });
 
 function App() {
-    this.editor = new Editor();
-    this.viewer = new Viewer();
     this.errorChecker = new ErrorChecker();
+    this.editor = new Editor(this.errorChecker);
+    this.viewer = new Viewer();
 }
+// function App(editor , viewer , errorChecker) {
+//     this.editor = editor;
+//     this.viewer = viewer;
+//     this.errorChecker = errorChecker;
+// }
 
 
 
 App.prototype = {
 
+    
+
     run() {
         const self = this;
 
-        function delayShow(time){
-            return new Promise(function(resolve, reject){
+        function delayShow(time) {
+            return new Promise(function (resolve, reject) {
                 setTimeout(() => {
                     self.viewer.showAll()
                     self.run();
-                },time);
+                }, time);
             })
         }
 
@@ -32,18 +39,17 @@ App.prototype = {
             try {
                 [key, ...message] = self.parseCommand(input)
                 this.errorChecker.$check(message)
-    
+
                 if (key === "show") {
                     [status] = message
                     status === "all" ? self.viewer.showAll() : self.viewer.showFiltered(status);
                     return self.run();
                 }
-               
-                
+
+
                 const result = self.editor[key + 'Todo'](...message);
-    
                 await self.viewer[key + 'Message'](...result)
-                await delayShow(1000);   
+                await delayShow(1000);
             }
             catch (e) {
                 console.log(e.message);
@@ -59,8 +65,8 @@ App.prototype = {
 }
 
 
-function Editor() {
-    this.errorChecker = new ErrorChecker();
+function Editor(errorChecker) {
+    this.errorChecker = errorChecker;
 }
 
 Editor.prototype = {
@@ -80,35 +86,50 @@ Editor.prototype = {
 
     updateTodo(_id, _status) {
 
-        const selectedObject = schedule_list.find(todo => todo.id === parseInt(_id));
+        const selectedObject = this.findSelectedIdObj(_id);
 
-
-        return schedule_list.some(todo => {
+        schedule_list.some(todo => {
             if (todo.id === parseInt(_id)) {
                 this.errorChecker.statusCheck(todo.status, _status)
                 return todo.status = _status
             }
-        }) ? Object.entries(selectedObject).filter(([key, value]) =>
-        key === 'name' || key === 'status'
-    ).map(([key, value]) => value) : this.errorChecker.foundIdError();
+        });
 
+        return this.filterNameStatusArrayfromSelectedObject(selectedObject);
 
     },
 
     deleteTodo(_id) {
 
-        const selectedObject = schedule_list.find(todo => todo.id === parseInt(_id));
-        const selectedArray = Object.entries(selectedObject).filter(([key, value]) =>
-        key === 'name' || key === 'status'
-        ).map(([key, value]) => value);
-        return schedule_list.some((todo, index) => {
+        const selectedObject = this.findSelectedIdObj(_id);
+
+        schedule_list.some((todo, index) => {
             if (todo.id === parseInt(_id)) return schedule_list.splice(index, 1)
-        }) ? selectedArray : this.errorChecker.foundIdError();
+        })
+        return this.filterNameStatusArrayfromSelectedObject(selectedObject);
+
     },
 
     getUniqueId() {
         return Date.now()
+    },
+
+    filterNameStatusArrayfromSelectedObject(_obj){
+        const filteredArray = Object.entries(_obj).filter(([key, value]) =>{
+            const seletedStatus = ['name','status'];
+            return seletedStatus.includes(key)
+        })
+        .map(([key, value]) => value);
+        return filteredArray;
+    },
+
+    findSelectedIdObj(_id){
+        const findedObj = schedule_list.find(todo => todo.id === parseInt(_id));
+        if (this.errorChecker.idCheck(findedObj));
+        return findedObj;
     }
+
+
 }
 
 
@@ -139,11 +160,12 @@ Viewer.prototype = {
     },
 
     updateMessage(_name, _status) {
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             setTimeout(() => {
                 console.log(`${_name}의 상태가 ${_status}로 변경되었습니다.`);
                 resolve();
-            },3000)})
+            }, 3000)
+        })
     },
 
     deleteMessage(_name, _status) {
@@ -162,20 +184,24 @@ ErrorChecker.prototype = {
         if (_todo_status === _status) throw new Error('StatusError : status is same')
     },
 
-    foundIdError() {
-        throw new Error('IdError : id is not exist')
+    idCheck(_obj) {
+        if (_obj === undefined) throw new Error('IdError : id is not valid')
     }
 
 
 }
 
+// const schedule_errorChecker = new ErrorChecker();
+// const schedule_editor = new Editor(schedule_errorChecker);
+// const schedule_viewer = new Viewer();
+// const schedule_manager = new App(schedule_editor,schedule_viewer,schedule_errorChecker);
 
 const schedule_manager = new App();
 schedule_manager.run()
 // schedule_manager.run('show$todo');
 // schedule_manager.run('add$운동하기$exercise');
 // schedule_manager.run('update$9$doing');
-// schedule_manager.run('delete$7');
+// schedule_manager.run('delete$6');
 // show$todo
 // add$운동하기$exercise
 // update$7$doing
