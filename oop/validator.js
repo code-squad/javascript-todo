acef1cf5dcd6a5795a8666291187244a40cf0fbc
aftersnowing <gurcks2@gmail.com>
2019-04-28 14:43:43 +0900

    validator 코드 수정 undo / redo input통과

--- oop/validator.js
+++ oop/validator.js
@@ -3,34 +3,38 @@ module.exports = class Validator {
     constructor(data) {
         this._data = data;
     }
-    
+
     initOrder() {
-        this._seperator = this._order.match(/\$/g);
-        this._appWord = this._order.match(/[\d\D]*(?=\$)/);
-        this._status = this._order.match(/(?<=\$)[a-z]*$/);
-        this._id = this._order.match(/(?<=\$)\d*/);
-        this._tag = this._order.match(/(?<=\[)[\d\D]+(?=\])/);
-        this._name = this._order.match(/(?<=\$)[\d\D]*(?=\$)/);
+        this._undoWord = this._userInput.match(/^undo$|^redo$/);
+        this._seperator = this._userInput.match(/\$/g);
+        this._appWord = this._userInput.match(/^show|^add|^delete|^update/);
+        this._status = this._userInput.match(/(?<=\$)[a-z]*$/);
+        this._id = this._userInput.match(/(?<=\$)\d*/);
+        this._tag = this._userInput.match(/(?<=\[)[\d\D]+(?=\])/);
+        this._name = this._userInput.match(/(?<=\$)[\d\D]*(?=\$)/);
     }
 
+    validateUndoWord() { return null }
     validateSeparator() { return '각각의 명령어를 $로 구분해주세요.'; }
     validateAppWord() { return '잘못된 appWords 입니다.'; }
     validateTag() { if (!this._tag) return '가능한 tag형식이 아닙니다.'; }
-    validateName() { if(!this._name) return '가능한 name형식이 아닙니다.';}
+    validateName() { if (!this._name) return '가능한 name형식이 아닙니다.'; }
     validateId() {
         let existId = this._data.some((element) => element.id === Number(this._id[0]));
         if (!existId) return '존재하지 않는 id입니다.';
     }
     validateStatus() {
-        const statusList = ['all', 'todo', 'doing','done'];
+        const statusList = ['all', 'todo', 'doing', 'done'];
         if (statusList.indexOf(this._status[0]) === -1) return '가능한 status 형식이 아닙니다.';
     }
 
-    excuteValidation(order) {
-        this._order = order;
+    excuteValidation(userInput) {
+        this._userInput = userInput;
         this.initOrder();
-        
-        if (!this._seperator){ return this.validateSeparator(); }
+
+        if (this._undoWord) { return this.validateUndoWord(); }
+        if (!this._appWord) { return this.validateAppWord(); }
+        if (!this._seperator) { return this.validateSeparator(); }
         switch (this._appWord[0]) {
             case 'show':
                 return this.validateStatus();
@@ -39,8 +43,8 @@ module.exports = class Validator {
             case 'delete':
                 return this.validateId();
             case 'update':
-                return this.validateId() || this.validateStatus(); 
-            default :
+                return this.validateId() || this.validateStatus();
+            default:
                 return this.validateAppWord();
         }
     }
