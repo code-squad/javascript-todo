7129436aef2e6d70990a7db0a9074821558aef1d
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-29 19:36:13 +0900

    Refactoring customException.js
    prototype pattern to class

---
+++ class/customException.js
@@ -0,0 +1,34 @@
+const CustomException = class {
+	missingSeperatorException (input) {
+		if (!this.isValidSeperator(input)) throw new Error("구분자 $가 존재하지 않습니다");
+	}
+	
+	notExistIdException () {
+		throw new Error(`찾으시는 id가 존재하지 않습니다`);
+	}
+	
+	sameStatusException () {
+		throw new Error(`같은 상태로 업데이트 할 수 없습니다`);
+	}
+	
+	CommandMissingException (command, arr) {
+		if (!this.isValidCommand(command, arr))
+		throw new Error(`올바른 명령어가 아닙니다`);
+	}
+	
+	isValidSeperator (input) {
+		let result = true;
+		const regexp = /\$|undo|redo/g;
+		if (input.match(regexp) == null) result = false;
+		
+		return result;
+	}
+	
+	isValidCommand(command, arr) {
+    let result = false;
+    if (arr.includes(command)) result = true;
+    return result;
+	}
+};
+
+module.exports = CustomException;
