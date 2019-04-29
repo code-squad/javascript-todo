348abc19a69e919b1d052c057de5d00d01189a12
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-29 19:37:26 +0900

    Refactoring utils.js : prototype to class

---
+++ class/utils.js
@@ -0,0 +1,21 @@
+const Utils  = class {
+
+	getArrByCondition (arr, condition) {
+        return arr.reduce((acc, val) => {
+            if (condition(val)) acc.push(val);
+            return acc;
+        }, []);
+    }	
+
+    getRandomId (max, min) {
+        return Math.floor(Math.random() * (max-min)) + 1;
+    }
+
+    delay (time) {
+		return new Promise(function(resolve, reject){
+			setTimeout(resolve, time);
+		});
+	}
+};
+
+module.exports = Utils;
