7cbc3a142eb9ea1e7de4dc8a3cb97a68a2e17810
sonypark <sonypark0204@gmail.com>
2019-04-29 21:27:36 +0900

    [refactor]: Rename function and variables
    - Check errors
    - redo/undo functions refactoring

--- oop/oopTodo.js
+++ oop/oopTodo.js
@@ -9,20 +9,22 @@ const inputReadline = readline.createInterface({
 const TodoApp = function (datalist) {
     this.datalist = datalist;
     this.command = undefined;
+    this.save = false;
     this.past = [];
     this.present = [];
     this.future = [];
+    this.storeCommand = [];
 };
 
 
 TodoApp.prototype = {
 
     // addTodo
-    addTodoExecutor(command, todoElement, todoTag) {
-        this.addTodoList(command, todoElement, todoTag);
+    addExecutor(command, todoElement, todoTag) {
+        this.addTodo(command, todoElement, todoTag);
     },
 
-    addTodoList(command, todoElement, todoTag) {
+    addTodo(command, todoElement, todoTag) {
         const id = this.createNewID(this.datalist, 10000)
 
         const newTodo = {
@@ -34,66 +36,63 @@ TodoApp.prototype = {
         this.datalist.push(newTodo);
         this.undoable(command, newTodo);
 
-        return this.addTodoResult(newTodo);
+        return this.addCompleted(newTodo);
     },
 
 
-    addTodoResult(newAddedObject) {
-        console.log(this.datalist);
-        const addlistResult = `${newAddedObject.name} 1 개가 추가됐습니다.(id : ${newAddedObject.id})`;
-        return this.showAll_printResult(addlistResult);
+    addCompleted(newAddedObject) {
+        const addResult = `${newAddedObject.name} 1 개가 추가됐습니다.(id : ${newAddedObject.id})`;
+        this.showAll_printResult(addResult);
     },
 
     // deleteTodo
-    deleteTodoExecutor(command,deleteID) {
-        return this.checkID(deleteID) ? this.deleteTodoList(command,deleteID) : this.printError()
+    deleteExecutor(command, deleteID) {
+        return this.checkID(deleteID) ? this.deleteTodo(command, deleteID) : this.printError()
     },
 
-    deleteTodoList(command,deletedID) {
-        const deletedIndex = this.getIndex(deletedID);
-        const [splicedData] = this.datalist.splice(deletedIndex, 1);
+    deleteTodo(command, deletedID) {
+        const currentIndex = this.getIndex(deletedID);
+        const [splicedData] = this.datalist.splice(currentIndex, 1);
         this.undoable(command, splicedData);
-        this.manageMaxPastList();
+        this.manageMaxLengthOfPastArr();
 
-        return this.deleteTodoResult(splicedData)
+        return this.deleteCompleted(splicedData)
     },
 
-    deleteTodoResult(splicedData) {
-        console.log(this.datalist);
-        const deletionResult = `${splicedData.name}가 ${splicedData.status}에서 삭제되었습니다.`
-        return this.showAll_printResult(deletionResult);
+    deleteCompleted(splicedData) {
+        const delResult = `${splicedData.name}가 ${splicedData.status}에서 삭제되었습니다.`
+        this.showAll_printResult(delResult);
     },
 
 
     // updateTodo
-    updateTodoExecutor(command, id, updatedStatus) {
-        return this.checkID(id) ? this.updateTodoStatus(command, id, updatedStatus) : this.printError()
+    updateExecutor(command, id, updatedStatus) {
+        return this.checkID(id) ? this.updateTodo(command, id, updatedStatus) : this.printError()
     },
 
 
-    updateTodoStatus(command, id, updatedStatus) {
-        const updatatingIndex = this.getIndex(id);
-        if (this.checkDuplicatedStatus(updatatingIndex, updatedStatus)) {
+    updateTodo(command, id, updatedStatus) {
+        const currentIndex = this.getIndex(id);
+        if (this.checkDuplicatedStatus(currentIndex, updatedStatus)) {
             console.log('\n');
             inputReadline.prompt();
-            return
+            return;
         };
-        const beforeUpdatedStatus = this.datalist[updatatingIndex].status
-        this.datalist[updatatingIndex].status = updatedStatus;
-        this.undoable(command,  {updatatingIndex, beforeUpdatedStatus, updatedStatus});
-        return this.updateTodoResult(updatatingIndex, updatedStatus)
+        const beforeUpdatedStatus = this.datalist[currentIndex].status
+        this.datalist[currentIndex].status = updatedStatus;
+        this.undoable(command, { currentIndex, beforeUpdatedStatus, updatedStatus });
+        return this.updateCompleted(currentIndex, updatedStatus)
     },
 
 
-    updateTodoResult(updatatingIndex, updatedStatus) {
-        console.log(this.datalist);
-        const updateResult = `${this.datalist[updatatingIndex].name} 가 ${updatedStatus}로 상태가 변경됬습니다.`;
-        return setTimeout(() => {
+    updateCompleted(currentIndex, updatedStatus) {
+        const updateResult = `${this.datalist[currentIndex].name} 가 ${updatedStatus}로 상태가 변경됬습니다.`;
+        setTimeout(() => {
             this.showAll_printResult(updateResult);
         }, 3000);
     },
 
-    showElementGetter() {
+    collectShowElements() {
         const todoList = this.checkStatus(this.datalist, 'todo');
         const doingList = this.checkStatus(this.datalist, 'doing');
         const doneList = this.checkStatus(this.datalist, 'done');
@@ -101,12 +100,12 @@ TodoApp.prototype = {
     },
 
 
-    showTodoList(status) {
-        const todoElementList = this.showElementGetter()
-        return this.showStatusExecutor(status, todoElementList);
+    carryShowElements(status) {
+        const todoElementList = this.collectShowElements()
+        return this.showTodo(status, todoElementList);
     },
 
-    showStatusExecutor(status, todoElementList) {
+    showTodo(status, todoElementList) {
         const { todoList, doingList, doneList } = todoElementList
         switch (status) {
             case 'all':
@@ -126,14 +125,14 @@ TodoApp.prototype = {
     showAll_printResult(result) {
         console.log(result);
         setTimeout(() => {
-            console.log((this.showTodoList('all')));
+            console.log((this.carryShowElements('all')));
             inputReadline.prompt();
         }, 1000);
     },
 
-    splitInputVal(inputData) {
+    splitInput(inputData) {
         const splitOnDollarSymbol = inputData.split("$");
-        return splitOnDollarSymbol
+        return splitOnDollarSymbol;
     },
 
     printError() {
@@ -169,16 +168,21 @@ TodoApp.prototype = {
         return matchedListByID;
     },
 
-    checkDuplicatedStatus(updatatingIndex, updatedStatus) {
-        if (this.datalist[updatatingIndex].status === updatedStatus) {
+    checkDuplicatedStatus(currentIndex, updatedStatus) {
+        if (this.datalist[currentIndex].status === updatedStatus) {
             console.log('입력한 상태와 동일한 상태입니다')
             return true;
         }
     },
 
+    saveCommandAfterUndo(command) {
+        if (this.save) {
+            this.storeCommand.push(command);
+        }
+    },
 
     checkCommands(userInput) {
-        const splitUserInput = this.splitInputVal(userInput);
+        const splitUserInput = this.splitInput(userInput);
         if (userInput.split('$').length < 1 || userInput.split('$').length > 3) {
             console.log("입력값을 확인해주세요");
             inputReadline.setPrompt('명령어를 입력하세요(종료하려면 q를 누르세요): ');
@@ -189,19 +193,23 @@ TodoApp.prototype = {
         const [command, commandElement, TagORStatusOfcommandElement] = splitUserInput;
 
         if (command === 'show') {
-            console.log(this.showTodoList(commandElement));
+            console.log(this.carryShowElements(commandElement));
             inputReadline.prompt();
         }
         else if (command === 'add') {
-            this.addTodoExecutor(command, commandElement, TagORStatusOfcommandElement);
+            this.saveCommandAfterUndo(command);
+            this.addExecutor(command, commandElement, TagORStatusOfcommandElement);
         }
         else if (command === 'update') {
-            this.updateTodoExecutor(command, Number(commandElement), TagORStatusOfcommandElement);
+            this.saveCommandAfterUndo(command);
+            this.updateExecutor(command, Number(commandElement), TagORStatusOfcommandElement);
         }
         else if (command === 'delete') {
-            this.deleteTodoExecutor(command, Number(commandElement));
+            this.saveCommandAfterUndo(command);
+            this.deleteExecutor(command, Number(commandElement));
         }
         else if (command === 'undo') {
+            this.save = true;
             this.undo();
         }
         else if (command === 'redo') {
@@ -212,79 +220,94 @@ TodoApp.prototype = {
         }
     },
 
-
     undoable(command, splicedData) {
         this.past.push(splicedData);
         this.command = command;
     },
 
-    manageMaxPastList() {
+    manageMaxLengthOfPastArr() {
         if (this.past.length < 4) {
             return
         }
         else {
             this.past.shift();
-            this.manageMaxPastList();
+            this.manageMaxLengthOfPastArr();
+        }
+    },
+
+    initSaveCommandAfterUndo() {
+        if (this.storeCommand.length != 0) {
+            this.past = [];
+            this.present = [];
+            this.future = [];
+            this.storeCommand = [];
+            this.save = false;
         }
     },
 
     undo() {
+
         if (this.past.length === 0) {
-            console.log('undo할 값이 없습니다!');
+            console.log('\n undo할 값이 없습니다!');
             inputReadline.prompt();
             return;
         }
 
         const popPastValue = this.past.pop();
 
-        if(this.present.length === 1){
+        if (this.present.length === 1) {
             this.future.push(this.present.pop());
             this.present.push(popPastValue);
-        } else{
+        } else {
             this.present.push(popPastValue);
         }
 
         if (this.command === 'add') {
             this.datalist.pop();
-            console.log(`${popPastValue.id}번 항목 ${popPastValue.name}가 식제됐습니다.`);
+            console.log(`\n ${popPastValue.id}번 항목 ${popPastValue.name}가 식제됐습니다.`);
         }
         else if (this.command === 'update') {
-            this.datalist[popPastValue.updatatingIndex].status = popPastValue.beforeUpdatedStatus;
-            console.log(`${popPastValue.id}번 항목 ${popPastValue.name}가 ${popPastValue.status}상태로 변경이 취소되었습니다.`);
+            this.datalist[popPastValue.currentIndex].status = popPastValue.beforeUpdatedStatus;
+            console.log(`\n ${datalist[popPastValue.currentIndex].id}번 항목 ${datalist[popPastValue.currentIndex].name}가 ${datalist[popPastValue.currentIndex].status}상태로 변경이 취소되었습니다.`);
         }
         else {
             this.datalist.push(popPastValue);
-            console.log(`${popPastValue.id}번 항목 ${popPastValue.name}가 삭제에서 ${popPastValue.status}상태로 변경되었습니다.`);
+            console.log(`\n ${popPastValue.id}번 항목 ${popPastValue.name}가 삭제에서 ${popPastValue.status}상태로 변경되었습니다.`);
         }
-        console.log(this.datalist);
+
+        this.initSaveCommandAfterUndo();
         inputReadline.prompt();
+
         return;
     },
 
     redo() {
-        if (this.present.length === 0) {
-            console.log('redo할 값이 없습니다!');
+        if (this.save || this.present[0] === undefined) {
+            console.log('\n redo할 값이 없습니다!');
             inputReadline.prompt();
             return;
         }
+        
+        this.initSaveCommandAfterUndo();
+
         const popPresentValue = this.present.pop();
-        
+
         this.past.push(popPresentValue);
-        this.present.push(this.future.shift());
-        
+        this.present.push(this.future.pop());
+
         if (this.command === 'add') {
             this.datalist.push(popPresentValue);
-            console.log(`${popPresentValue.name} 1 개가 추가됐습니다.(id : ${popPresentValue.id})`);
+            console.log(`\n ${popPresentValue.name} 1 개가 추가됐습니다.(id : ${popPresentValue.id})`);
         }
         else if (this.command === 'update') {
-            this.datalist[popPresentValue.updatatingIndex].status = popPresentValue.updatedStatus;
-            console.log(`${this.datalist[popPresentValue.updatatingIndex].name} 가 ${this.datalist[popPresentValue.updatatingIndex].status}로 상태가 변경됬습니다.`);
+            this.datalist[popPresentValue.currentIndex].status = popPresentValue.updatedStatus;
+            console.log(`\n ${this.datalist[popPresentValue.currentIndex].name} 가 ${this.datalist[popPresentValue.currentIndex].status}로 상태가 변경됬습니다.`);
         }
         else {
             this.datalist.pop();
-            console.log(`${popPresentValue.id}번 항목 ${popPresentValue.name}가 ${popPresentValue.status}상태에서 삭제되었습니다.`);
+            console.log(`\n ${popPresentValue.id}번 항목 ${popPresentValue.name}가 ${popPresentValue.status}상태에서 삭제되었습니다.`);
         }
-        console.log(this.datalist);
+
         inputReadline.prompt();
         return;
     },
