587e2aae619cced300d448e89d91422d064b4fc7
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-29 19:38:28 +0900

    Feat utils.js : add deepCopy method

--- class/utils.js
+++ class/utils.js
@@ -15,7 +15,17 @@ const Utils  = class {
 		return new Promise(function(resolve, reject){
 			setTimeout(resolve, time);
 		});
-	}
+    }
+    
+    deepCopy (obj) {
+        if (obj === null || typeof(obj) !== "object") return obj;
+       
+        let copy = {};
+        for(let key in obj) {
+          copy[key] = this.deepCopy(obj[key]);
+        }
+        return copy;
+    }
 };
 
 module.exports = Utils;
