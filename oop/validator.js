66323304b62fa05b7891070fec1bc6c6fed08441
aftersnowing <gurcks2@gmail.com>
2019-04-30 11:00:53 +0900

    validator 리팩토링

--- oop/validator.js
+++ oop/validator.js
@@ -11,21 +11,21 @@ Validator.prototype.initOrder = function () {
     this._id = this._userInput.match(/(?<=\$)\d*/);
     this._tag = this._userInput.match(/(?<=\[)[\d\D]+(?=\])/);
     this._name = this._userInput.match(/(?<=\$)[\d\D]*(?=\$)/);
-}
+};
 
-Validator.prototype.validateUndoWord = function () { return null }
-Validator.prototype.validateSeparator = function () { return '각각의 명령어를 $로 구분해주세요.'; }
-Validator.prototype.validateAppWord = function () { return '잘못된 appWords 입니다.'; }
-Validator.prototype.validateTag = function () { if (!this._tag) return '가능한 tag형식이 아닙니다.'; }
-Validator.prototype.validateName = function () { if (!this._name) return '가능한 name형식이 아닙니다.'; }
+Validator.prototype.validateUndoWord = function () { return null };
+Validator.prototype.validateSeparator = function () { return '각각의 명령어를 $로 구분해주세요.' };
+Validator.prototype.validateAppWord = function () { return '잘못된 appWords 입니다.' };
+Validator.prototype.validateTag = function () { if (!this._tag) return '가능한 tag형식이 아닙니다.' };
+Validator.prototype.validateName = function () { if (!this._name) return '가능한 name형식이 아닙니다.' };
 Validator.prototype.validateId = function () {
     let existId = this._data.some((element) => element.id === Number(this._id[0]));
-    if (!existId) return '존재하지 않는 id입니다.';
-}
+    if (!existId) return '존재하지 않는 id입니다.'
+};
 Validator.prototype.validateStatus = function () {
     const statusList = ['all', 'todo', 'doing', 'done'];
-    if (statusList.indexOf(this._status[0]) === -1) return '가능한 status 형식이 아닙니다.';
-}
+    if (statusList.indexOf(this._status[0]) === -1) return '가능한 status 형식이 아닙니다.'
+};
 
 Validator.prototype.excuteValidation = function (userInput) {
     this._userInput = userInput;
@@ -36,13 +36,13 @@ Validator.prototype.excuteValidation = function (userInput) {
     if (!this._seperator) { return this.validateSeparator(); }
     switch (this._appWord[0]) {
         case 'show':
-            return this.validateStatus();
+            return this.validateStatus()
         case 'add':
-            return this.validateName() || this.validateTag();
+            return this.validateName() || this.validateTag()
         case 'delete':
-            return this.validateId();
+            return this.validateId()
         case 'update':
-            return this.validateId() || this.validateStatus();
+            return this.validateId() || this.validateStatus()
     }
-}
+};
 module.exports = Validator;
