254d606abe61031f205cfde152547b1692c9996e
김선욱 <43375739+wo0kgod@users.noreply.github.com>
2019-05-02 07:25:52 +0900

    wook & circus Step5 3 (#139)
    * feat: undostack구현 후 add undo 구현
    
    1. editor에서 viewer로this.result 객체를 주고받게 함
    2. app의 run함수에서 undostack에 this.result와 명령을 push함.
    3. undoTodo함수는  editor의 undostack을  pop하여 add의 undo를 실행하고 pop한 객체를 undoMessage로 전달
    
    * feat: Undo기능 구현
    
    undoTodo의 delete undo와 update undo 구현
    
    delete undo기능을 구현할떄는 id값을 가지고있는지 여부를 판단하는 기능추가
    update undo를 구현할때는 preStatus를 저장하고 함께 넘겨주었음.
    
    구현중 find로 찾은 객체가 원본 배열의 객체를 참조하여
    find로 찾은 객체의 status를 변경하여도 원본 배열의 status가 바뀌는 문제 발견
    
    * feat: redo 기능 구현
    
    redoTodo , redoMessage 함수구현
    
    1. undo가 실행되면 Editor의 undoTodo에서 redo스택에 쌓는다.
    2. show,undo,redo가 아닌  다른 명령이 실행되면 app에서 if문을 통해 redo스택을 초기화하였다.
    3. redo가 실행되면 redo스택에 쌓인 명령을 pop하여 실행하고, Editor의 redoTodo에서 undo스택에 쌓는다.
    4. redoTodo에서 pop한 명령을 return하여 redoMessage로 보내주었다.
    
    * refactor: prototype 패턴을 class 패턴으로 변경
    
    class 내부에 새로운 생성자함수를 만드는것을 실패하여
    싱글턴객체 newTodoObject를 addTodo 안에 생성
    
    * refactor: undoTodo와 redoTodo 함수의 if문제거
    
    undoTodo에서 this.Note안의 메소드가 this가 바뀌는 현상을 발견하여 call을 통해 this 바인딩하고
    함수별 필요한 인자를 NoteParam변수를 이용하여 할당하여 if문을 제거
    
    * refactor: editor의 내부메소드 인자 객체로 변경, 그외
    
    1. editor의 내부 메소드의 인자를 함수가 아니라 객체로 변경하여
    undoNoteParam, redoNoteParam을 제거하였음. 그러나 update의 경우 preStatus를
    넘겨주어야해서 undo내에서 if문을 추가
    * 여담으로 일괄처리를 하려다보면 항상 예외가 생겨 if문을 사용하게되는듯
    
    2.불필요한 filterNameStatus~ 함수 삭제
    
    3.undoStack에 push하는 객체 message 제거하고 , 비구조화 할당 용이하게 하기위해 this.result대신 함수내에 변수를 만들었고, 변수명을 변경하였음.
    
    추가적으로 리팩토링을 조금 더 고민해보면  undoStack에 쌓이는 객체와 , editor와 viewer사이에서 넘어가는 currentResult 객체의 key를 통일할 수 있을듯.

--- oop/schedule-manager.js
+++ oop/schedule-manager.js
@@ -5,52 +5,80 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// param으로 obj를 받으면 obj에 무엇이 들어있는지 알기 어려운것같은데 해결방안이 있을까요? 
-function App(obj) {
-    this.errorChecker = obj.errorChecker;
-    this.editor = obj.editor;
-    this.viewer = obj.viewer;
-}
+class App {
+    constructor(obj) {
+        const { errorChecker, editor, viewer } = obj
+        this.errorChecker = errorChecker;
+        this.editor = editor
+        this.viewer = viewer;
+        this.delayTime = 1000;
+        this.updateDelayTime = 3000;
+        // this.result; 현재결과를 저장하면 좋긴하겠지만 비구조화 할당불편함
+    }
 
+    convertParamObj([key, ...message]) {
+        // 리팩토링하고 싶지만 message[0]이건 각 명령이 너무 달라서 못바꾸고
+        // 각 key마다 생성되는 객체의 구조는 같아서 생성자함수 만들고, 덮어쓰는 방법으로는 해결할 수 있을듯 
+        const keyCommand = {
+            add: {
+                name: message[0],
+                tag: message[1]
+            },
+            update: {
+                id: message[0],
+                status: message[1]
+            },
+            delete: {
+                id: message[0]
+            }
+        }
+   
+        return keyCommand[key]
 
-
-App.prototype = {
-
+    }
 
     run() {
 
         rl.question('명령을 입력하세요: ', (input) => {
             if (input === 'q') return rl.close();
             try {
-                [key, ...message] = this.parseCommand(input)
-                this.errorChecker.$check(message)
+                const [key, ...message] = this.parseCommand(input)
+                const paramObj = this.convertParamObj(this.parseCommand(input));
+                if (key !== 'redo' && key !== 'undo') this.errorChecker.$check(message)
 
                 if (key === "show") {
-                    [status] = message
+                    const [status] = message
                     status === "all" ? this.viewer.showAll() : this.viewer.showFiltered(status);
                     return this.run();
                 }
 
-                const result = this.editor[key + 'Todo'](...message);
+                const currentResult = this.editor[key + 'Todo'](paramObj);
 
-                Promise.resolve()
+
+                if (key !== 'undo' && key !== 'redo') {
+                    this.editor.redoStack = [];
+                    this.editor.undoStack.push({ key, currentResult });
+                }
+                // console.log(this.editor.undoStack);
+                // console.log('---------------------');
+                // console.log(this.editor.redoStack);
+                // console.log('---------------------');
+                // console.log(schedule_list);
+
+                Promise.resolve(this.updateDelayTime)
                     .then(() => {
                         if (key === 'update') {
                             return new Promise((resolve) => {
                                 setTimeout(() => {
-                                    this.viewer[key + 'Message'](...result)
-                                    resolve()
-                                }, 3000)
+                                    this.viewer[key + 'Message'](currentResult)
+                                    resolve(this.delayTime)
+                                }, this.updateDelayTime)
                             })
                         }
-                        this.viewer[key + 'Message'](...result)
+                        this.viewer[key + 'Message'](currentResult)
+                        return new Promise((resolve) => resolve(this.delayTime))
                     })
-                    .then(() => {
-                        setTimeout(() => {
-                            this.viewer.showAll()
-                            this.run();
-                        }, 1000);
-                    })
+                    .then((time) => this.delayShow(time))
             }
             catch (e) {
                 this.viewer.errorMessage(e);
@@ -58,67 +86,104 @@ App.prototype = {
             }
 
         });
-    },
+    }
+
+    delayShow(time) {
+        setTimeout(() => {
+            this.viewer.showAll()
+            this.run();
+        }, time);
+    }
 
     parseCommand(input) {
         return input.split('$');
-    },
+    }
 }
 
 
-function Editor(errorChecker) {
-    this.errorChecker = errorChecker;
-}
+class Editor {
+    constructor(errorChecker) {
+        this.errorChecker = errorChecker;
+        this.undoStack = [];
+        this.redoStack = [];
+        this.Note = {
+            add : this.deleteTodo,
+            update: this.updateTodo,
+            delete: this.addTodo
+        }
 
-Editor.prototype = {
-    TodoObject: function (_name, _tag, _newId) {
-        this.name = _name
-        this.tag = _tag
-        this.status = 'todo'
-        this.id = _newId
-    },
 
-    addTodo(_name, _tag) {
-        const _newId = this.getUniqueId()
-        const newTodoObject = new this.TodoObject(_name, _tag, _newId);
+    }
+
+
+    undoTodo() {
+        this.errorChecker.stackNull(this.undoStack.length)
+        const lastEdit = this.undoStack.pop();
+        this.redoStack.push(lastEdit);
+
+        if (lastEdit.key === 'update'){
+            this.updateTodo({
+                id: lastEdit.currentResult.id,
+                status: lastEdit.currentResult.preStatus
+            });
+        }
+
+        else{this.Note[lastEdit.key].call(this, lastEdit.currentResult)}
+        return lastEdit;
+
+    }
+
+    redoTodo() {
+        this.errorChecker.stackNull(this.redoStack.length)
+
+        const lastUndo = this.redoStack.pop();
+        this[lastUndo.key + "Todo"](lastUndo.currentResult)
+        this.undoStack.push(lastUndo);
+        return lastUndo
+    }
+
+    addTodo(_obj) {
+        const { name, tag, id } = _obj;
+        const newId = id || this.getUniqueId()
+
+        // class 안에 생성자 함수 못만드는것같아서 add내부에 싱글턴 생성 
+        const newTodoObject = {
+            name: name,
+            tag: tag,
+            status: 'todo',
+            id: newId
+        };
         schedule_list.push(newTodoObject);
-        return [newTodoObject.name, newTodoObject.id];
-    },
+        return { ...newTodoObject };
+    }
 
-    updateTodo(_id, _status) {
+    updateTodo(_obj) {
+        const { id, status } = _obj;
 
-        const selectedObject = this.findSelectedIdObj(_id);
-        this.errorChecker.statusCheck(selectedObject.status,_status);
-        selectedObject.status = _status;
+        const selectedObject = this.findSelectedIdObj(id);
+        const preStatus = selectedObject.status
+        this.errorChecker.statusCheck(selectedObject.status, status);
+        // find로 찾은 객체가 원본 배열의 객체를 바꾸어버리는 문제 발견 
+        selectedObject.status = status;
 
-        return this.filterNameStatusArrayfromSelectedObject(selectedObject);
+        return { ...selectedObject, preStatus };
 
-    },
+    }
 
-    deleteTodo(_id) {
+    deleteTodo(_obj) {
+        const { id } = _obj;
+        const selectedObject = this.findSelectedIdObj(id);
+        const selectedObjectIndex = this.findSelectedIdObjIndex(id);
+        schedule_list.splice(selectedObjectIndex, 1);
 
-        const selectedObject = this.findSelectedIdObj(_id);
-        
+        return { ...selectedObject };
 
-        schedule_list.some((todo, index) => {
-            if (todo.id === parseInt(_id)) return schedule_list.splice(index, 1)
-        })
-        return this.filterNameStatusArrayfromSelectedObject(selectedObject);
-
-    },
+    }
 
     getUniqueId() {
         return Date.now()
-    },
+    }
 
-    filterNameStatusArrayfromSelectedObject(_obj) {
-        const filteredArray = Object.entries(_obj).filter(([key, value]) => {
-            const seletedStatus = ['name', 'status'];
-            return seletedStatus.includes(key)
-        })
-            .map(([key, value]) => value);
-        return filteredArray;
-    },
 
     findSelectedIdObj(_id) {
         const findedObj = schedule_list.find(todo => todo.id === parseInt(_id));
@@ -126,14 +191,15 @@ Editor.prototype = {
         return findedObj;
     }
 
+    findSelectedIdObjIndex(_id) {
+        return schedule_list.findIndex(todo => todo.id === parseInt(_id));
+    }
 
 }
 
 
 
-function Viewer() { }
-
-Viewer.prototype = {
+class Viewer {
     showAll() {
         const statusBox = schedule_list.reduce((accum, curr) => {
             accum[curr.status] = ++accum[curr.status] || 1;
@@ -143,58 +209,83 @@ Viewer.prototype = {
 
         const showAllResult = Object.entries(statusBox).map(([key, value]) => `${key}는 ${value}개`).join(", ");
         console.log(`현재상태 : ${showAllResult}`);
-    },
+    }
 
     showFiltered(_status) {
         const showFilteredResult = schedule_list.filter(todo => todo.status === _status)
             .map(todo => `'${todo.name}, ${todo.id}번'`);
         console.log(`${_status}리스트 : 총${showFilteredResult.length}건 : ${showFilteredResult.join(', ')}`);
 
-    },
+    }
+    undoMessage(_obj) {
+        const { key, currentResult } = _obj
+        const { id, name, status, preStatus } = currentResult
+        const undoNote = {
+            add: `명령취소! id: ${id} ${name}가 ${status}에서 삭제되었습니다.`,
+            update: `명령취소! id: ${id} ${name}의 ${status}를 ${preStatus}로 되돌렸습니다.`,
+            delete: `명령취소! id: ${id} ${name}가 삭제에서 ${status}로 생성되었습니다.`
+        }
+        console.log(undoNote[key])
 
-    addMessage(_name, _id) {
-        console.log(`${_name}가 추가되었습니다. (id :${_id})`);
-    },
+    }
+    redoMessage(_obj) {
+        const { key, currentResult } = _obj
+        const { id, name, status, preStatus } = currentResult
+        const redoNote = {
+            add: `undo취소! id: ${id} ${name}가 삭제에서 ${status}로 재 생성되었습니다.`,
+            update: `undo취소! id: ${id} ${name}의 ${preStatus}를 ${status}로 되돌렸습니다.`,
+            delete: `undo취소! id: ${id} ${name}가 ${status}에서 재 삭제되었습니다.`
+        }
+        console.log(redoNote[key])
+    }
 
-    updateMessage(_name, _status) {
-        console.log(`${_name}의 상태가 ${_status}로 변경되었습니다.`);
-    },
+    addMessage(_obj) {
+        const { name, id } = _obj
+        console.log(`${name}가 추가되었습니다. (id :${id})`);
+    }
 
-    deleteMessage(_name, _status) {
-        console.log(`${_name} ${_status}가 목록에서 삭제 되었습니다.`);
-    },
-    
-    errorMessage(errorObject){
+    updateMessage(_obj) {
+        const { name, status } = _obj
+        console.log(`${name}의 상태가 ${status}로 변경되었습니다.`);
+    }
+
+    deleteMessage(_obj) {
+        const { name, status } = _obj
+        console.log(`${name} ${status}가 목록에서 삭제 되었습니다.`);
+    }
+
+    errorMessage(errorObject) {
         console.log(errorObject.message)
     }
+
 }
 
-function ErrorChecker() { }
-
-ErrorChecker.prototype = {
+class ErrorChecker {
     $check(_message) {
         if (_message.length === 0) throw new Error('InputError : $ is not exist ')
-    },
+    }
 
     statusCheck(_todo_status, _status) {
         if (_todo_status === _status) throw new Error('StatusError : status is same')
-    },
+    }
 
     idCheck(_obj) {
         if (_obj === undefined) throw new Error('IdError : id is not valid')
     }
 
-
+    stackNull(_length) {
+        if (_length === 0) throw new Error('stackError : Stack is empty')
+    }
 }
 
 const schedule_errorChecker = new ErrorChecker();
 const schedule_editor = new Editor(schedule_errorChecker);
 const schedule_viewer = new Viewer();
 
-const schedule_manager =  new App({
-    errorChecker : schedule_errorChecker,
-    editor : schedule_editor,
-    viewer : schedule_viewer
+const schedule_manager = new App({
+    errorChecker: schedule_errorChecker,
+    editor: schedule_editor,
+    viewer: schedule_viewer
 })
 
 schedule_manager.run()
